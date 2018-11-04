815b35f4f264bddc36e2af1432782ea0eeeb685b
kb173 <karl.bittner@outlook.com>
2018-11-04 20:12:51 +0100

    Added sliders for setting current time and season to UI; WorldEnvironment receives signal and reacts

--- Scripts/GlobalSignal.gd
+++ Scripts/GlobalSignal.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal date_changed
 signal time_changed
+signal season_changed
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
--- Scripts/WorldEnvironment.gd
+++ Scripts/WorldEnvironment.gd
@@ -6,6 +6,9 @@ export var port = 8000
 
 onready var light = get_node("DirectionalLight")
 
+var current_time = 0
+var current_season = 0
+
 func get_middle_of_season(season): # 0 = winter, 1 = spring, 2 = summer, 3 = fall
 	return {day = 1, month = 2 + season * 3, year = 2018}
 	# Example: Spring -> 1.5.2018
@@ -37,20 +40,30 @@ func set_sun_position(altitude, azimuth):
 	
 	# Change the directional light to reflect sun change
 	light.rotation_degrees = Vector3(-altitude, 180 - azimuth, 0)
+	
 
 func _ready():
 	# React to time_changed events
 	GlobalSignal.connect("time_changed", self, "_on_time_changed")
+	GlobalSignal.connect("season_changed", self, "_on_season_changed")
 	
-	# Default values
-	set_sun_position_for_seasontime(2, 0)
+	# Set time and season with default values
+	update_time_season()
 	
 func _on_time_changed(time):
+	current_time = time
+	update_time_season()
+	
+func _on_season_changed(season):
+	current_season = season
+	update_time_season()
+	
+func update_time_season():
 	# Run this in a thread to prevent stutter while waiting for HTTP request
 	
 	# TODO: Kill old thread on new request
 	var sun_change_thread = Thread.new()
-	sun_change_thread.start(self, "_bg_set_sun_position_for_seasontime", [2, time])
+	sun_change_thread.start(self, "_bg_set_sun_position_for_seasontime", [current_season, current_time])
 	
 func _bg_set_sun_position_for_seasontime(data): # Threads can only take one argument, so we need this helper function
 	set_sun_position_for_seasontime(data[0], data[1])
