d37ab55002726157e555cc714aa2ad7738f2ceae
kb173 <karl.bittner@outlook.com>
2018-11-06 20:05:37 +0100

    Sky color matches sun position (dark when sun is far down, red in the morning and evening, ...)

--- Scripts/WorldEnvironment.gd
+++ Scripts/WorldEnvironment.gd
@@ -9,6 +9,9 @@ onready var light = get_node("DirectionalLight")
 var current_time = 0
 var current_season = 0
 
+var base_horizon_color = Color(142.0 / 255.0, 209.0 / 255.0, 232.0 / 255.0, 1.0) # Godot's default values - they look pretty good
+var base_top_color = Color(12.0 / 255.0, 116.0 / 255.0, 249.0 / 255.0, 1)
+
 func get_middle_of_season(season): # 0 = winter, 1 = spring, 2 = summer, 3 = fall
 	return {day = 1, month = 2 + season * 3, year = 2018}
 	# Example: Spring -> 1.5.2018
@@ -41,6 +44,38 @@ func set_sun_position(altitude, azimuth):
 	# Change the directional light to reflect sun change
 	light.rotation_degrees = Vector3(-altitude, 180 - azimuth, 0)
 	
+	update_colors(altitude, azimuth)
+	
+func update_colors(altitude, azimuth):
+	var new_horizon_color = base_horizon_color
+	var new_top_color = base_top_color
+	
+	if altitude < 20 and altitude > -20: # Sun is close to the horizon
+		# Make the horizon red/yellow-ish the closer the sun is to the horizon
+		var distance_to_horizon = 1 - abs(altitude) / 20
+		var horizon_blend_color = Color(0.7, 0.3, 0, distance_to_horizon)
+		
+		new_horizon_color = new_horizon_color.blend(horizon_blend_color)
+		
+		# Make the sky get progressively darker
+		var distance_to_black_point = 1 - ((20 + altitude) / 40)
+		new_horizon_color = new_horizon_color.darkened(distance_to_black_point)
+		
+	elif altitude <= -20: # Sun is far down -> make the horizon black
+		new_horizon_color = Color(0, 0, 0, 0)
+	
+	# Also make the top color darker / black when the sun is down
+	if altitude < 0 and altitude > -30:
+		var distance_to_black_point = abs(altitude) / 30
+		new_top_color = base_top_color.darkened(distance_to_black_point)
+		
+	elif altitude <= -30:
+		new_top_color = Color(0, 0, 0, 0)
+	
+	# Apply the colors to the sky
+	environment.background_sky.ground_horizon_color = new_horizon_color
+	environment.background_sky.sky_horizon_color = new_horizon_color
+	environment.background_sky.sky_top_color = new_top_color
 
 func _ready():
 	# React to time_changed events
