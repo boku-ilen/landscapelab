92e3fa56e919aba6a1b5e23b45e6da15b6bd9760
MoritzRoth <moritz.roth98@gmail.com>
2018-11-22 19:02:15 +0100

    implemented building base functionality
    - added Building scene, script and material
    - added EnvironmentalObject as superclass to Building and Tree, since both of them use update_position()
    - removed terrain_data parameter in methods of World.gd and instead used object variables
    - added realWorldToLocalWorld method to World.gd which transforms geo-data vectors into local space

---
+++ Scripts/EnvironmentalObject.gd
@@ -0,0 +1,30 @@
+extends Spatial
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func update_position():
+	var position = transform.origin
+	var vert = Vector3(0,1000,0)
+	#TODO might want to scale the up vert to max height so that no trees are left out in higher terrain
+	
+	var space_state = get_world().direct_space_state
+	var resultUp = space_state.intersect_ray(position, position + vert)
+	var resultDown = space_state.intersect_ray(position, position - vert)
+	
+	if not resultUp.empty():
+		global_transform.origin = resultUp.position
+	elif not resultDown.empty():
+		global_transform.origin = resultDown.position
