6652b56999bd5fc4741701e7ed496c048963c23e
kb173 <karl.bittner@outlook.com>
2018-12-08 21:49:01 +0100

    Added blocking queue for the ThreadPool tasks

--- Scripts/ThreadPool.gd
+++ Scripts/ThreadPool.gd
@@ -3,11 +3,11 @@ extends Node
 # Since starting a thread is expensive (it causes noticeable stutters), this singleton provides pre-started threads which you can delegate work to.
 # Starting a function in one of these pooled threads is non-blocking.
 
-const THREAD_COUNT = 1
-var tasks = []
-var threads = []
+const BlockingQueue = preload("res://scripts/BlockingQueue.gd")
 
-var array_mutex = Mutex.new()
+const THREAD_COUNT = 1
+var task_queue = BlockingQueue.new()
+var threads = []
 
 func _ready():
 	# Start THREAD_COUNT threads
@@ -21,8 +21,6 @@ func _ready():
 func thread_worker(data):
 	while true:
 		# If there are tasks in the queue: Fetch one and execute it
-		if tasks.size() == 0: continue # This should probably be checked when dequeueing for consistency, but that causes a crash!
-		
 		var task = dequeue_task()
 		
 		if task is Task:
@@ -30,16 +28,11 @@ func thread_worker(data):
 
 # Puts a task (obj + method) into the task queue
 func enqueue_task(task):
-#	array_mutex.lock()
-	tasks.push_front(task)
-#	array_mutex.unlock()
+	task_queue.enqueue(task)
 
 # Returns the item in the task queue which has been there the longest
 func dequeue_task():
-#	array_mutex.lock()
-	var task = tasks.pop_back()
-#	array_mutex.unlock()
-	return task
+	return task_queue.dequeue()
 
 # This class groups an object, a method and parameters for the method.
 # It provides an 'execute' function which calls the provided method on the provided object, with the set parameters.
