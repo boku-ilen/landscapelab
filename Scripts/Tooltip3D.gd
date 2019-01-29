04205ecffbd3858e7362df61c0837480d3e7178b
Karl Bittner <karl.bittner@outlook.com>
2019-01-29 17:22:10 +0100

    Added generic Tooltip3D for adding a tooltip with text and/or icon to any node

---
+++ Scripts/Tooltip3D.gd
@@ -0,0 +1,39 @@
+extends Spatial
+
+#
+# This scene can be added to any node in order to display a tooltip for it when the player is nearby.
+#
+
+onready var label_node = get_node("ViewportContainer/Viewport/CenterContainer/VBoxContainer/Label")
+onready var icon_node = get_node("ViewportContainer/Viewport/CenterContainer/VBoxContainer/MarginContainer/TextureRect")
+
+export(String) var label setget set_label_text, get_label_text
+export(Resource) var icon setget set_icon, get_icon
+
+var ready = false
+
+func _ready():
+	ready = true
+	update()
+	
+func update():
+	if ready:
+		if label != null:
+			label_node.text = label
+		icon_node.texture = icon
+
+# Set the text to display on the tooltip
+func set_label_text(text):
+	label = text
+	update()
+	
+func get_label_text():
+	return label_node.text
+
+# Set an icon to display above the text
+func set_icon(img):
+	icon = img
+	update()
+	
+func get_icon():
+	return icon_node.texture
