209945bcb4af6de41f069bd347f841cf4bc48343
Karl Bittner <karl.bittner@outlook.com>
2019-04-20 11:11:41 +0200

    Started redesigning UI according to mockup
    The VR and Debug toggles are dummies with no functionality at the moment. For the Season and Time button, a new type of button which displays its children when pressed (ShowHideChildrenButton) was added, which this is an instance of.

---
+++ UI/CustomElements/ShowHideChildrenButton.gd
@@ -0,0 +1,16 @@
+extends Button
+
+#
+# This button shows/hides its child elements when pressed.
+# They are hidden by default.
+#
+
+
+func _ready():
+	for child in get_children():
+		child.visible = false
+	
+	
+func _pressed() -> void:
+	for child in get_children():
+		child.visible = !child.visible
