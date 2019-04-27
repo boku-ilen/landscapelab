6a857f39bc3bc58387419577b91ad0d5e5f7382a
Karl Bittner <karl.bittner@outlook.com>
2019-04-27 12:09:59 +0200

    Minimap camera has a fixed height
    A new setting has been added for this height. The orthographic projection makes the height not matter much, but it does influence what's being displayed.

--- Perspectives/PC/Minimap/Minimap.gd
+++ Perspectives/PC/Minimap/Minimap.gd
@@ -1,10 +1,15 @@
 extends Spatial
 
+#
 # Basic minimap implementation which uses an orthographic camera placed above the player.
+#
 
 onready var cam = get_node("Camera")
 
+var CAMERA_HEIGHT = Settings.get_setting("minimap", "height")
+
 func _process(delta):
-	# Update position
+	# Set position to the last known player location, but at a fixed height
 	var player_pos = PlayerInfo.get_engine_player_position()
-	cam.translation = player_pos + Vector3(0, 1000, 0)
+	cam.translation = player_pos
+	cam.translation.y = CAMERA_HEIGHT
