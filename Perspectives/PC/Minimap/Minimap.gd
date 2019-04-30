35e5209e5ed514626308a4c53ef531c0c083a02b
Karl Bittner <karl.bittner@outlook.com>
2019-04-30 19:04:50 +0200

    Minimap size (zoom) is defined in settings

--- Perspectives/PC/Minimap/Minimap.gd
+++ Perspectives/PC/Minimap/Minimap.gd
@@ -7,6 +7,19 @@ extends Spatial
 onready var cam = get_node("Camera")
 
 var CAMERA_HEIGHT = Settings.get_setting("minimap", "height")
+var CAMERA_SIZE = Settings.get_setting("minimap", "size")
+
+
+func _ready():
+	change_size(CAMERA_SIZE)
+	
+	
+# Changes the size of the minimap camera to the given 'size'.
+# Size refers to the width/height of the camera making the orthographic projection.
+# e.g. a size of 2000 results in a visible area of roughly 4000x3000.
+func change_size(size):
+	cam.size = size
+
 
 func _process(delta):
 	# Set position to the last known player location, but at a fixed height
