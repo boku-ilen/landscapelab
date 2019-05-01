90f31790e42dc5ce5d4f6024fa3066750e0caed8
graf <christoph.graf@boku.ac.at>
2019-05-02 00:46:21 +0200

    * added global signals from/for the UI

--- Global/GlobalSignal.gd
+++ Global/GlobalSignal.gd
@@ -1,11 +1,28 @@
 extends Node
 
+#
+# GUI emitted signals
+# 
+
+# Date, Time, Season change
 signal date_changed
 signal time_changed
 signal season_changed
 
+# Minimap control
+signal minimap_zoom_in
+signal minimap_zoom_out
+signal toggle_follow_mode
+signal miniview_close
+signal miniview_map
+signal miniview_switch
+signal miniview_3rd
+signal miniview_1st
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+# special signals to notify the UI what the missing scene is now
+signal missing_map
+signal missing_3rd
+signal missing_1st
+
+# signal the ui to show the miniview again
+signal miniview_show
