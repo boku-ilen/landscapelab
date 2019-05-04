b534b826434e56005454602e50f7aa09954fb830
MoritzRoth <moritz.roth98@gmail.com>
2019-05-05 00:36:48 +0200

    added MinimapPolygon (just border, will not get filled atm)

--- Perspectives/PC/Minimap/MinimapPolygon.gd
+++ Perspectives/PC/Minimap/MinimapPolygon.gd
@@ -1,6 +1,11 @@
 extends Spatial
 
 export(Array, Vector2) var points
+export(bool) var face = true
+export(Color) var color = Color(255,171,171)
+export(float,0,1) var face_alpha = 0.5
+
+var border_width = 50
 
 func _ready():
 	
@@ -9,4 +14,31 @@ func _ready():
 	for p in points:
 		pts.append(Vector3(p.x, 0, p.y))
 	
-	#get_node("MeshInstance").update_mesh(pts, 10.0)
+	# generate border points
+	var border_pts = []
+	var inner_pts = []
+	var outer_pts = []
+	for i in range(points.size()):
+		var normal = (next_element(pts, i) - previous_element(pts, i)).normalized().cross(Vector3(0,1,0))
+		
+		var inner = pts[i] - normal * border_width
+		var outer = pts[i] + normal * border_width
+		border_pts.append([inner, outer])
+		inner_pts.append(inner)
+		outer_pts.append(outer)
+	
+	# TODO: if not face calculate border_pts for start and end differenlty
+	
+	get_node("PolygonEdge").update_mesh(border_pts, face)
+	#if face:
+	#	get_node("PolygonFace").update_mesh(inner_pts)
+
+
+func previous_element(var elements, var i):
+	if i == 0:
+		 return elements[elements.size()-1]
+	return elements[i-1]
+
+
+func next_element(var elements, var i):
+	return elements[(i + 1) % elements.size()]
---
+++ Perspectives/PC/Minimap/PolygonFace.gd
@@ -0,0 +1,10 @@
+extends MeshInstance
+
+
+# Triangulates given points and creates a mesh
+func update_mesh(var points):
+	
+	"""var surface_tool = SurfaceTool.new()
+	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLE)
+	
+	mesh = surface_tool.commit()"""
