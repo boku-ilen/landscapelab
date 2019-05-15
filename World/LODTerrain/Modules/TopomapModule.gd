b9e2e7fed26156d87492bd8b6b803803a2b177c0
Karl Bittner <karl.bittner@outlook.com>
2019-05-15 10:05:44 +0200

    Opentopomap is displayed on the minimap

---
+++ World/LODTerrain/Modules/TopomapModule.gd
@@ -0,0 +1,34 @@
+extends Module
+
+#
+# This module fetches the topomap for its tile, to be displayed on the minimap.
+#
+
+onready var mesh = get_node("MeshInstance")
+
+var topo
+
+
+func _ready():
+	mesh.mesh = tile.create_tile_plane_mesh(false)
+	
+	ThreadPool.enqueue_task(ThreadPool.Task.new(self, "get_textures", []))
+
+
+func _on_ready():
+	if topo:
+		mesh.material_override.albedo_texture = topo
+
+
+func get_topo():
+	var response = tile.get_texture_result("raster")
+	
+	if response:
+		if response.has("map"):
+			topo = CachingImageTexture.get(response.get("map"))
+
+
+func get_textures(data):
+	get_topo()
+	
+	make_ready()
