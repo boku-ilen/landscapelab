6bb5b5a6fe4c3f5734688f283c51a1c7523bb0c7
Karl Bittner <karl.bittner@outlook.com>
2019-05-15 09:45:39 +0200

    Approximate implementation of StaticAssetModule
    Some values are still not known or placeholders, but roughly, the implementation will look like this.

--- World/LODTerrain/Modules/StaticAssetModule.gd
+++ World/LODTerrain/Modules/StaticAssetModule.gd
@@ -11,27 +11,35 @@ extends Module
 var asset_result
 var dscn_node = preload("res://addons/dscn_io/DSCN_Runtime_Node.gd")
 
-export(int) var asset_id
+export(int) var asset_type_id
 
 
 func _ready() -> void:
-	ThreadPool.enqueue_task(ThreadPool.Task.new(self, "get_building_data_from_server", []))
+	ThreadPool.enqueue_task(ThreadPool.Task.new(self, "get_asset_data_from_server", []))
 
 
 func _on_ready():
-	for obj in asset_result:
-		var name_path = obj.asset + ".dae.dscn"
-		var dscn_node = dscn_node.new()
-		
-		# TODO: Instantiate
+	if asset_result:
+		for obj in asset_result:
+			# TODO: We only get the names, so we'll need to build the full path here somehow
+			var name_path = "/path/to/asset/" + obj.asset + ".dae.dscn"
+			var dscn_node = dscn_node.new()
+			
+			# TODO: Untested! Variable names and formats in the 'obj' might not be correct
+			var abs_pos = obj.position
+			var local_pos = Offset.to_engine_coordinates(abs_pos)
+			var ground_pos = WorldPosition.get_position_on_ground(Vector3(local_pos.x, 0, local_pos.y))
+			
+			dscn_node.transform.origin = ground_pos
+			
+			# Load the scene
+			dscn_node.filepath = name_path
+			dscn_node.import_dscn()
+			
+			add_child(dscn_node)
 
 
-func get_building_data_from_server(d):
-	var tile_pos = tile.get_true_position()
-	var osm_z = tile.get_osm_zoom()
-	
-	asset_result = ServerConnection.get_json("/assetpos/get/%d/%d/%d/%d.json" % \
-		[asset_id, -tile_pos[0], tile_pos[2], osm_z])
+func get_asset_data_from_server(d):
+	asset_result = tile.get_texture_result("assetpos/get_all/%d" % [asset_type_id])
 	
 	make_ready()
-
