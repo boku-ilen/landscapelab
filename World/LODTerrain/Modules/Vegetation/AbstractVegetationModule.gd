805c6453bfb547289e6a9e77b7ef6cbd9d408bae
Karl Bittner <karl.bittner@outlook.com>
2019-05-15 12:07:07 +0200

    Fixed stutters when vegetation is instanced
    All images are now loaded in the thread which also does the server requests. This means instancing vegetation is much more seamless now. Also made some documentation and error handling improvements in the process.

--- World/LODTerrain/Modules/Vegetation/AbstractVegetationModule.gd
+++ World/LODTerrain/Modules/Vegetation/AbstractVegetationModule.gd
@@ -13,8 +13,11 @@ var particles_scene = preload("res://World/LODTerrain/Modules/Util/HeightmapPart
 var LODS = Settings.get_setting("herbage", "density-at-lod")
 
 var result
-var vegetation_layer_data = {}
 var heightmap
+var splatmap
+
+# Holds image data for all phytocoenosis which will be loaded
+var phyto_data = {}
 
 
 func _ready():
@@ -32,45 +35,68 @@ func _ready():
 func get_splat_data(d):
 	var true_pos = tile.get_true_position()
 
+	# Vegetation result for this tile
 	result = ServerConnection.get_json("/%s/%d.0/%d.0/%d"\
 		% ["vegetation", -true_pos[0], true_pos[2], tile.get_osm_zoom()])
 		
+	# Load splatmap
+	if result and result.has("path_to_splatmap"):
+		splatmap = CachingImageTexture.get(result.get("path_to_splatmap"))
+		
+	# Get heightmap
 	var dhm_response = tile.get_texture_result("raster")
 	if dhm_response and dhm_response.has("dhm"):
 		heightmap = CachingImageTexture.get(dhm_response.get("dhm"))
 
 	if result and result.has("ids"):
+		# Iterate over all phytocoenosis IDs on this tile (but don't exceed num_layers)
 		for current_index in range(0, min(result.get("ids").size(), num_layers)):
-			vegetation_layer_data[result.get("ids")[current_index]] = ServerConnection.get_json("/vegetation/%d/%d"
+			# Data for the phytocoenosis with this ID
+			var this_result = ServerConnection.get_json("/vegetation/%d/%d"
 				% [result.get("ids")[current_index], my_vegetation_layer])
+			
+			# Load all images (distribution, spritesheet) and corresponding data
+			# We do this here because doing it in the main thread causes big stutters
+			phyto_data[result.get("ids")[current_index]] = VegetationData.new(
+				CachingImageTexture.get(this_result.get("path_to_distribution")),
+				CachingImageTexture.get(this_result.get("path_to_spritesheet")),
+				this_result.get("distribution_pixels_per_meter"),
+				this_result.get("number_of_sprites"))
 		
 	make_ready()
 
 
 func _on_ready():
 	if result:
-		construct_vegetation(result.get("path_to_splatmap"), result.get("ids"))
+		construct_vegetation(result.get("ids"))
 	else:
-		logger.warning("Vegetation module did not receive a response!")
+		logger.warning("Vegetation module did not receive a response! Deleting particle scenes...")
+		
+		# Delete the particle emitters since they will not be needed
+		for child in get_children():
+			child.queue_free()
 
 
 # Readies all required HeightmapParticles instances
-func construct_vegetation(splat_path, splat_ids):
+func construct_vegetation(splat_ids):
 	if LODS.has(String(tile.lod)):
 		var node = 0
 		var steps = 0
 		
 		for id in splat_ids:
+			# TODO: We might want to check here whether we have all required data for the
+			# phytocoenosis with this ID
+			
 			if steps >= num_layers:
 				break
 			
+			# Initialize the particle emitter
 			var nd = get_node(String(node))
-		
 			nd.set_rows(tile.size * LODS[String(tile.lod)])
 			nd.set_spacing(1 / LODS[String(tile.lod)])
 			
 			if node > num_layers - 1: break
-			set_parameters([nd, splat_path, id, node])
+			set_parameters([nd, id])
 			
 			node += 1
 			steps += 1
@@ -78,30 +104,43 @@ func construct_vegetation(splat_path, splat_ids):
 
 # Sets all shader parameters for both the particle shader and the texture shader of a HeightmapParticles instance
 func set_parameters(data):
-	if not heightmap or not vegetation_layer_data[data[2]] or vegetation_layer_data[data[2]].has("Error") \
-			or not vegetation_layer_data[data[2]].get("path_to_spritesheet"):
+	if not heightmap or not phyto_data.has(data[1]):
 		logger.warning("Vegetation module received a response, but the response contained invalid or incomplete data!");
 		return
 	
-	var distribution = CachingImageTexture.get(vegetation_layer_data[data[2]].get("path_to_distribution"))
-	var spritesheet = CachingImageTexture.get(vegetation_layer_data[data[2]].get("path_to_spritesheet"))
-	var distribution_pixels_per_meter = vegetation_layer_data[data[2]].get("distribution_pixels_per_meter")
-	var splatmap = CachingImageTexture.get(data[1])
+	var distribution = phyto_data[data[1]].distribution
+	var spritesheet = phyto_data[data[1]].spritesheet
+	var distribution_pixels_per_meter = phyto_data[data[1]].distribution_pixels_per_meter
+	var sprite_count = phyto_data[data[1]].number_of_sprites
 	
 	heightmap.flags = 4  # Enable filtering for smooth slopes
 	
-	var sprite_count = vegetation_layer_data[data[2]].get("number_of_sprites")
-	
+	# Values for the material of the particles themselves
 	data[0].material_override.set_shader_param("pos", translation)
 	data[0].material_override.set_shader_param("spritesheet", spritesheet)
 	data[0].material_override.set_shader_param("distribution", distribution)
 	data[0].material_override.set_shader_param("sprite_count", sprite_count)
 	data[0].material_override.set_shader_param("distribution_pixels_per_meter", distribution_pixels_per_meter)
 	
+	# Values for the material which places the particles
 	data[0].process_material.set_shader_param("tile_pos", translation)
 	data[0].process_material.set_shader_param("splatmap", splatmap)
 	data[0].process_material.set_shader_param("heightmap", heightmap)
-	data[0].process_material.set_shader_param("id", data[2])
+	data[0].process_material.set_shader_param("id", data[1])
 	
 	tile.set_heightmap_params_for_obj(data[0].process_material)
 	tile.set_heightmap_params_for_obj(data[0].material_override)
+	
+
+# Basic data structure for the data of one phytocoenosis
+class VegetationData:
+	var distribution
+	var spritesheet
+	var distribution_pixels_per_meter
+	var number_of_sprites
+	
+	func _init(dist, s, ppm, nns):
+		distribution = dist
+		spritesheet = s
+		distribution_pixels_per_meter = ppm
+		number_of_sprites = nns
