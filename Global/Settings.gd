39cc5128d13cca5fab206ff789a656705875a244
chrisgraf <christoph.graf@boku.ac.at>
2019-05-15 15:44:17 +0200

    * renamed settings.json to global-settings.json as first step to make an optional external settings.json readable to overwrite the bundled default settings * added a not yet used default parameter for the local resources path as a first step to make sending relative paths an option

--- Global/Settings.gd
+++ Global/Settings.gd
@@ -10,8 +10,8 @@ var data = parse_data()
 
 func parse_data():
 	var data_file = File.new()
-	if data_file.open("res://settings.json", File.READ) != OK:
-	    logger.error("Settings could not be read from json!")
+	if data_file.open("res://default-settings.json", File.READ) != OK:
+	    logger.error("BUG: default settings could not be read from json!")
 		
 	var data_text = data_file.get_as_text()
 	data_file.close()
@@ -19,7 +19,7 @@ func parse_data():
 	var data_parse = JSON.parse(data_text)
 	
 	if data_parse.error != OK:
-	    logger.error("Settings could not be parsed from json! Is the syntax correct?")
+	    logger.error("BUG: default settings could not be parsed from json! Is the syntax correct?")
 		
 	return data_parse.result
 
@@ -28,10 +28,10 @@ func parse_data():
 func get_setting(category, label, default=null):
 		
 	if not data.has(category):
-		logger.error("Invalid setting category: %s" % [category])
+		logger.error("BUG: Invalid setting category: %s" % [category])
 	elif not data[category].has(label):
 		if default == null:
-			logger.error("Setting category %s does not have label %s!" % [category, label])
+			logger.error("BUG: Setting category %s does not have label %s!" % [category, label])
 		else:  # if not found return a default value if it is defined
 			return default
 	else:
