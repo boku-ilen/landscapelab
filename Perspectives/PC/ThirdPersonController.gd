b0290717c9da7418924821726295c9ef88300212
Karl Bittner <karl.bittner@outlook.com>
2019-05-15 10:20:26 +0200

    Added collider to third person controller
    Thanks to the controller now being a kinematic body with a collider, it collides with the ground and anything else that also has a collider (in the same collision layer)

--- Perspectives/PC/ThirdPersonController.gd
+++ Perspectives/PC/ThirdPersonController.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends KinematicBody
 
 var MAX_DISTANCE_TO_GROUND = Settings.get_setting("third-person", "max-distance-to-ground")
 var START_DISTANCE_TO_GROUND = Settings.get_setting("third-person", "start_height")
@@ -13,7 +13,7 @@ onready var ground_check_ray = get_node("GroundCheckRay")
 func _ready():
 	Offset.connect("shift_world", self, "shift")
 	
-	current_distance_to_ground = START_DISTANCE_TO_GROUND
+	translation.y = START_DISTANCE_TO_GROUND
 
 
 func _input(event):
@@ -24,21 +24,21 @@ func _input(event):
 			else:
 				dragging = false
 				
-		elif event.button_index == BUTTON_WHEEL_UP:
-			current_distance_to_ground -= MOUSE_ZOOM_SPEED
+		elif event.button_index == BUTTON_WHEEL_UP: # Move down when scrolling up
+			move_and_collide(Vector3(0, -MOUSE_ZOOM_SPEED, 0))
 			
-		elif event.button_index == BUTTON_WHEEL_DOWN:
-			current_distance_to_ground += MOUSE_ZOOM_SPEED
+		elif event.button_index == BUTTON_WHEEL_DOWN: # Move up when scrolling down
+			move_and_collide(Vector3(0, MOUSE_ZOOM_SPEED, 0))
 		
 		current_distance_to_ground = clamp(current_distance_to_ground, 0, MAX_DISTANCE_TO_GROUND)
 		
 	elif event is InputEventMouseMotion:
 		if dragging:
-			translation -= Vector3(event.relative.x, 0, event.relative.y) * current_distance_to_ground / 600
+			move_and_collide(-Vector3(event.relative.x, 0, event.relative.y) * current_distance_to_ground / 600)
 
 
 func _process(delta):
-	translation.y = current_distance_to_ground
+	current_distance_to_ground = translation.y
 	PlayerInfo.update_player_pos(translation)
 
 
