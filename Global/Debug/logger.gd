9d236b87694054c13f763a5142e7ba825877773c
Karl Bittner <karl.bittner@outlook.com>
2019-05-28 12:14:23 +0200

    Added debug view with Logger to UI
    When toggling the debug button, the logger ouput is shown on the UI. The log level can also be manipulated.

--- Global/Debug/logger.gd
+++ Global/Debug/logger.gd
@@ -60,148 +60,167 @@ extends Node
 
 # Levels of debugging available
 class Level:
-    const DEBUG = 0
-    const INFO = 1 # default
-    const WARNING = 2
-    const ERROR = 3
-    const CRITICAL = 4
+	const DEBUG = 0
+	const INFO = 1 # default
+	const WARNING = 2
+	const ERROR = 3
+	const CRITICAL = 4
 
 # Built in time formatters
 class TimeFormat:
-    const NONE = 0
-    const ELAPSED = 1
-    const TIME = 2
-    const DATETIME = 3 # default
+	const NONE = 0
+	const ELAPSED = 1
+	const TIME = 2
+	const DATETIME = 3 # default
 
 const STRING_TYPE = typeof("") # TODO: Is there a nicer way to avoid creating a string every time we compare an object?
+
+# The logger appends all logged data to this array
+var stream = []
+var stream_mutex = Mutex.new()
+var MAX_STREAM_SIZE = 0
 
 # Print to stdout?
 var print_stdout = true setget set_print_stdout, get_print_stdout
 func get_print_stdout():
-    return print_stdout
+	return print_stdout
 func set_print_stdout(value):
-    assert(value in [true, false])
-    print_stdout = value
+	assert(value in [true, false])
+	print_stdout = value
 
 # Logging level.
-var level = Level.INFO setget set_level, get_level
+var level = 0 setget set_level, get_level
 func get_level():
-    return level
+	return level
 func set_level(value):
-    assert(level in [Level.DEBUG, Level.INFO, Level.WARNING, Level.ERROR, Level.CRITICAL])
-    level = value
+	assert(level in [Level.DEBUG, Level.INFO, Level.WARNING, Level.ERROR, Level.CRITICAL])
+	level = value
+	
+# We need to get the settings in the _ready function since the logger is instantiated before the settings on startup
+func _ready():
+	MAX_STREAM_SIZE = Settings.get_setting("logger", "max-messages-in-stream")
+	level = Settings.get_setting("logger", "default-level")
 
 # Logging to file.
 var file = null
 var outputfilename = null setget set_filename, get_filename
 func get_filename():
-    return outputfilename
+	return outputfilename
 func set_filename(value):
-    if file != null:
-        info("Stopped logging to file: %s" % filename)
-        file.close()
+	if file != null:
+		info("Stopped logging to file: %s" % filename)
+		file.close()
 
-    if value != null:
-        file = File.new()
-        outputfilename = value
-        file.open(outputfilename, File.WRITE)
-        info("Started logging to file: %s" % outputfilename)
-    else:
-        file = null
-        outputfilename = null  
+	if value != null:
+		file = File.new()
+		outputfilename = value
+		file.open(outputfilename, File.WRITE)
+		info("Started logging to file: %s" % outputfilename)
+	else:
+		file = null
+		outputfilename = null  
 
 # Log timer
 var time_format_func = funcref(self, "format_time_datetime") setget set_time_format_func
 func set_time_format_func(value):
-    time_format_func = value
+	time_format_func = value
 
 var time_format = TimeFormat.DATETIME setget set_time_format
 func set_time_format(value):
-    if value == TimeFormat.NONE:
-        self.time_format_func = funcref(self, "format_time_none")
-    elif value == TimeFormat.DATETIME:
-        self.time_format_func = funcref(self, "format_time_datetime")
-    elif value == TimeFormat.TIME:
-        self.time_format_func = funcref(self, "format_time_time")
-    elif value == TimeFormat.ELAPSED:
-        self.time_format_func = funcref(self, "format_time_elapsed")
-    else:
-        assert(false) # Bad time format used.
+	if value == TimeFormat.NONE:
+		self.time_format_func = funcref(self, "format_time_none")
+	elif value == TimeFormat.DATETIME:
+		self.time_format_func = funcref(self, "format_time_datetime")
+	elif value == TimeFormat.TIME:
+		self.time_format_func = funcref(self, "format_time_time")
+	elif value == TimeFormat.ELAPSED:
+		self.time_format_func = funcref(self, "format_time_elapsed")
+	else:
+		assert(false) # Bad time format used.
 
 # --- Time formatters for use by the logger.
 
 func format_time_none():
-    return ""
-    
+	return ""
+	
 func format_time_elapsed():
-    return "[%s] " % _format_elapsed()
+	return "[%s] " % _format_elapsed()
 
 func format_time_time():
-    return "[%s] " % _format_time()
+	return "[%s] " % _format_time()
 
 func format_time_datetime():
-    return "[%s %s] " % [_format_date(), _format_time()]
+	return "[%s %s] " % [_format_date(), _format_time()]
 
 # --- General time formatting functions
 
 func _format_time():
-    """Not used directly, but might come in useful"""
-    var time = OS.get_time()
-    # This is not "correct", but gives impression of ms moving on!
-    var ms = OS.get_ticks_msec() % 1000
+	"""Not used directly, but might come in useful"""
+	var time = OS.get_time()
+	# This is not "correct", but gives impression of ms moving on!
+	var ms = OS.get_ticks_msec() % 1000
 
-    return "%02d:%02d:%02d.%03d" % [time["hour"], time["minute"], time["second"], ms]
+	return "%02d:%02d:%02d.%03d" % [time["hour"], time["minute"], time["second"], ms]
 
 func _format_elapsed():
-    """Not used directly, but might come in useful"""
-    var time = OS.get_ticks_msec()
-    var ms = time % 1000
-    var s = int(time / 1000) % 60
-    var m = int(time / 60000) % 60
-    var h = int(time / 3600000)
+	"""Not used directly, but might come in useful"""
+	var time = OS.get_ticks_msec()
+	var ms = time % 1000
+	var s = int(time / 1000) % 60
+	var m = int(time / 60000) % 60
+	var h = int(time / 3600000)
 
-    return "%d:%02d:%02d.%03d " % [h, m, s, ms]
+	return "%d:%02d:%02d.%03d " % [h, m, s, ms]
 func _format_date():
-    """Not used directly, but might come in useful"""
-    var date = OS.get_date()
+	"""Not used directly, but might come in useful"""
+	var date = OS.get_date()
 
-    return "%d-%02d-%02d" % [date["year"], date["month"], date["day"]]
+	return "%d-%02d-%02d" % [date["year"], date["month"], date["day"]]
 
 # --- Message writing methods
 
 func debug(data):
-    """Debugging message"""
-    if level == Level.DEBUG:
-        _write('DEBUG:', data)
+	"""Debugging message"""
+	if level == Level.DEBUG:
+		_write('DEBUG:', data)
 
 func info(data):
-    """Informational message"""
-    if level <= Level.INFO:
-        _write('INFO:', data)
+	"""Informational message"""
+	if level <= Level.INFO:
+		_write('INFO:', data)
 
 func warning(data):
-    """Warning message"""
-    if level <= Level.WARNING:
-        _write('WARN:', data)
+	"""Warning message"""
+	if level <= Level.WARNING:
+		_write('WARN:', data)
 
 func error(data):
-    """Error message"""
-    if level <= Level.ERROR:
-        _write('ERROR:', data)
+	"""Error message"""
+	if level <= Level.ERROR:
+		_write('ERROR:', data)
 
 func critical(data):
-    """Critical error message"""
-    _write('CRIT:', data)
+	"""Critical error message"""
+	_write('CRIT:', data)
 
 func _write(type, data):
-    """Actually write out the message string"""
-    if typeof(data) != STRING_TYPE:
-        data = var2str(data)
+	"""Actually write out the message string"""
+	if typeof(data) != STRING_TYPE:
+		data = var2str(data)
 
-    var message = '%s%5s %s' % [time_format_func.call_func(), type, data]
+	var message = '%s%5s %s' % [time_format_func.call_func(), type, data]
 
-    if print_stdout:
-        print(message)
+	# Insert the message into the log stream
+	# We must use a mutex here, since the logger can be called from threads!
+	stream_mutex.lock()
+	stream.push_back(message)
+	# Pop old elements if the stream is too big
+	while stream.size() > MAX_STREAM_SIZE:
+		stream.pop_front()
+	stream_mutex.unlock()
 
-    if file != null:
-        file.store_line(message)
+	if print_stdout:
+		print(message)
+
+	if file != null:
+		file.store_line(message)
