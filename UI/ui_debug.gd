e647ab96c2aef68d9a72b81c3b0dda37a6d33855
Karl Bittner <karl.bittner@outlook.com>
2019-06-10 16:27:12 +0200

    Added SignalButton for buttons which send signals on press/release
    Since several buttons in the UI only emit signals from GlobalSignal when they're pressed or released (e.g. debug, VR), this functionality has been abstracted to the SignalButton.

--- UI/ui_debug.gd
+++
@@ -1,9 +0,0 @@
-extends TextureButton
-
-
-# emit the switch of the debug button
-func _toggled(button_pressed: bool) -> void:
-	if button_pressed:
-		GlobalSignal.emit_signal("debug_enable")
-	else:
-		GlobalSignal.emit_signal("debug_disable")
--- UI/ui_vr.gd
+++
@@ -1,9 +0,0 @@
-extends TextureButton
-
-
-# emit the switch of the vr button
-func _pressed():
-	if pressed:
-		GlobalSignal.emit_signal("vr_enable")
-	else:
-		GlobalSignal.emit_signal("vr_disable")
