f1591790941f7aa5f78714b2656e8ea753119b9d
Karl Bittner <karl.bittner@outlook.com>
2019-06-10 15:53:25 +0200

    Added real player position to debug view

---
+++ UI/DebugInfo/PositionDisplay.gd
@@ -0,0 +1,10 @@
+extends Control
+
+onready var label = get_node("HBoxContainer/Data")
+
+
+func _process(delta: float) -> void:
+	# Change the label text to show the current player position
+	var player_pos = PlayerInfo.get_true_player_position()
+	
+	label.text = "%d, %d, %d" % [player_pos[0], player_pos[1], player_pos[2]]
