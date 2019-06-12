98b51b3209584e37744d08278cb70f6f7c4105de
Karl Bittner <karl.bittner@outlook.com>
2019-06-12 21:34:41 +0200

    Refactored interpolating height

--- World/LODTerrain/Modules/TerrainColliderModule.gd
+++ World/LODTerrain/Modules/TerrainColliderModule.gd
@@ -46,36 +46,40 @@ func get_height_at_position(var pos):
 		return 0
 	
 	if img:
+		# TODO: There is still a slight inconsistency with how the mesh actually looks here.
+		# I believe that this might be because Godot's plane mesh has the triangles like this:
+		#  ___
+		# |  /|
+		# | / |
+		# |/__|
+		# 
+		# This makes them fold in a particular way which the linear interpolation can't predict.
 		img.lock()
 		
 		var img_size = img.get_size()
+		var subdiv = max(1, int(round(img_size.x / tile.subdiv)))
 		
 		# Scale the position to a pixel position on the image
 		var pos_scaled = (Vector2(pos.x, pos.z) - Vector2(gtranslation.x, gtranslation.z) + Vector2(tile.size / 2, tile.size / 2)) / tile.size
 		var pix_pos = pos_scaled * img_size
 		
+		# We want to limit the accuracy here to what is actually displayed - otherwise, there are bumps
+		#  and holes here which are not visible. So e.g. if we have a subdivision of 8 and 256 pixels,
+		#  we clamp the values to 0, 32, 64, ... (256/8)
+		var scaled_pix_pos = Vector2()
+		scaled_pix_pos.x = int(pix_pos.x) - int(pix_pos.x) % subdiv
+		scaled_pix_pos.y = int(pix_pos.y) - int(pix_pos.y) % subdiv
+		
 		# The factor by which the point we want is offset from the closest actual pixel
 		# Used for interpolation later
-		var xf = (pix_pos.x - int(pix_pos.x) + int(pix_pos.x) % 16) / 16
-		var yf = (pix_pos.y - int(pix_pos.y) + int(pix_pos.y) % 16) / 16
-		
-		# We want to limit the accuracy here to what is actually displayed - otherwise, there are bumps
-		#  and holes which are not visible
-		pix_pos.x = int(pix_pos.x) - int(pix_pos.x) % 16
-		pix_pos.y = int(pix_pos.y) - int(pix_pos.y) % 16
-		
-		# Offset since we only add values later?
-		pix_pos -= Vector2(8, 8)
-		
-		# Clamp to max values
-		pix_pos.x = clamp(pix_pos.x, 0, img_size.x - 1)
-		pix_pos.y = clamp(pix_pos.y, 0, img_size.y - 1)
+		var xf = (pix_pos.x - scaled_pix_pos.x) / subdiv
+		var yf = (pix_pos.y - scaled_pix_pos.y) / subdiv
 		
 		# Get multiple height samples, offset by the accuracy of our visible mesh
-		var height1 = get_height_from_image(img, pix_pos + Vector2(0, 0))
-		var height2 = get_height_from_image(img, pix_pos + Vector2(16, 0))
-		var height3 = get_height_from_image(img, pix_pos + Vector2(0, 16))
-		var height4 = get_height_from_image(img, pix_pos + Vector2(16, 16))
+		var height1 = get_height_from_image(img, scaled_pix_pos + Vector2(0, 0) * subdiv)
+		var height2 = get_height_from_image(img, scaled_pix_pos + Vector2(1, 0) * subdiv)
+		var height3 = get_height_from_image(img, scaled_pix_pos + Vector2(0, 1) * subdiv)
+		var height4 = get_height_from_image(img, scaled_pix_pos + Vector2(1, 1) * subdiv)
 		
 		img.unlock()
 	
@@ -90,6 +94,11 @@ func get_height_at_position(var pos):
 
 # Returns the height on the image at the given pixel position in meters.
 func get_height_from_image(img, pix_pos):
+	var img_size = img.get_size()
+	
+	pix_pos.x = clamp(pix_pos.x, 0, img_size.x - 1)
+	pix_pos.y = clamp(pix_pos.y, 0, img_size.y - 1)
+	
 	return (img.get_pixel(pix_pos.x, pix_pos.y).r * 255 * pow(2, 16) \
 		+ img.get_pixel(pix_pos.x, pix_pos.y).g * 255 * pow(2, 8) \
 		+ img.get_pixel(pix_pos.x, pix_pos.y).b * 255) / 100
