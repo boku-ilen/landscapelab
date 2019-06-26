60c5c7888c6a215475180e822420b1078f53d1c8
mathiasBaumgartinger <hiasbaum@gmail.com>
2019-06-26 11:44:07 +0200

    Polygon drawer does not crash the simulation anymore, but probably also does not quite work as intended ...

--- Util/PolygonDrawer/PolygonDrawer.gd
+++ Util/PolygonDrawer/PolygonDrawer.gd
@@ -1,47 +1,85 @@
-extends MeshInstance
+extends Node
 
 #
-# This scene can take a specific geojson data format and draw a polygon from the extracted data to 
-# a given perspective. This scene should be instanciated when clicking an item and destructed when clicking a new item
+# This class can take a specific geojson data format and draw a polygon from the extracted data to 
+# a given perspective. This class should be instanced when clicking an item and destructed when clicking a new item
 # or leaving the placement option. The ID has to be set in order for this to function
 #
 
-export(int) var id
-# So the material can be configured using the Godot-GUI
-export(SpatialMaterial) var material
-
-
-var coordinates = Array()
-
-
-func _ready():
-	exctract_geo_data()
-	mesh = mesh_it(coordinates)
-	self.set_mesh(mesh)
-
-
-# Extracts the wanted coordinates to draw the polygon from the json file
-func exctract_geo_data():
-	var assets = ServerConnection.get_json("/assetpos/get_all_editable_assettypes.json")
-	for asset_type in assets:
-		if asset_type["assets"][id] != null:
-			for geo_data in asset_type["placement_arreas"]:
-				var as_string_floats = geo_data.split_floats(" ")
-				var as_vector = Vector2(as_string_floats[0].to_float(), as_string_floats[1].to_float())
-				coordinates.append(as_vector)
-
-
-# Creates the vertecies to draw the polygon
-func mesh_it(vertecies):
+class Drawer:
+	var id
 	
-	var surface_tool = SurfaceTool.new();
-	surface_tool.set_material(material)
-	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES);
+	var coordinates = Array()
+	var polygon_mesh = preload("res://Util/PolygonDrawer/Polygon.tscn")
 	
-	for i in range(0, vertecies.size()):
-		surface_tool.add_vertex(vertecies[i])
+	func build(asset_id):
+		id = asset_id
+		#exctract_geo_data()
+		dummy_extract_data()
+		var mesh = mesh_it(coordinates)
+		var instanced_polygon = polygon_mesh.instance()
+		instanced_polygon.set_mesh(mesh)
+		
+		return instanced_polygon
 	
-	for i in range(0, vertecies.size()):
-		surface_tool.add_index(i)
 	
-	return surface_tool.commit()
+	# Extracts the wanted coordinates to draw the polygon from the json file
+	func exctract_geo_data():
+		var assets = ServerConnection.get_json("/assetpos/get_all_editable_assettypes.json")
+		for asset_type in assets:
+			if asset_type["assets"][id] != null:
+				for geo_data in asset_type["placement_arreas"]:
+					var as_string_floats = geo_data.split_floats(" ")
+					var as_vector = Vector3(as_string_floats[0].to_float(), 0, as_string_floats[1].to_float())
+					coordinates.append(as_vector)
+	
+	
+	# As the GeoJSON only gives us 2D-Coordinates we have to find the y-axis for given x,z
+	func generate_y_axis():
+		# The for loop sadly does give pass by value thus we have to fill new array
+		var coordinates_new = Array()
+		for coordinate in coordinates:
+			coordinates_new.append(WorldPosition.get_position_on_ground(coordinate))
+		
+		coordinates = coordinates_new
+	
+	
+	func dummy_extract_data():
+		coordinates.append(Vector3(-1558000, 0, 5906696))
+		coordinates.append(Vector3(-1559000, 0, 6000000))
+		coordinates.append(Vector3(-1559000, 0, 5500000))
+		
+#		var file = File.new()
+#		file.open("res://Polygon-Test.json", file.READ)
+#		var text = file.get_as_text()
+#		var result_json = JSON.parse(text)
+#
+#		var polygon_description =  result_json.result["bounding_polygon"]
+#		var occurance_of_polygon = polygon_description.find("POLYGON")
+#		var coords = polygon_description.substring(occurance_of_polygon + 7)
+#		coords.trim_prefix("(((")
+#		coords.trim_suffix(")))")
+#
+#
+#		var parsed_string_coordinates = coords.split(",")
+#		for string_coordinate in parsed_string_coordinates:
+#			var as_string_floats = parsed_string_coordinates.split_floats(" ")
+#			var as_vector = Vector3(as_string_floats[0].to_float(), 0, as_string_floats[1].to_float())
+#			coordinates.append(as_vector)
+			
+		
+	
+	# Creates the vertecies to draw the polygon
+	func mesh_it(vertecies):
+		
+		var surface_tool = SurfaceTool.new();
+		#surface_tool.set_material(material)
+		surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES);
+		
+		for i in range(0, vertecies.size()):
+			surface_tool.add_vertex(vertecies[i])
+		
+		for i in range(0, vertecies.size()):
+			surface_tool.add_index(i)
+		
+		return surface_tool.commit()
--- World/MainScene/MainScene.gd
+++ World/MainScene/MainScene.gd
@@ -1,5 +1,32 @@
 extends Spatial
 
 
+#onready var polygon_mesh = preload("res://Util/PolygonDrawer/PolygonDrawer.tscn")
+onready var polygon_drawer = load("res://Util/PolygonDrawer/PolygonDrawer.gd")
+var instanced_drawer
+var instanced_polygon = null
+
+
 func _ready():
-	pass
+	# Connect the signals to show the editable-asset polygons or hide them
+	GlobalSignal.connect("changed_asset_id", self, "instance_polygon") 
+	GlobalSignal.connect("input_disabled", self, "remove_polygon")
+	instanced_drawer = polygon_drawer.Drawer.new()
+
+
+# Instace a polygon with the by searching the wished coordinates in a GeoJSON with the given id of the siganl
+func instance_polygon(id):
+	# Remove the instanced polygon of the clicked asset from before
+	if instanced_polygon != null:
+		self.remove_child(instanced_polygon)
+		instanced_polygon.queue_free()
+		
+	instanced_polygon = instanced_drawer.build(id)
+	self.add_child(instanced_polygon, true)
+
+
+# Remove the polygon when leaving the asset-mode
+func remove_polygon():
+	self.remove_child(instanced_polygon)
+	instanced_polygon.queue_free()
+	#polygon_mesh = null
