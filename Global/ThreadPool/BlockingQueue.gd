30831c9a82e83cb885fae8f030290b19e0ec1e30
Karl Bittner <karl.bittner@outlook.com>
2019-06-26 11:42:50 +0200

    Better priority handling in ThreadPool

--- Global/ThreadPool/BlockingQueue.gd
+++ Global/ThreadPool/BlockingQueue.gd
@@ -8,13 +8,11 @@ var _queue = []
 
 # Place an element at the back of the queue.
 # This function is blocking if the queue is currently locked (being accessed).
-func enqueue(val, priority):
+func enqueue(val):
 	while _mutex.try_lock() == ERR_BUSY:
 		continue
 	
-	_queue.push_front([val, priority])
-	# TODO: Naive implementation of a priority queue. This is too inefficient!
-	_queue.sort_custom(self, "sort_priority")
+	_queue.push_front(val)
 	_mutex.unlock()
 	_sem.post()
 
@@ -28,12 +26,7 @@ func dequeue():
 	
 	while _mutex.try_lock() == ERR_BUSY:
 		continue
-	var val = _queue.pop_back()[0]
+	var val = _queue.pop_back()
 	_mutex.unlock()
 	return val
-	
 
-# Custom sort function for the priority queue - the priority is the second element in
-#  the array (the first is the actual Task)
-func sort_priority(v1, v2):
-	return v1[1] < v2[1]
