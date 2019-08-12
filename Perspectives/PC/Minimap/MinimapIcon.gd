ffa3871e76fb7b8e9ac37a46e1aeebab2154a0c1
karl <karl.bittner@hexaquo.at>
2019-08-12 21:52:56 +0200

    Added rotating billboard shader for MinimapIcon

--- Perspectives/PC/Minimap/MinimapIcon.gd
+++ Perspectives/PC/Minimap/MinimapIcon.gd
@@ -13,15 +13,28 @@ onready var icon_sprite = get_node("IconSprite")
 
 
 func _ready():
-	if not rotate: 
-		# 1 equals BILLBOARD_ENABLED
-		icon_sprite.material_override.params_billboard_mode = 1
-	else:
-		# 0 equals DISABLED
-		# TODO: in godot 3.2 billboard rotation will be added so the icon will only rotate around z-axis
-		icon_sprite.material_override.params_billboard_mode = 0
-		
 	update_icon(icon)
+
+
+# Because Godot's Vector2.angle_to(Vector2) always returns the __minimum__ angle, we
+#  implement our own function to get the __clockwise__ angle (meaning an angle to the
+#  left yields a different result than the same angle to the right)
+func _get_clockwise_angle(vector1: Vector2, vector2: Vector2):
+	var dot = vector1.dot(vector2)
+	var det = vector1.x * vector2.y - vector1.y * vector2.x
+	
+	return atan2(det, dot)
+
+
+func _process(delta):
+	# If the rotate flag is set, supply the shader with the latest rotation.
+	if rotate:
+		# The rotation is the angle between the local forward vector and the global forward vector.
+		# The y axis can be ignored since we only care about rotation viewed from the top (yaw).
+		var local_forward = global_transform.basis.z
+		var angle = _get_clockwise_angle(Vector2(local_forward.x, local_forward.z), Vector2(0, 1))
+		
+		icon_sprite.material_override.set_shader_param("rotation_rads", angle)
 
 
 # updates the icon with a new texture
