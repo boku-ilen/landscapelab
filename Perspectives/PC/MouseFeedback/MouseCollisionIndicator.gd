8d371db2bfe055cb809cf18acd773ccd7fb7120a
mathiasBaumgartinger <hiasbaum@gmail.com>
2019-08-19 09:17:38 +0200

    Instanciated marker, almost working. Mouse cursor position seems to be inverted however.

---
+++ Perspectives/PC/MouseFeedback/MouseCollisionIndicator.gd
@@ -0,0 +1,46 @@
+extends Spatial
+
+#
+# Attach this scene to the MousePoint scene. It will give an indicator on where the
+# mouse cursor in the world is currently placed. 
+#
+
+onready var world = get_tree().get_root().get_node("Main/TileHandler") # Required for getting exact ground positions
+onready var cursor = get_node("RayCast")
+onready var player = get_tree().get_root().get_node("Main/Perspectives/FirstPersonPC")
+
+var RAY_LENGTH = Settings.get_setting("item-spawner", "camera-ray-length") # Distance that will be checked for collision with the ground
+
+export(Mesh) var particle_highlight_position
+export(Mesh) var particle_teleport_to
+var particle = preload("res://Perspectives/PC/MouseFeedback/Particle.tscn")
+
+var teleport_mode : bool = false
+
+
+func _ready():
+	cursor.cast_to = Vector3(0, 0, -RAY_LENGTH)
+	
+	GlobalSignal.connect("teleport", self, "_set_teleport_mode")
+	particle = particle.instance()
+	get_tree().get_root().add_child(particle)
+
+
+func _process(delta):
+	if cursor.is_colliding():
+		particle.translation = world.get_ground_coords(cursor.get_collision_point())
+
+
+# This callback is called whenever any input is registered
+func _input(event):
+	if teleport_mode:
+		if event.is_action_pressed("teleport"):
+			_teleport_player()
+
+
+func _set_teleport_mode():
+	teleport_mode = !teleport_mode
+
+
+func _teleport_player():
+	player.translation = world.get_ground_coords(cursor.get_collision_point())
