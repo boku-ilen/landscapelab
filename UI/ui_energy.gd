3a03f8fc2860e11650029b65a0fc951857674e3b
mathiasBaumgartinger <hiasbaum@gmail.com>
2019-08-26 16:19:22 +0200

    Target value implementation. Option for changing the value with season change is there, however not connected with the GUI-signal yet. Color formatting is missing.

--- UI/ui_energy.gd
+++ UI/ui_energy.gd
@@ -6,22 +6,31 @@ extends TextureButton
 # details view
 #
 
+# The target energy value shows the energy that should optimally be reached, there are three different options:
+# General, summer, winter
+var target_energy_value : Dictionary
 # Get the "Energy Value"- and "Amount"-label so it can be dynamically changed with update()
-var energy_value
-var amount
+var energy_value_label : Label
+var assets_amount_label : Label
+var target_energy_label : Label
 
 
 func _ready():
 	GlobalSignal.connect("asset_removed", self, "_update")
 	GlobalSignal.connect("asset_spawned", self, "_update")
 	
-	energy_value = get_parent().get_node("Energy").get_node("Energy Value")
-	amount = get_parent().get_node("Energy").get_node("Amount of Assets")
+	energy_value_label = Label.new()
+	assets_amount_label = Label.new()
+	target_energy_label = Label.new()
+	
+	_load_target_values()
+	_setup_gui()
 	
 	for child in get_children():
 		child.visible = false
 	
 	_update()
+	_update_target_value("")
 
 
 func _toggled(button_pressed) -> void:
@@ -38,5 +47,36 @@ func _toggled(button_pressed) -> void:
 # An update should be called whenever the value changes (new asset spawned, asset removed, etc.)
 func _update():
 	var asset_details = ServerConnection.get_json("/assetpos/energy_contribution/all.json")
-	energy_value.text = String(asset_details["total_energy_contribution"]) + " MW"
-	amount.text = "Total placed assets: " + String(asset_details["number_of_assets"])
+	energy_value_label.text = String(asset_details["total_energy_contribution"])
+	assets_amount_label.text = "Total placed assets: " + String(asset_details["number_of_assets"])
+
+
+# Changes the target value depending on the season
+func _update_target_value(season):
+	if season == "summer":
+		target_energy_label.text = String(target_energy_value["summer"])
+	elif season == "winter": 
+		target_energy_label.text = String(target_energy_value["winter"])
+	else:
+		target_energy_label.text = String(target_energy_value["general"])
+
+
+func _load_target_values():
+	target_energy_value["general"] = Session.get_current_scenario()["energy_requirement_total"]
+	target_energy_value["summer"] = Session.get_current_scenario()["energy_requirement_summer"]
+	target_energy_value["winter"] = Session.get_current_scenario()["energy_requirement_winter"]	
+
+
+func _setup_gui():
+	var container = HBoxContainer.new()
+	container.visible = true
+	var unit = Label.new()
+	
+	unit.text = "MW /"
+	
+	container.add_child(energy_value_label)
+	container.add_child(unit)
+	container.add_child(target_energy_label)
+	
+	get_parent().get_node("Energy").add_child(container)
+	get_parent().get_node("Energy").add_child(assets_amount_label)
