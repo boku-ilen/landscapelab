3d2faf316ab6234e25bc2dff50a27275a428a43a
karl <karl.bittner@hexaquo.at>
2019-09-01 20:22:55 +0200

    LargePVPlant builds itself using rows, cols params
    That way, we can easily modify how many individual PV Plants there are
    to achieve the desired size.

---
+++ Assets/PhotovoltaicPlant/LargePVPlant.gd
@@ -0,0 +1,37 @@
+tool
+extends MoveableObject
+
+# 
+# Automatically builds a large PV plant with a given number of rows and
+# columns of individual PV modules.
+# Note that the resulting rows and columns can be off by 1 to preserve
+# symmetry.
+# 
+
+export(int) var rows
+export(int) var cols
+
+export(float) var row_spacing
+export(float) var col_spacing
+
+var mesh = load("res://Assets/Util/PVMesh.tscn")
+
+onready var pickup_shape = get_node("PickupBody/CollisionShape")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Spawn rows * cols PV modules
+	# Add 1 to make sure we get an odd number - asymmetry otherwise
+	for row in range(-rows / 2, rows / 2 + 1):
+		for col in range(-cols / 2, cols / 2 + 1):
+			var new_scene = mesh.instance()
+			
+			new_scene.translation.x += col * col_spacing
+			new_scene.translation.z += row * row_spacing
+			
+			add_child(new_scene)
+	
+	# Change the size of the PickupBody accordingly
+	pickup_shape.shape.extents.x = cols / 2 * col_spacing
+	pickup_shape.shape.extents.z = rows / 2 * row_spacing
