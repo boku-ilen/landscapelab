a2fde38b89ccf8ced2de57fece6d0a4ecbea4413
retour <retour@boku.ac.at>
2019-09-19 11:33:39 +0200

    Fix Lego extent

--- Util/LegoExtentDrawer.gd
+++ Util/LegoExtentDrawer.gd
@@ -7,7 +7,7 @@ extends Spatial
 
 
 var HEIGHT = 20000  # TODO: Setting
-var SYMBOL_THICKNESS = 50  # TODO: Setting
+var SYMBOL_THICKNESS = 150  # TODO: Setting
 
 onready var lego_extent = get_node("LegoExtent") as MeshInstance
 
@@ -25,10 +25,11 @@ func _ready():
 # Set the extent of the mesh which displays the Lego extent.
 # Must receive engine coordinates.
 func set_mesh_extent(top_left: Vector3, bottom_right: Vector3):
-	var size = Vector3(top_left.x - bottom_right.x, HEIGHT, top_left.z - bottom_right.z)
+	var size = Vector3(abs(top_left.x - bottom_right.x), HEIGHT, abs(top_left.z - bottom_right.z))
 	
-	var center = top_left - (size / 2)
+	var center = bottom_right - (size / 2)
 	center.y = 0
+	center.z += size.z
 	
 	transform.origin = center
 	
@@ -41,9 +42,9 @@ func set_mesh_extent(top_left: Vector3, bottom_right: Vector3):
 	# Hollow out the lego extent symbol with another slightly smaller cube
 	# It would make more sense if we had to subtract SYMBOL_THICKNESS here, but I believe the size
 	#  is negative because of the format of the coordinates in the engine vs on the server
-	lego_extent_symbol_inside.width = size.x + SYMBOL_THICKNESS
+	lego_extent_symbol_inside.width = size.x - SYMBOL_THICKNESS
 	lego_extent_symbol_inside.height = size.y + 100
-	lego_extent_symbol_inside.depth = size.z + SYMBOL_THICKNESS
+	lego_extent_symbol_inside.depth = size.z - SYMBOL_THICKNESS
 
 
 # Move self accordingly when a world shift happens
