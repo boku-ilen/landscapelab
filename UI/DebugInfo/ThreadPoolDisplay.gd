c67dd1fbc40b987179997241b93e82c840cb62cf
retour <retour@boku.ac.at>
2019-09-22 00:09:09 +0200

    Add thread queue size to debug window
    This was useful for improving the thread numbers and priorities

---
+++ UI/DebugInfo/ThreadPoolDisplay.gd
@@ -0,0 +1,10 @@
+extends HBoxContainer
+
+onready var label = get_node("Data")
+
+
+func _process(delta):
+	label.text = ""
+	
+	for queue in ThreadPool.task_queues:
+		label.text += "%s: " % queue.get_size()
