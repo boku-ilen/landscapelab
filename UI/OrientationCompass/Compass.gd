f68402cd832c6a67a6cac59af83437a129793f86
retour <retour@boku.ac.at>
2019-09-26 17:32:19 +0200

    Fix compass for VR

--- UI/OrientationCompass/Compass.gd
+++ UI/OrientationCompass/Compass.gd
@@ -1,22 +1,19 @@
-extends Sprite3D
+extends Spatial
 
 #
 # A compass indicating the orientation of the firstPerson.
 # Rotates with the latest global player position from PlayerInfo.
 #
 
+onready var parent = get_parent()
 var direction : float
 
-
-func _ready():
-	PlayerInfo.connect("player_look_direction_changed", self, "_on_new_player_look_direction")
-
-
-func _on_new_player_look_direction(new_dir):
-	var player_look = Vector2(new_dir.x, new_dir.z)
+func _process(delta):
+	var player_look = Vector2(transform.basis.z.x, transform.basis.z.z)
 	
 	# look at https://stackoverflow.com/questions/14066933/direct-way-of-computing-clockwise-angle-between-2-vectors
 	# this link for further information, as the global forward is (0, -1) the result looks like:
 	direction = atan2(-player_look.x, -player_look.y)
 	
-	rotation.z = -direction #+ deg2rad(180.0)
+	global_transform = Transform.IDENTITY
+	global_transform.origin = parent.global_transform.origin
