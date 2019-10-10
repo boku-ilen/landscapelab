e49d851bdbd446eb9ac08b90726eba2a3ef9b40f
mathiasBaumgartinger <hiasbaum@gmail.com>
2019-10-10 12:36:22 +0200

    Updates and code clean up in the ui so the new font classes can be assigned properly. Also a translation system can be added way more easily now.

--- UI/ui_energy.gd
+++ UI/ui_energy.gd
@@ -6,38 +6,8 @@ extends TextureButton
 # details view
 #
 
-# Get the "Energy Value"- and "Amount"-label so it can be dynamically changed with update()
-var energy_value_label : Label = Label.new()
-var assets_amount_label : Label = Label.new()
-var target_energy_label : Label = Label.new()
 
-onready var energy_details = get_node("Panel")
-onready var requester = get_node("RegularServerRequest")
-onready var progress_bar = get_node("ProgressBar")
-
-
-func _ready():
-	# Make the the interval for the assets request in intervals of 2 seconds
-	requester.interval = 2 
-	requester.set_request("/energy/contribution/" + String(Session.scenario_id) + "/all.json")
-	requester.connect("new_response", self, "_on_new_response", [], CONNECT_DEFERRED)
-	
-	progress_bar.max_value = Session.get_current_scenario()["energy_requirement_total"]
-	
-	_setup_gui()
-	
-	energy_details.visible = false
-
-
-func _on_new_response(response):
-	var asset_details = response
-	
-	progress_bar.value = float(asset_details["total_energy_contribution"])
-	
-	if not asset_details == null:
-		energy_value_label.text = String(int(round(asset_details["total_energy_contribution"])))
-		assets_amount_label.text = "Anzahl aller Anlagen: " + String(asset_details["number_of_assets"])
-
+onready var energy_details = get_node("EnergyDetailsPanel")
 
 
 func _toggled(button_pressed) -> void:
@@ -48,20 +18,3 @@ func _toggled(button_pressed) -> void:
 		GlobalSignal.emit_signal("energy_details_enabled")
 	else:
 		GlobalSignal.emit_signal("energy_details_disabled")
-
-
-func _setup_gui():
-	var container = HBoxContainer.new()
-	container.visible = true
-	var unit = Label.new()
-	
-	unit.text = "MWh/a von"
-	
-	target_energy_label.text = String(Session.get_current_scenario()["energy_requirement_total"]) + " MWh/a"
-	
-	container.add_child(energy_value_label)
-	container.add_child(unit)
-	container.add_child(target_energy_label)
-	
-	get_parent().get_node("Energy Panel").get_node("Energy").add_child(container)
-	get_parent().get_node("Energy Panel").get_node("Energy").add_child(assets_amount_label)
