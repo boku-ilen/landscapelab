ef5cd89cabbd077eea03e19d70fb0eccf251d9d5
mathias <hiasbaum@gmail.com>
2019-11-21 17:02:41 +0100

    In the inspector the new linearCSGMesh in the Imaging/DrawPath does what it should: Visualize the drawn Path (like a texture.)
    I cannot quite figure out, why it does not work ingame, maybe someone could have a look at this?

--- Imaging/DrawPath/DrawPath.gd
+++ Imaging/DrawPath/DrawPath.gd
@@ -6,27 +6,35 @@ extends Spatial
 # "imaging_set_focus" a path and a focussed point can be set.
 #
 
-export var path_texture: CurveTexture
+export var height_above_ground: float
+export var path_mesh_size: float
 
 onready var focus: Spatial = get_node("Focus")
 onready var path: Path = get_node("Path")
+onready var path_mesh: LinearCSGPolygon = get_node("Path/PathFollow/PathTexture")
 onready var cursor: RayCast = get_parent().get_node("InteractRay")
 
 var currently_imaging: bool = false
+var height_correction: Vector3
 
 
 func _ready():
-	path_texture.set_width(5)
-	#path_texture.set_curve(path.curve)
+	path_mesh.set_width(path_mesh_size)
+	path_mesh.set_height(path_mesh_size)
+	height_correction = Vector3(0, height_above_ground, 0)
+
 
 func _input(event):
 	if event.is_action_pressed("imaging"):
 		_switch_imaging_mode()
 	elif currently_imaging:
+		
 		if event.is_action_pressed("imaging_set_path"):
-			_add_path_point(WorldPosition.get_position_on_ground(cursor.get_collision_point()))
+			var position = WorldPosition.get_position_on_ground(cursor.get_collision_point())
+			_add_path_point(position)
 		elif event.is_action_pressed("imaging_set_focus"):
-			_set_focus_position(WorldPosition.get_position_on_ground(cursor.get_collision_point()))
+			var position = WorldPosition.get_position_on_ground(cursor.get_collision_point())
+			_set_focus_position(position)
 
 
 func _switch_imaging_mode():
@@ -34,8 +42,8 @@ func _switch_imaging_mode():
 
 
 func _add_path_point(position):
-	path.curve.add_point(position)
+	path.curve.add_point(position + height_correction)
 
 
 func _set_focus_position(position):
-	focus.global_transform.origin = position
+	focus.global_transform.origin = (position + height_correction)
