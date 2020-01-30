8c98718b6031119c368b67cbc4de37038a75d8ae
mathiasBaumgartinger <hiasbaum@gmail.com>
2020-01-30 11:53:29 +0100

    Added and removed on client only if the server request was sucessful.

--- UI/PointsOfInterest/PoI.gd
+++ UI/PointsOfInterest/PoI.gd
@@ -22,21 +22,24 @@ func _on_Save_pressed():
 	# Create an array for the locations data (only contains "x" and "z"-axis)
 	var fixed_pos = [PlayerInfo.get_true_player_position()[0], PlayerInfo.get_true_player_position()[2]]
 	
-	item_list.add_item(input_field.text)
-	# The item will be added as the last element in the list
-	item_list.set_item_metadata(item_list.get_item_count() - 1, fixed_pos)
-	
 	# As the coordinates on the server are responded in a different type,
 	# we have to use a "-" on the x-axis to properly save it
 	var result = ServerConnection.get_json("/location/create/%s/%d.0/%d.0/%d" % [input_field.text, -fixed_pos[0], fixed_pos[1], Session.scenario_id], false)
-	var test = 10
+	
+	# Only store on client if it was also successfully stored on server
+	if result.creation_success:
+		item_list.add_item(input_field.text)
+		# The item will be added as the last element in the list
+		item_list.set_item_metadata(item_list.get_item_count() - 1, fixed_pos)
 
 
 func _on_Delete_pressed():
 	# select mode is set to single, so only one item can be selected
-	var current_item = item_list.get_selected_items()[0]
-	var item_text = item_list.get_item_text(current_item)
-	item_list.remove_item(current_item)
+	var current_item : int = item_list.get_selected_items()[0]
+	var item_text : String = item_list.get_item_text(current_item)
 	
 	var result = ServerConnection.get_json("/location/remove/%s/%d" % [item_text, Session.scenario_id], false)
-	var test = 10
+	
+	# Only store on client if it was also successfully stored on server
+	if result.removal_success:
+		item_list.remove_item(current_item)
