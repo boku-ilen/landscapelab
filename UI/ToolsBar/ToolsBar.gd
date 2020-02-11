8dfb01e88e23f5db4fd56348d832725e9d803735
retour <retour@boku.ac.at>
2020-02-11 12:51:14 +0100

    Hoverable toolsbar for cleaner GUI usage.

---
+++ UI/ToolsBar/ToolsBar.gd
@@ -0,0 +1,28 @@
+extends HBoxContainer
+
+#
+# A hover menu for all the tools that are to follow so the GUI does not look 
+# as overloaded as it currently does. The mouse property of children has to be 
+# set to pass in order for it to function properly.
+#
+
+
+onready var tween = get_node("Tween")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("mouse_exited", self, "_on_mouse_exited")
+	connect("mouse_entered", self, "_on_mouse_entered")
+
+
+func _on_mouse_entered():
+	for child in get_children():
+		child.visible = true
+	
+
+
+func _on_mouse_exited():
+	for child in get_children():
+		if child.name != "Hoverable" and not child.pressed:
+			child.visible = false 
