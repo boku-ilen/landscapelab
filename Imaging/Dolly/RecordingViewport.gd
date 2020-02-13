5d942801673a44074d0398a9044f6a555c23a228
karl <karl.bittner@hexaquo.at>
2020-02-13 13:47:41 +0100

    Make RecordingViewport code a bit more modular

--- Imaging/Dolly/RecordingViewport.gd
+++ Imaging/Dolly/RecordingViewport.gd
@@ -24,16 +24,22 @@ func _input(event: InputEvent) -> void:
 			timer.stop()
 
 
-func make_screenshot():
+func _get_screenshot_from_viewport():
 	# Retrieve the captured image
 	var img = get_texture().get_data()
   
 	# Flip it on the y-axis (because it's flipped)
 	img.flip_y()
 	
+	return img
+	
+
+
+func make_screenshot():
+	var img = _get_screenshot_from_viewport()
+	
 	# Save to a file, use the current time for naming
-	var timestamp = OS.get_datetime()
-	var screenshot_filename = "user://videoframe-%d-%d.png" % [Session.session_id, OS.get_system_time_msecs()]
+	var screenshot_filename = _get_screenshot_filename()
 	
 	# Medium to low priority - we do want it to save sometime soon, but doesn't have to be immediate
 	ThreadPool.enqueue_task(ThreadPool.Task.new(self, "save_screenshot", [img, screenshot_filename]), 15)
@@ -43,3 +49,7 @@ func make_screenshot():
 func save_screenshot(img_filename_array):
 	img_filename_array[0].save_png(img_filename_array[1])
 	logger.info("captured screenshot in %s " % [img_filename_array[1]])
+
+
+func _get_screenshot_filename():
+	return "user://videoframe-%d-%d.png" % [Session.session_id, OS.get_system_time_msecs()]
