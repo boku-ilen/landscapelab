356a9dfe55c95e8c7cf9e6b42c571763964f4a86
retour <retour@boku.ac.at>
2020-02-13 16:25:10 +0100

    Dolly cam interpolation works but as of now it causes stackoverflows because of a lot of recursion.

--- Imaging/DrawPath/Path/Path.gd
+++ Imaging/DrawPath/Path/Path.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends "res://Util/LinearDrawer/InterpolateLinear.gd"
 
 
 # As we do not want to have the line directly on the ground there will be a placeholder value
@@ -7,7 +7,7 @@ export var height_above_ground: float
 onready var focus: Spatial = get_node("Focus")
 onready var path: Path = get_node("Path")
 
-var height_correction = Vector3(0, height_above_ground, 0)
+onready var height_correction = Vector3(0, height_above_ground, 0)
 
 
 func _ready():
@@ -22,7 +22,18 @@ func on_shift_world(delta_x, delta_z):
 
 
 func _add_path_point(position):
-	path.curve.add_point(position + height_correction)
+	var all_points = path.curve.get_baked_points()
+	
+	if not all_points.empty():
+		var last_point = path.curve.get_point_position(path.curve.get_point_count() - 1)
+		
+		var possible_interpolated: Array = interpolate_points(last_point, position + height_correction)
+		for point in possible_interpolated:
+			point = WorldPosition.get_position_on_ground(point) + height_correction
+			
+			path.curve.add_point(point)
+	else:
+		path.curve.add_point(position + height_correction)
 
 
 func _set_focus_position(position):
