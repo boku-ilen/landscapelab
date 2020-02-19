1730e0926b68981bec17e488127e065f72e0fce2
retour <retour@boku.ac.at>
2020-02-19 15:20:32 +0100

    Added a JsonParser class. Changed the values-settings to general game-mode-settings and applied settings for tools.

---
+++ Global/GameModeLoader.gd
@@ -0,0 +1,25 @@
+extends JSONParser
+
+
+var _settings: JSONParseResult = _parse_json("res://game-mode-settings.json")
+var _values_settings = _settings["values"]
+var _tool_settings = _settings["tools"]
+
+
+func get_all_values_for_mode(mode: int):
+	var all_values_for_mode: Dictionary
+	for value in _values_settings["modules"]:
+		if _values_settings["modules"][value]["modes"].has(float(mode)):
+			all_values_for_mode[value] = String(_values_settings["path-prefix"] +
+				 "/" + _values_settings["modules"][value]["path"])
+	
+	return all_values_for_mode
+
+
+func get_all_tools_for_mode(mode: int):
+	var all_tools_for_mode = []
+	for _tool in _tool_settings:
+		if _tool_settings[_tool]["modes"].has(float(mode)):
+			all_tools_for_mode.append(_tool)
+	
+	return all_tools_for_mode
