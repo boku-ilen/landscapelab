77270fce5e59a3935c76da2e2f0f81bc16cc94f6
retour <retour@boku.ac.at>
2020-02-20 15:25:23 +0100

    Lego mode is enabled by default.

--- UI/Settings/Settings.gd
+++ UI/Settings/Settings.gd
@@ -9,9 +9,6 @@ onready var lego = get_node("Panel/ScrollContainer/VBoxContainer/Lego")
 func _ready():
 	lego.get_node("EnableLego").connect("toggled", self, "_on_lego")
 	connect("pressed", self, "_on_Settings_pressed")
-	
-	# Lego is enabled by default, we have to emit the signal on_ready
-	GlobalSignal.emit_signal("sync_moving_assets")
 
 
 func _on_Settings_pressed():
--- World/MainScene/MainScene.gd
+++ World/MainScene/MainScene.gd
@@ -2,4 +2,6 @@ extends Spatial
 
 
 func _ready():
-	pass
+	# Lego is enabled by default, we have to emit the signal on_ready
+	# as nodes get built inside-out we need to do it on top of the scene-tree.
+	GlobalSignal.emit_signal("sync_moving_assets")
