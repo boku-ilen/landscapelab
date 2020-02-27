34e8fea9b0293391f353477d286bc85c1a522ad8
retour <retour@boku.ac.at>
2020-02-27 16:04:20 +0100

    Added boolean so the check of the world translation can be turned off for testing in the test.tscn.

--- Perspectives/VR/ARVROrigin.gd
+++ Perspectives/VR/ARVROrigin.gd
@@ -1,7 +1,10 @@
 extends ARVROrigin
 
+export(bool) var testing = false
+
 func _process(delta):
 	# Keep the VR player at the global player position
-	translation = WorldPosition.get_position_on_ground(PlayerInfo.get_engine_player_position())
+	if not testing:
+		translation = WorldPosition.get_position_on_ground(PlayerInfo.get_engine_player_position())
 	
 	PlayerInfo.update_player_look_direction(-(get_node("SettingsARVRCamera").global_transform.basis.z))
