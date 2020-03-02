554a7f24d6ad9463be3ef548afdd818ac6b7b300
retour <retour@boku.ac.at>
2020-03-02 17:33:55 +0100

    We can now easily project a ui-element onto a mesh which could be helpful for all kinds of vr-interfaces, etc..

--- VR/GuiToMesh.gd
+++ VR/GuiToMesh.gd
@@ -8,5 +8,7 @@ onready var viewport = get_node("Viewport")
 
 func _ready():
 	viewport.add_child(viewport_texture.instance())
-	#viewport.call_deferred("add_child", viewport_texture.instance())  
-	pass
+	var material = SpatialMaterial.new()
+	var viewport_img = viewport.get_texture()
+	material.albedo_texture = viewport_img
+	set_surface_material(0, material)
