9dfde9b55a48ffc7fdefcc3d8cc5fd9938e814ce
kb173 <karl.bittner@hexaquo.at>
2020-03-02 16:54:46 +0100

    Remove unused script

--- Perspectives/VR/ARVRCamera.gd
+++
@@ -1,9 +0,0 @@
-extends ARVRCamera
-
-
-func get_look_direction():
-	return -global_transform.basis.z
-
-
-func _physics_process(delta):
-	PlayerInfo.update_player_look_direction(get_look_direction())
