a2f6b14183dca01374d9d541673378d4b31669ee
kb173 <karl.bittner@hexaquo.at>
2020-03-03 14:51:35 +0100

    Connect new ParentTracker (VR tracking) to the Session system

--- Perspectives/VR/ParentTracker.gd
+++ Perspectives/VR/ParentTracker.gd
@@ -11,11 +11,30 @@ var file = File.new()
 
 
 func _ready():
-	# TODO: Do this when a new tracking session starts and add the session_id
-	#  to the filename
-	open_tracking_file("user://tracking1.csv")
+	# Connect these signals in CONNECT_DEFERRED so that we have the new session_id by then
+	GlobalSignal.connect("tracking_start", self, "start_tracking", [], CONNECT_DEFERRED)
+	GlobalSignal.connect("tracking_pause", self, "toggle_pause_tracking", [], CONNECT_DEFERRED)
+	GlobalSignal.connect("tracking_stop", self, "stop_tracking", [], CONNECT_DEFERRED)
+
+
+# Start saving data with the current Session id
+func start_tracking():
+	var filename = "user://tracking%d.csv" % [Session.session_id]
 	
-	tracking_timer.connect("timeout", self, "write_to_tracking_file")
+	open_tracking_file(filename)
+
+
+# Start or stop tracking depending on the Session id
+func toggle_pause_tracking():
+	if Session.session_id > 0:
+		start_tracking()
+	else:
+		close_tracking_file()
+
+
+# Stop saving data
+func stop_tracking():
+	close_tracking_file()
 
 
 func get_look_direction():
@@ -26,6 +45,7 @@ func get_position():
 	return get_parent().global_transform.origin
 
 
+# Prepare for tracking: Open / create the CSV file and connect the timer to it
 func open_tracking_file(filename):
 	if file.open(filename, File.WRITE) != 0:
 		logger.error("Couldn't open tracking file!")
@@ -44,13 +64,24 @@ func open_tracking_file(filename):
 	line.append("Roll")
 	
 	file.store_csv_line(line)
+	
+	tracking_timer.connect("timeout", self, "write_to_tracking_file")
 
 
+# Close the CSV file and stop saving data into it
+func close_tracking_file():
+	file.close()
+	
+	tracking_timer.disconnect("timeout", self, "write_to_tracking_file")
+
+
+# Write a line of tracking data into the CSV
 func write_to_tracking_file():
 	var time = str(OS.get_system_time_msecs())
-	var line = PoolStringArray()
 	var pos = get_position()
 	var look = get_look_direction()
+	
+	var line = PoolStringArray()
 	
 	line.append(time)
 	
