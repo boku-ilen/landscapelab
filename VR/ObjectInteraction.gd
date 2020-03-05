a6f4f5b143af125b65a28790a23a38e3ef3f6533
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-03-05 21:30:38 +0100

    I general for the functionality of InteractableObjects has been created.

--- VR/ObjectInteraction.gd
+++ VR/ObjectInteraction.gd
@@ -6,12 +6,14 @@ var current_object = null
 
 
 func _process(delta):
+	# Side grip and front shoulder button
 	if controller.is_button_pressed(2) and controller.is_button_pressed(15):
 		if not area.get_overlapping_bodies() == null:
 			for body in area.get_overlapping_bodies():
 				if body.is_in_group("Interactable"):
+					body.picked_up(controller)
 					current_object = body
 					break
 	
 	if current_object:
-		current_object.global_transform.origin = global_transform.origin
+		current_object.global_transform = global_transform
