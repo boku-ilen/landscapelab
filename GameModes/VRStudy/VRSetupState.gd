aaf5e82520b630d23e718bbf52c124a3f0f60c7f
Karl Bittner <karl.bittner@outlook.com>
2020-03-18 16:21:38 +0100

    Terrain is hidden during VR setup

--- GameModes/VRStudy/VRSetupState.gd
+++ GameModes/VRStudy/VRSetupState.gd
@@ -2,4 +2,10 @@ extends "res://addons/gameflow/GameState.gd"
 
 
 func _ready() -> void:
-	get_node("Timer").connect("timeout", self, "emit_completed")
+	GlobalTerrain.hide_terrain()
+	get_node("Timer").connect("timeout", self, "setup_done")
+
+
+func setup_done():
+	GlobalTerrain.show_terrain()
+	emit_completed()
