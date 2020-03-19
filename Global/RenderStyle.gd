1f9bfa7bfc7e46016cb0cf3867e6c5bb084cc14b
Karl Bittner <karl.bittner@outlook.com>
2020-03-19 16:11:41 +0100

    Proof of concept RenderStyle selection + signal emission

--- Global/RenderStyle.gd
+++ Global/RenderStyle.gd
@@ -1,10 +1,26 @@
 extends Node
 
 
-enum STYLE {
-	REALISTIC,
-	ABSTRACT
-}
+var styles = [
+	Style.new("Realistic"),
+	Style.new("Abstract")
+]
 
 
 signal change_style(style_id)
+
+
+func _ready():
+	connect("change_style", self, "_on_change_style")
+
+
+func _on_change_style(id: int):
+	# TODO: This is just a proof of concept of the functionality. Will be removed
+	print(styles[id].name)
+
+
+class Style:
+	var name
+	
+	func _init(name: String):
+		self.name = name
--- UI/RenderingStyle/RenderingStyleSelect.gd
+++ UI/RenderingStyle/RenderingStyleSelect.gd
@@ -2,5 +2,14 @@ extends ItemList
 
 
 func _ready() -> void:
-	add_item("Realistic")
-	add_item("Abstract")
+	# Add all styles from the RenderStyle list.
+	# Because the order of adding is the same as the order in RenderStyle.styles,
+	#  these item IDs are the same as the IDs in the RenderStyle list.
+	for style in RenderStyle.styles:
+		add_item(style.name)
+	
+	connect("item_activated", self, "_on_item_activated")
+
+
+func _on_item_activated(id: int):
+	RenderStyle.emit_signal("change_style", id)
