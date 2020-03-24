995872bbc0f9c7bdf2e1524e00bd43809c8a417c
Karl Bittner <karl.bittner@outlook.com>
2020-03-24 16:09:26 +0100

    UI Performance Debug Overlay can be toggled with F3

--- UI/DebugInfo/DebugOverlay.gd
+++ UI/DebugInfo/DebugOverlay.gd
@@ -6,6 +6,11 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_debug"):
+		visible = !visible
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	text = """
