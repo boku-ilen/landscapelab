13670122e0182b21cb94eef6fcd99acaf342b858
Karl Bittner <karl.bittner@outlook.com>
2020-03-30 21:56:45 +0200

    Add ability to get paths with ending in GeodataPaths

--- Global/GeodataPaths.gd
+++ Global/GeodataPaths.gd
@@ -12,9 +12,17 @@ func get_relative(geodata_name: String) -> String:
 	return _paths[geodata_name]["name"]
 
 
+func get_relative_with_ending(geodata_name: String) -> String:
+	return get_relative(geodata_name) + "." + get_type(geodata_name)
+
+
 func get_type(geodata_name: String) -> String:
 	return _paths[geodata_name]["type"]
 
 
 func get_absolute(geodata_name: String) -> String:
 	return get_base().plus_file(get_relative(geodata_name))
+
+
+func get_absolute_with_ending(geodata_name: String) -> String:
+	return get_base().plus_file(get_relative_with_ending(geodata_name))
