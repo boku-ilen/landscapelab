b001924c1d5322099867d09162bc8d582d5ce1f5
Karl Bittner <karl.bittner@outlook.com>
2020-03-31 18:04:18 +0200

    Load line data in a thread
    I think it makes sense to avoid filling the ThreadPool with regular requests with relatively long wait time like this; especially since the Node exists from the beginning, so the cost of Thread.new is not an issue.

--- World/AssetHandler/ShapefileToPathsNearPlayer.gd
+++ World/AssetHandler/ShapefileToPathsNearPlayer.gd
@@ -11,12 +11,14 @@ export(PackedScene) var path_scene
 
 var shapefile_path = GeodataPaths.get_absolute_with_ending("infrastructure")
 var radius = 500
-var max_lines = 20
+var max_lines = 100
+
+var load_thread: Thread = Thread.new()
 
 
 func _get_line_array():
 	var player_pos = PlayerInfo.get_true_player_position()
-	
+	print(-player_pos[0], " ", player_pos[2])
 	return Geodot.get_lines(shapefile_path, -player_pos[0], player_pos[2], radius, max_lines)
 
 
@@ -25,20 +27,37 @@ func _ready() -> void:
 	get_node("Timer").connect("timeout", self, "_on_timeout")
 
 
-func _on_timeout():
+func _get_all_lines(data):
 	var lines = _get_line_array()
+	var root = Spatial.new()
 	
 	for line in lines:
-		spawn_path(line)
+		root.add_child(_get_path_node_for_line(line))
+	
+	call_deferred("_lines_received")
+	return root
 
 
-func spawn_path(line):
+func _lines_received():
+	var line_root = load_thread.wait_to_finish()
+	line_root.name = "Lines"
+	
+	if has_node("Lines"):
+		get_node("Lines").free()
+	
+	add_child(line_root)
+
+
+func _on_timeout():
+	if not load_thread.is_active():
+		load_thread.start(self, "_get_all_lines")
+
+
+func _get_path_node_for_line(line):
 	var curve = line.get_offset_curve3d(Offset.x, 0, -Offset.z) as Curve3D
 	var drawer = path_scene.instance() as LinearDrawer
-	
-	print(curve.get_baked_points()[4])
 	
 	drawer.set_curve(curve)
 	drawer.set_width(float(line.get_attribute("WIDTH")))
 	
-	add_child(drawer)
+	return drawer
