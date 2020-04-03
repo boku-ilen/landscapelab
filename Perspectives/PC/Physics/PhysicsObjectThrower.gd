da5f9e2952f0af465c54453771e73c5ce4ad8281
Karl Bittner <karl.bittner@outlook.com>
2020-04-03 12:08:54 +0200

    Throw physical balls in 3rd person for testing
    This allows us to debug physics - pressing "B" will throw a ball into the cursor direction in 3rd person mode. The ball is physically simulated and interacts will all colliders.

---
+++ Perspectives/PC/Physics/PhysicsObjectThrower.gd
@@ -0,0 +1,26 @@
+extends Spatial
+
+
+onready var origin: Spatial = get_parent()
+onready var cursor: RayCast = get_parent().get_node("InteractRay")
+onready var thrown_objects = get_node("ThrownObjects")
+
+export(PackedScene) var object_to_throw_scene
+
+
+func _ready():
+	UISignal.connect("imaging", self, "_imaging")
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("throw_physics_ball"):
+		var direction = cursor.cast_to.normalized()
+		var instance_position = origin.global_transform.origin
+		
+		var instance = object_to_throw_scene.instance() as RigidBody
+		thrown_objects.add_child(instance)
+		
+		instance.global_transform.origin = instance_position + direction * 20.0
+		instance.apply_central_impulse(direction * 10000.0)
+		
+		print(instance.global_transform.origin)
