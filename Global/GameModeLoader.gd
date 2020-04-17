acdd46c74644d1bc9934a1d1d7880ef802c0327b
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-04-17 22:36:27 +0200

    Madee it possible to load tools for a game mode from setting.

--- Global/GameModeLoader.gd
+++ Global/GameModeLoader.gd
@@ -44,3 +44,40 @@ func get_all_tools_for_mode(mode: int):
 
 func get_startup_mode():
 	return _settings["game-mode"]
+
+
+func get_vr_settings_for_mode(my_mode: String):
+	for mode in _modes_json:
+		if mode["name"] == my_mode:
+			return mode["vr"]
+
+
+func get_base_native(mode: String):
+	return get_vr_settings_for_mode(mode)["native-base-dir"]
+
+
+func get_base_landscape(mode: String):
+	return get_vr_settings_for_mode(mode)["landscape-base-dir"]
+
+
+func get_vr_tools(mode, side) -> Array:
+	var tools = []
+	for _tool in get_vr_settings_for_mode(mode)[side]["landscape-tools"]:
+		tools.append(get_base_landscape(mode).plus_file(_tool))
+	for _tool in get_vr_settings_for_mode(mode)[side]["native-tools"]:
+		tools.append(get_base_native(mode).plus_file(_tool))
+	
+	return tools
+
+
+func get_vr_show_meshes(mode: String, side: String) -> Dictionary:
+	var show_hand = get_vr_settings_for_mode(mode)[side]["hand-mesh"]
+	var show_controller = get_vr_settings_for_mode(mode)[side]["controller-mesh"]
+	
+	var gui_finger 
+	if show_hand:
+		gui_finger = get_vr_settings_for_mode(mode)[side]["gui-finger"]
+	else:
+		gui_finger = false
+	
+	return {"hand": show_hand, "controller": show_controller, "gui_finger": gui_finger}
