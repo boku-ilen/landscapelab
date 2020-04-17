cd52ce33f1b364cccccdb45125787fb3cd1584e4
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-04-17 22:36:12 +0200

    Changed Game modes accordingly.

--- GameModes/VRStudy/VRSetupState.gd
+++ GameModes/VRStudy/VRSetupState.gd
@@ -3,9 +3,8 @@ extends "res://addons/gameflow/GameState.gd"
 
 func _ready() -> void:
 	GlobalTerrain.hide_terrain()
-	get_node("Timer").connect("timeout", self, "setup_done")
+	get_node("GuiToMesh").viewport_texture.get_node("Button").connect("pressed", self, "setup_done")
 
 
 func setup_done():
 	GlobalTerrain.show_terrain()
-	emit_completed()
