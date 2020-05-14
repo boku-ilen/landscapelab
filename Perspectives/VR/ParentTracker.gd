8085f8759d0564050246d861689087255aa26e17
karl <karl.bittner@hexaquo.at>
2020-05-14 15:39:16 +0200

    Unify tracking, remove old code

--- Perspectives/VR/ParentTracker.gd
+++ Perspectives/VR/ParentTracker.gd
@@ -8,7 +8,8 @@ extends Node
 var version = Settings.get_setting("meta", "version")
 var usage = Settings.get_setting("meta", "usage")
 
-onready var tracking_timer = get_node("TrackingTimer")
+onready var position_tracking_timer = get_node("PositionTrackingTimer")
+onready var screenshot_timer = get_node("ScreenshotTimer")
 
 var file = File.new()
 
@@ -30,6 +31,8 @@ func start_tracking(additional_flag: String = ""):
 		filename = "user://tracking-%s-%s-session%d-%s.csv" % [usage, version, Session.session_id, additional_flag]
 	
 	open_tracking_file(filename)
+	
+	screenshot_timer.connect("timeout", self, "take_screenshot")
 
 
 # Start or stop tracking depending on the Session id
@@ -37,12 +40,13 @@ func toggle_pause_tracking():
 	if Session.session_id > 0:
 		start_tracking()
 	else:
-		close_tracking_file()
+		stop_tracking()
 
 
 # Stop saving data
 func stop_tracking():
 	close_tracking_file()
+	screenshot_timer.disconnect("timeout", self, "take_screenshot")
 
 
 func get_look_direction():
@@ -73,14 +77,14 @@ func open_tracking_file(filename):
 	
 	file.store_csv_line(line)
 	
-	tracking_timer.connect("timeout", self, "write_to_tracking_file")
+	position_tracking_timer.connect("timeout", self, "write_to_tracking_file")
 
 
 # Close the CSV file and stop saving data into it
 func close_tracking_file():
 	file.close()
 	
-	tracking_timer.disconnect("timeout", self, "write_to_tracking_file")
+	position_tracking_timer.disconnect("timeout", self, "write_to_tracking_file")
 
 
 # Write a line of tracking data into the CSV
@@ -102,3 +106,26 @@ func write_to_tracking_file():
 	line.append(rad2deg(look.z))
 	
 	file.store_csv_line(line)
+
+
+func take_screenshot():
+	# Retrieve the captured image
+	var img = get_viewport().get_texture().get_data()
+  
+	# Flip it on the y-axis (because it's flipped)
+	img.flip_y()
+	
+	# Save to a file, use the current time for naming
+	var timestamp = OS.get_datetime()
+	var screenshot_filename = "user://screenshot-%d%d%d-%d%d%d-%d.png" % [timestamp["year"], timestamp["month"],
+	 timestamp["day"], timestamp["hour"], timestamp["minute"], timestamp["second"], Session.session_id]
+	
+	# Medium to low priority - we do want it to save sometime soon, but doesn't have to be immediate
+	ThreadPool.enqueue_task(ThreadPool.Task.new(self, "_save_screenshot", [img, screenshot_filename]), 15)
+
+
+# Actually save a screenshot - to be run in a thread
+func _save_screenshot(img_filename_array):
+	img_filename_array[0].save_png(img_filename_array[1])
+	logger.info("captured screenshot in %s " % [img_filename_array[1]])
+	
