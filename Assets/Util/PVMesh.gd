d804a06a79322cdece70469a65d3d2f9b68fcd23
karl <karl.bittner@hexaquo.at>
2020-05-14 15:40:18 +0200

    Use ground normal to align PV

--- Assets/Util/PVMesh.gd
+++ Assets/Util/PVMesh.gd
@@ -1,1 +1,13 @@
 extends GroundedSpatial
+
+func _place_on_ground():
+	._place_on_ground()
+	
+	var normal_on_ground = WorldPosition.get_normal_on_ground(global_transform.origin)
+	normal_on_ground.z = 0
+	
+	var angle = normal_on_ground.angle_to(Vector3.UP)
+	if normal_on_ground.x > 0:
+		angle = -angle
+	
+	rotation.z = angle
