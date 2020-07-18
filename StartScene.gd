8670e5b1e10ae2da1c13b9d5657fab672f70a80e
chrisgraf <christoph.graf@boku.ac.at>
2020-07-18 14:12:33 +0200

    EXPERIMENTAL: moved startscene into mainscene EXPERIMENTAL: 'fixed' moving toolbars by disabling movement (we need to investigate this further and write a fix that works with any resolution and setup)

---
+++ StartScene.gd
@@ -0,0 +1,18 @@
+extends TextureButton
+
+var _timer = null
+
+# TODO: this button is currently replacing a proper vanishing
+# TODO: of the welcome screen after a proper loading 
+func _ready():
+	_timer = Timer.new()
+	add_child(_timer)
+
+	_timer.connect("timeout", self, "_on_Timer_timeout")
+	_timer.set_wait_time(2.5)
+	_timer.set_one_shot(true) # Make sure it loops
+	_timer.start()
+
+
+func _on_Timer_timeout():
+	self.get_parent().visible = false
