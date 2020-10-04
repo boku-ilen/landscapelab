171925e8d01a8d87a040e9b005489bd0e27410f1
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-10-04 21:40:48 +0200

    Remove layer function added.

--- Layers/Singletons/Layers.gd
+++ Layers/Singletons/Layers.gd
@@ -6,6 +6,9 @@ var layers: Dictionary
 signal new_rendered_layer(layer)
 signal new_scored_layer(layer)
 signal new_layer(layer)
+signal removed_rendered_layer(layer_name)
+signal removed_scored_layer(layer_name)
+signal removed_layer(layer_name)
 
 
 func get_layer(name: String):
@@ -31,6 +34,17 @@ func add_layer(layer: Layer):
 		emit_signal("new_rendered_layer", layer)
 	
 	emit_signal("new_layer", layer)
+
+
+func remove_layer(layer_name: String):
+	if layers[layer_name].is_scored:
+		emit_signal("removed_scored_layer", layer_name)
+	if is_layer_rendered(layers[layer_name]):
+		emit_signal("removed_rendered_layer", layer_name)
+	
+	emit_signal("removed_layer", layer_name)
+	
+	layers.erase(layer_name)
 
 
 func is_layer_rendered(layer: Layer):
