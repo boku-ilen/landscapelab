b5b229471c4a5d96f4c31c6db222be1cf593c1dd
Mathias Baumgartinger <hiasbaum@gmail.com>
2020-10-04 11:04:02 +0200

    A simple scene that makes it possible to search for a file in one's file-system that also shows the path in a text-edit.

---
+++ Util/FileChooser.gd
@@ -0,0 +1,26 @@
+extends HBoxContainer
+
+
+export var filters: PoolStringArray = ["*.shp", "*.gpkg"]
+
+onready var button = get_node("Button")
+onready var file_dialog = get_node("Button/FileDialog")
+onready var file_name = get_node("FileName")
+
+
+func _ready():
+	file_dialog.filters = filters
+	button.connect("pressed", self, "_pop_file_dialog")
+	file_dialog.connect("file_selected", self, "_file_selected")
+	file_name.connect("text_changed", self, "_check_path")
+
+
+func _pop_file_dialog():
+	file_dialog.popup(Rect2(button.rect_global_position, Vector2(500, 400)))
+
+
+func _file_selected(which: String):
+	file_name.set_text(which)
+
+
+
