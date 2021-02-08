6e23f19b962e0e05ad600ecf1c1b1eb5ca495b68
karl <karl.bittner@hexaquo.at>
2021-02-08 20:25:40 +0100

    Make Group Plant selection show up in Plant Details

--- Vegetation/Editor/VegetationEditor.gd
+++ Vegetation/Editor/VegetationEditor.gd
@@ -10,6 +10,7 @@ extends HSplitContainer
 func _ready():
 	$PlantPanel/PlantDetails/DetailList/GroupButton.connect("pressed", self, "_add_current_plant_to_current_group")
 	$HSplitContainer/VBoxContainer/UpdateVisButton.connect("pressed", self, "_update_visualization")
+	$HSplitContainer/GroupPanel/GroupDetails/GroupAttributes/Attributes/GroupPlantList.connect("item_selected", self, "_on_group_plant_selected")
 
 
 func _add_current_plant_to_current_group():
@@ -25,3 +26,9 @@ func _update_visualization():
 	var current_group = $HSplitContainer/GroupPanel/GroupDetails.current_group
 	
 	$HSplitContainer/VBoxContainer/ViewportContainer/Viewport/Visualization.update_visualization(current_group.id)
+
+
+
+func _on_group_plant_selected(selected_id: int):
+	var plant = $PlantPanel/PlantList.get_item_metadata(selected_id)
+	$PlantPanel/PlantDetails.set_plant(plant)
