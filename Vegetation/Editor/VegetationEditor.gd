f89b766a4a62f239fec923906924b343568e8d9c
karl <karl.bittner@hexaquo.at>
2021-02-10 17:57:38 +0100

    Fix bug with group plant selection
    The wrong plant was used in the PlantDetails when a plant of the current group was selected.

--- Vegetation/Editor/VegetationEditor.gd
+++ Vegetation/Editor/VegetationEditor.gd
@@ -1,12 +1,6 @@
 extends HSplitContainer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$PlantPanel/PlantDetails/DetailList/GroupButton.connect("pressed", self, "_add_current_plant_to_current_group")
 	$HSplitContainer/VBoxContainer/UpdateVisButton.connect("pressed", self, "_update_visualization")
@@ -31,5 +25,5 @@ func _update_visualization():
 
 
 func _on_group_plant_selected(selected_id: int):
-	var plant = $PlantPanel/PlantList.get_item_metadata(selected_id)
+	var plant = $HSplitContainer/GroupPanel/GroupDetails/GroupAttributes/Attributes/GroupPlantList.get_item_metadata(selected_id)
 	$PlantPanel/PlantDetails.set_plant(plant)
