08baa51c77f6bd3dcf3c171f6eda40aa8af34637
karl <karl.bittner@hexaquo.at>
2021-02-15 20:24:22 +0100

    Improve data loading in TerrainRenderer
    Parallel threads with safe copies of data, and some is_valid checks (using the latest Geodot functionality).

--- Layers/Renderers/Terrain/TerrainLOD.gd
+++ Layers/Renderers/Terrain/TerrainLOD.gd
@@ -20,6 +20,8 @@ var current_texture
 
 
 func _ready():
+	visible = false
+	
 	if is_inner:
 		mesh = inner_mesh
 	else:
@@ -39,23 +41,33 @@ func build():
 	scale.x = size / mesh_size
 	scale.z = size / mesh_size
 	
-	# TODO: We need new objects here due to being in a thread. Solve this with a "copy" function instead.
-	current_heightmap = height_layer.get_image(
+	var current_height_image = height_layer.get_image(
 		top_left_x,
 		top_left_y,
 		size,
 		100,
 		1
-	).get_image_texture()
-	current_texture = texture_layer.get_image(
+	)
+	
+	if current_height_image.is_valid():
+		current_heightmap = current_height_image.get_image_texture()
+	
+	
+	var current_ortho_image = texture_layer.get_image(
 		top_left_x,
 		top_left_y,
 		size,
 		1000,
 		1
-	).get_image_texture()
+	)
+	
+	if current_ortho_image.is_valid():
+		current_texture = current_ortho_image.get_image_texture()
 
 
 func apply_textures():
-	material_override.set_shader_param("heights", current_heightmap)
-	material_override.set_shader_param("tex", current_texture)
+	if current_heightmap and current_texture:
+		material_override.set_shader_param("heights", current_heightmap)
+		material_override.set_shader_param("tex", current_texture)
+		
+		visible = true
