a6d977134e9a97dad099d4301974e655ae83bb1a
karl <karl.bittner@hexaquo.at>
2021-02-18 18:03:03 +0100

    Add ability to filter plants by type

--- Vegetation/Editor/PlantList.gd
+++ Vegetation/Editor/PlantList.gd
@@ -1,18 +1,10 @@
 extends ItemList
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Vegetation.connect("new_data", self, "_build_list")
-	
-	# Initial build - this node is created after the new_data signal is first emitted
-	_build_list()
-
-
-func _build_list():
+func build_list(plants):
 	clear()
 	
-	for plant in Vegetation.plants.values():
+	for plant in plants:
 		var item_id = get_item_count()
 		add_item(plant.get_title_string())
 		
