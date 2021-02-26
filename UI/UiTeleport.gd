df95e8d282333d223f54cd9487e7866fa94f5dfe
Mathias Baumgartinger <hiasbaum@gmail.com>
2021-02-26 10:51:16 +0100

    Missed a file for the teleportation through UI. With this everything should work - however the offset does not yet get set according to the current geographic location, so the PoI port fails.

--- UI/UiTeleport.gd
+++ UI/UiTeleport.gd
@@ -1,17 +1,33 @@
 extends "res://UI/Tools/ToolsButton.gd"
 
 
+var pc_player: AbstractPlayer setget set_player
+var pos_manager
+
+
 func _ready():
 	connect("toggled", self, "_on_toggle")
-	GlobalSignal.connect("teleported", self, "_on_teleported")
+	$WindowDialog/PoI/VBoxContainer/ItemList.connect("item_activated", self, "_on_poi_activated")
+
+
+func set_player(player):
+	pc_player = player
+	pc_player.get_node("ActionHandler").connect("teleport_finished", self, "_on_toggle", [false])
 
 
 func _on_toggle(toggled: bool):
+	pressed = toggled
 	if toggled:
-		UISignal.emit_signal("set_teleport_mode", true)
+		pc_player.get_node("ActionHandler").set_current_mode("teleport")
 	else:
-		UISignal.emit_signal("set_teleport_mode", false)
+		pc_player.get_node("ActionHandler").stop_current_mode()
+		$WindowDialog.hide()
 
 
-func _on_teleported():
-	set_pressed(false)
+# We saved the location coordinates in the metadata of the list items,
+# if one is clicked handle the teleport manually here
+func _on_poi_activated(index):
+	var engine_cords = pos_manager.to_engine_coordinates($WindowDialog/PoI/VBoxContainer/ItemList.get_item_metadata(index))
+	# FIXME: we need an alternative for WorldPosition.get_position_on_ground()
+	pc_player.teleport(Vector3(engine_cords.x, pc_player.transform.origin.y, engine_cords.y))
+	_on_toggle(false)
