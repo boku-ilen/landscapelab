6d808b6d125d3fd3d09a9bcda6b228218b2cfc5c
Mathias Baumgartinger <hiasbaum@gmail.com>
2021-02-27 11:45:07 +0100

    Changed the functionality of the aciton handler. I know added an abstract class for how action should look like. These can be defined anywhere from anywhere and will be applied in the playercontroller using the actionhandler.

--- Perspectives/PC/ActionHandler.gd
+++ Perspectives/PC/ActionHandler.gd
@@ -1,43 +1,53 @@
 extends Node
+class_name ActionHandler
 
 
 var cursor: RayCast
 var collision_indicator: Spatial
 var player: AbstractPlayer
 
-var current_mode: String
-
-signal teleport_finished
+var current_action: Action
 
 
 # The string should precisely resemble a function in this script
-func set_current_mode(mode: String):
-	current_mode = mode
+func set_current_action(action: Action):
+	current_action = action
 
 
-func stop_current_mode():
-	current_mode = ""
+func stop_current_action():
+	current_action = null
 
 
 # If an input comes, the player will check if the action handler has a pending
 # action. If so it will call for this action. Otherwise it will handle input
 # in it's usual manner.
 func has_action() -> bool:
-	return current_mode != ""
+	return current_action != null
+
+# Actions can be blocking (the playercontroller will not further handle the input)
+func has_blocking_action() -> bool:
+	return has_action() and current_action.is_blocking
 
 
 # This should solely be used for things that need an input inside the player's
 # script, and actually handles this via callbacks. Other functionalities that 
-# do not require a direct input (e.g. viewshed) should be handled sperately.
+# do not require a direct input (e.g. viewshed) should be handled seperately.
 func action(event):
-	if has_method(current_mode):
-		call(current_mode, event)
+	if has_action():
+		current_action.apply(event)
 
 
-func teleport(event):
-	if event.is_action_pressed("teleport_player"):
-		player.teleport(cursor.get_collision_point() + Vector3(0, 2, 0))
-		emit_signal("teleport_finished")
+# The action class, which can be extended from anywhere the class is needed
+class Action:
+	var player: AbstractPlayer
+	var is_blocking: bool
+	
+	func _init(p, blocking):
+		player = p
+		is_blocking = blocking
+	
+	func apply(event):
+		pass
 
 
 func enable_viewshed(enabled: bool):
