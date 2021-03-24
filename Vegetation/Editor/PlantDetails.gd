5098a6a54cbc99b19db131d96285efb8973c8739
karl <karl.bittner@hexaquo.at>
2021-03-24 19:46:15 +0100

    Add dropdown for Density Class

--- Vegetation/Editor/PlantDetails.gd
+++ Vegetation/Editor/PlantDetails.gd
@@ -15,6 +15,7 @@ func set_plant(plant: Vegetation.Plant):
 	$DetailList/MinHeight/LineEdit.text = str(plant.height_min)
 	$DetailList/MaxHeight/LineEdit.text = str(plant.height_max)
 	$DetailList/Density/LineEdit.text = str(plant.density_ha)
+	$DetailList/DensityClass/DensityClassDropdown.selected = plant.density_class
 
 
 func save_plant_values():
@@ -23,3 +24,4 @@ func save_plant_values():
 		current_plant.height_min = float($DetailList/MinHeight/LineEdit.text)
 		current_plant.height_max = float($DetailList/MaxHeight/LineEdit.text)
 		current_plant.density_ha = float($DetailList/Density/LineEdit.text)
+		current_plant.density_class = $DetailList/DensityClass/DensityClassDropdown.get_selected_class()
