841e9b334eafcc495f3e5d0417ea8f8c56ea6953
karl <karl.bittner@hexaquo.at>
2021-03-30 12:55:09 +0200

    Fix errors in ObjectBroker
    the class_name shouldn't be in a "", and the type safety complained about no return object.

--- GameEngine/ObjectBroker.gd
+++ GameEngine/ObjectBroker.gd
@@ -1,5 +1,5 @@
 extends Node
-class_name "ObjectBroker"
+class_name ObjectBroker
 
 # this class manages the loading and saving ('serialization') of the gamestate
 # used to setup the initial game and save the progress and result for later
@@ -14,4 +14,4 @@ func save(game_object: GameObject):
 # FIXME: do we want to indiviudally load a game object (via an identifier) or load the game state as a whole
 # FIXME: replacing the currently existing one
 func load() -> GameObject:
-	pass
+	return GameObject.new()
