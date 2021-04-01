06984a29c7ecbf6e2f5267351155fd002e10acbb
karl <karl.bittner@hexaquo.at>
2021-04-01 13:33:24 +0200

    "Save Configuration" overrides previous CSVs

--- Vegetation/Editor/SaveButton.gd
+++ Vegetation/Editor/SaveButton.gd
@@ -2,8 +2,15 @@ extends Button
 
 
 func _ready():
-	connect("pressed", self, "_on_pressed")
+	connect("pressed", self, "save_override")
 
 
-func _on_pressed():
-	$SaveDialog.popup()
+func save_override():
+	# Load the paths from the config
+	var config = ConfigFile.new()
+	config.load("user://vegetation_paths.cfg")
+	
+	var plant_path = config.get_value("paths", "plant_csv_path")
+	var group_path = config.get_value("paths", "group_csv_path")
+	
+	Vegetation.save_to_files(plant_path, group_path)
