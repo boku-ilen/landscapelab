f602bcc3ff4b696f3674662b70933fa3d1a75dd8
Mathias Baumgartinger <hiasbaum@gmail.com>
2021-04-03 12:54:07 +0200

    Added logic for adding features in the object layer using geodot.

--- Layers/Renderers/Objects/ObjectRenderer.gd
+++ Layers/Renderers/Objects/ObjectRenderer.gd
@@ -8,15 +8,7 @@ func load_new_data():
 	var features = layer.get_features_near_position(center[0], center[1], radius, max_features)
 	
 	for feature in features:
-		var instance = layer.render_info.object.instance()
-		
-		var local_object_pos = feature.get_offset_vector3(-center[0], 0, -center[1])
-		local_object_pos.y = layer.render_info.ground_height_layer.get_value_at_position(
-			center[0] + local_object_pos.x, center[1] - local_object_pos.z)
-		
-		instance.transform.origin = local_object_pos
-		
-		add_child(instance)
+		apply_new_feature(feature)
 
 
 func apply_new_data():
@@ -24,6 +16,18 @@ func apply_new_data():
 	pass
 
 
+func apply_new_feature(feature):
+	var instance = layer.render_info.object.instance()
+	
+	var local_object_pos = feature.get_offset_vector3(-center[0], 0, -center[1])
+	local_object_pos.y = layer.render_info.ground_height_layer.get_value_at_position(
+		center[0] + local_object_pos.x, center[1] - local_object_pos.z)
+	instance.transform.origin = local_object_pos
+	
+	add_child(instance)
+
+
 func _ready():
+	layer.geo_feature_layer.connect("feature_added", self, "apply_new_feature")
 	if not layer is FeatureLayer or not layer.is_valid():
 		logger.error("ObjectRenderer was given an invalid layer!")
