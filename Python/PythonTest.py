8aae9331116fb4b8ccd08104fb32aa291f94ca17
chrisgraf <christoph.graf@boku.ac.at>
2021-08-09 20:44:12 +0200

    * fixed tcl issue by setting the tcl library before executing any tcl-related code

--- Python/PythonTest.py
+++ Python/PythonTest.py
@@ -1,5 +1,9 @@
 from godot import exposed, export
 from godot import *
+import os
+import sys
+
+os.environ["TCL_LIBRARY"] = os.path.join(sys.base_exec_prefix, "lib", "tcl8.6")
 
 import matplotlib.pyplot as pyplot
 
@@ -12,6 +16,7 @@ class PythonTest(Node):
 	b = export(str, default='foo')
 
 	def _ready(self):
+
 		pyplot.plot([1, 2, 3, 4, 5, 6],[4, 5, 1, 3, 6, 7])
 		pyplot.title('Test Chart')
 		pyplot.show()
