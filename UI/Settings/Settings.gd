1c82855516e7ab203efcc60d34bbc1a08e557944
Mathias Baumgartinger <mathiasbaumgartinger@tutanota.com>
2021-08-12 12:45:03 +0200

    Minor changes in the appearance and behaviour of Settings

--- UI/Settings/Settings.gd
+++ UI/Settings/Settings.gd
@@ -5,6 +5,7 @@ onready var settings = get_node("SettingsWindow")
 onready var labtable = get_node("SettingsWindow/ScrollContainer/VBoxContainer/LabTable")
 onready var button = get_node("ShowSettingsButton")
 
+
 func _ready():
 	labtable.get_node("EnableLabTable").connect("toggled", self, "_on_labtable")
 	button.connect("pressed", self, "_on_Settings_pressed")
