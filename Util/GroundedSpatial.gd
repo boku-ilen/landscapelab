0f853f8d19ec45ae4ff92853455cfd70224fdbac
Mathias Baumgartinger <mathiasbaumgartinger@tutanota.com>
2021-08-13 14:29:43 +0200

    GroundedSpatial node reworked - works with raycast from far up to far down.

--- Util/GroundedSpatial.gd
+++ Util/GroundedSpatial.gd
@@ -1,47 +1,27 @@
-extends Spatial
-class_name GroundedSpatial
+extends Node
+tool
+
 
 #
-# Standard Spatial node which can stay on the LODTerrain ground.
+# Attach this Node as a child to any spatial you want to always be fixed on 
+# the ground (no matter of level of detail, etc ...)
 #
 
 
-var tile_underneath: WorldTile
-
-var _just_placed_on_ground = false
-
-var pos_manager: PositionManager
+export var is_active := true setget set_active
+# Distance above ground any time
+export var above_height := 5
 
 
-func _ready():
-	# Required to make the _notification with NOTIFICATION_TRANSFORM_CHANGED work
-	set_notify_transform(true)
-
-	_place_on_ground()
+func set_active(active: bool) -> void:
+	is_active = active
+	$RayCast.enabled = active
 
 
-func _notification(what):
-	# If the global position of this node has changed, get the tile this node is now on and
-	#  place the node on the ground
-	if what == NOTIFICATION_TRANSFORM_CHANGED and is_inside_tree():
-		# Without this flag, we would loop infinitely since we cause a NOTIFICATION_TRANSFORM_CHANGED
-		#  inside _place_on_ground()!
-		if _just_placed_on_ground:
-			_just_placed_on_ground = false
-		else:
-			_place_on_ground()
-
-
-# Puts the origin of the node on the ground.
-func _place_on_ground():
-	# FIXME: Update position: global_transform.origin =
-
-	_just_placed_on_ground = true
-
-
-# React to a world shift
-func shift(delta_x, delta_z):
-	global_transform.origin += Vector3(delta_x, 0, delta_z)
-
-	# Since this was a world shift, we don't need to get a new height
-	_just_placed_on_ground = true
+func _process(delta):
+	if $RayCast.is_colliding() and "translation" in get_parent():
+		# Apply parent x, z to the raycast
+		var parent_pos = get_parent().global_transform.origin
+		$RayCast.translation = Vector3(parent_pos.x, $RayCast.translation.y, parent_pos.z)
+		# Apply raycast collision-height to parent
+		get_parent().translation.y = $RayCast.get_collision_point().y
