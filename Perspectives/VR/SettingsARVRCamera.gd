a7f3739a16ad44f38f0cb5fa3d343f4c48538371
Mathias Baumgartinger <mathiasbaumgartinger@tutanota.com>
2021-08-13 14:32:57 +0200

    New FirstPersonVR

--- Perspectives/VR/SettingsARVRCamera.gd
+++
@@ -1,8 +0,0 @@
-extends ARVRCamera
-
-
-var VIEW_DISTANCE = Settings.get_setting("player", "view-distance")
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	far = VIEW_DISTANCE
