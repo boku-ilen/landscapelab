26b63104f875f2a55d5c17c4633be6be1ff02235
Karl <karl.bittner@hexaquo.at>
2021-10-18 18:33:07 +0200

    Randomize wind turbine speed and start rotation

--- Objects/WindTurbine/GenericWindTurbine.gd
+++ Objects/WindTurbine/GenericWindTurbine.gd
@@ -6,21 +6,27 @@ extends Spatial
 
 onready var rotor = get_node("Mesh/Rotor")
 
-export(float) var speed = 0.05 # Rotation speed in radians
+export(float) var speed = 0.1 # Rotation speed in radians
 export(float) var wind_direction = 0 setget set_wind_direction, get_wind_direction # Rotation of wind in degrees
 
 export(Vector3) var forward_for_rotation = Vector3(1, 0, 0)
 
+
 func _ready():
 	# Orient the windmill according to the scenario's wind direction
 	# This assumes that a wind direction of 90° means that the wind is blowing from west to east.
-	# FIXME: Since this was removed from the scenario, we need an alternative
-	#  (maybe game mode specific) way to get the wind direction!
+	# FIXME: Should be set from the outside (e.g. using another layer)
 	set_wind_direction(315.0)
 	
 	# If is_inside_tree() in set_wind_direction() returned false, we need to catch up on
 	#  setting the wind direction now.
 	update_rotation()
+	
+	# Randomize speed a little
+	speed += (randf() - 0.5) * (speed * 0.5)
+	
+	# Start at a random rotation
+	rotor.transform.basis = rotor.transform.basis.rotated(forward_for_rotation, randf() * PI * 2.0)
 
 
 # Saves the specified wind direction and updates the model's rotation
@@ -47,6 +53,3 @@ func update_rotation():
 func _process(delta):
 	if delta > 0.8: return  # Avoid skipping
 	rotor.transform.basis = rotor.transform.basis.rotated(forward_for_rotation, -speed * delta)
-
-
-# TODO: The wind_direction could be automatically set using signals at some point
