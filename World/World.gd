66f2a286ebf2961d9440f9d2a674ce0ca3a94645
Karl <karl.bittner@hexaquo.at>
2021-10-28 18:34:31 +0200

    Fix loading bug in previous commit

--- World/World.gd
+++ World/World.gd
@@ -5,8 +5,8 @@ var is_fullscreen: bool = false
 
 
 func _ready():
-	$PositionManager.terrain = get_node("Terrain")
 	$PositionManager.layer_configurator = get_node("LayerConfigurator")
+	$PositionManager.terrain = get_node("Terrain")
 
 
 func _input(event):
