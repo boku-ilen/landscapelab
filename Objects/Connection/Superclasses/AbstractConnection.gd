cff35fa82fa5d9ad117d5a37067ab52710abee10
Mathias Baumgartinger <mathiasbaumgartinger@tutanota.com>
2021-10-28 10:28:05 +0200

    Added a connection system containing catenary system

---
+++ Objects/Connection/Superclasses/AbstractConnection.gd
@@ -0,0 +1,12 @@
+extends Node
+class_name AbstractConnection
+
+
+# Abstract method: can be thread unsafe
+func apply_connection():
+	pass
+
+
+# Abstract method: must be thread safe
+func find_connection_points(P1: Vector3, P2: Vector3, length_factor: float):
+	pass
