c4f25c64f973576af121bb87f6bc747f9c5056f4
unknown <hiasbaum@gmail.com>
2021-12-01 19:44:53 +0100

    Added a globally accesible screenshotting feature.

---
+++ Global/Screencapture.gd
@@ -0,0 +1,34 @@
+extends Node
+
+#
+# Simplified access to screenshotting
+#
+
+# Will be automatically set from the world once the positionmanager is loaded
+var pos_manager
+
+
+func _input(event):
+	if event.is_action_pressed("screenshot"): screenshot()
+
+
+func screenshot(
+				image_name := "user://photo-%d-%s.png" % \
+					[OS.get_system_time_msecs(), pos_manager.get_center_node_world_position()], 
+				viewport_size: Vector2 = pos_manager.get_viewport().size):
+	
+	var previous_viewport_size = pos_manager.get_viewport().size
+	
+	pos_manager.get_viewport().size = viewport_size * 5
+	
+	VisualServer.force_draw()
+	
+	# get data of the viewport and flip (because ... it is flipped
+	var image = pos_manager.get_viewport().get_texture().get_data()
+	image.flip_y()
+
+	# save to a file
+	image.save_png(image_name)
+	
+	pos_manager.get_viewport().size = previous_viewport_size
+	VisualServer.force_draw()
