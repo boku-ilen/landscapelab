13dfd1fe1eb9d9df832690e83f466d6b68258acc
karl <karl.bittner@hexaquo.at>
2021-12-14 23:13:25 +0100

    Display information about cursor on middle mouse click
    Middle mouse click now opens a window. It shows the distance of the cursor from the camera (closes #267). In the future, more values could be added, e.g. regarding the Object under the mouse cursor.
    
    In additional, the visuals of the cursor were made to be a bit more pleasant by not becoming huge nearby.

--- Perspectives/PC/MousePoint.gd
+++ Perspectives/PC/MousePoint.gd
@@ -10,6 +10,7 @@ extends Spatial
 
 onready var camera: Camera = get_parent()
 onready var cursor: RayCast = get_node("InteractRay")
+onready var info := get_node("CursorInfoDialog")
 
 var RAY_LENGTH = Settings.get_setting("mouse-point", "camera-ray-length") # Distance that will be checked for collision with the ground
 
@@ -17,14 +18,29 @@ var RAY_LENGTH = Settings.get_setting("mouse-point", "camera-ray-length") # Dist
 func _ready():
 	cursor.cast_to = Vector3(0, 0, -RAY_LENGTH)
 	$MouseCollisionIndicator.cursor = cursor
+	$MouseCollisionIndicator.camera = camera
+	
+	# Reparent the info dialog to the root node in order to ensure that mouse clicks are handled by
+	# it first, rather than having the player handle the clicks (which would happen without this
+	# workaround), thus making it impossible to interact with it
+	remove_child(info)
+	get_tree().get_root().call_deferred("add_child", info)
+
+
+func _process(delta):
+	# Direct the mouse position on the screen along the camera
+	# We use a local ray since it should be relative to the rotation of any parent node
+	# This is done here rather than in _input to prevent missing updates during framerate drops
+	var mouse_point_vector = camera.project_local_ray_normal(get_viewport().get_mouse_position())
+	
+	# Transform the forward vector to this projected vector (-z is forward)
+	transform.basis.z = -mouse_point_vector
 
 
 # Whenever the mouse moves, align the rotation again
 func _input(event):
-	if event is InputEventMouseMotion:
-		# Direct the mouse position on the screen along the camera
-		# We use a local ray since it should be relative to the rotation of any parent node
-		var mouse_point_vector = camera.project_local_ray_normal(event.position)
-		
-		# Transform the forward vector to this projected vector (-z is forward)
-		transform.basis.z = -mouse_point_vector
+	if event is InputEventMouseButton and event.button_index == BUTTON_MIDDLE and event.pressed:
+		# Fill the info window with values and display it
+		var distance = (cursor.get_collision_point() - camera.global_transform.origin).length()
+		info.set_distance(distance)
+		info.popup_at_mouse_position()
