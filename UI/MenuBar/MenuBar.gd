78b3842092b1214aa3d202bfaee0017d1a733027
unknown <hiasbaum@gmail.com>
2022-05-02 15:00:37 +0200

    Can now load gpkg over the menubar

--- UI/MenuBar/MenuBar.gd
+++ UI/MenuBar/MenuBar.gd
@@ -4,6 +4,18 @@ extends HBoxContainer
 var layer_configurator: Configurator setget set_layer_configurator
 
 
+enum ProjectOptions {
+	PRE_GPKG
+}
+
+
+func _ready():
+	$ProjectButton.get_popup().add_item("Open a preconfigured GeoPackage ...", ProjectOptions.PRE_GPKG)
+	$ProjectButton.get_popup().set_item_metadata(ProjectOptions.PRE_GPKG, "_pop_gpkg_menu")
+	
+	$ProjectButton.get_popup().connect("index_pressed", self, "_on_proj_menu_pressed")
+
+
 func set_layer_configurator(lc):
 	layer_configurator = lc
 	layer_configurator.connect("geodata_invalid", self, "_pop_gpkg_menu")
@@ -12,3 +24,7 @@ func set_layer_configurator(lc):
 
 func _pop_gpkg_menu():
 	$ProjectButton/GeopackageFileDialog.popup_centered()
+
+
+func _on_proj_menu_pressed(idx: int):
+	call($ProjectButton.get_popup().get_item_metadata(idx))
