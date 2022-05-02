5be0e6f291b2f39ce9b7b590065a44edc08b2917
unknown <hiasbaum@gmail.com>
2022-05-02 17:51:53 +0200

    Made loading (primitve) polygons from UI possible

--- UI/Layers/LayerConfiguration/SpecificLayerUI/PolygonLayer.gd
+++ UI/Layers/LayerConfiguration/SpecificLayerUI/PolygonLayer.gd
@@ -1,16 +1,24 @@
 extends SpecificLayerUI
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func assign_specific_layer_info(layer: Layer):
+	if layer.render_info == null:
+		layer.render_info = Layer.PolygonRenderInfo.new()
+	
+	var polygon_layer = $RightBox/PolygonChooser.get_geo_layer(false)
+	var height_layer = $RightBox/GroundHeightChooser.get_geo_layer(true)
+
+	if !validate(polygon_layer) or !validate(height_layer):
+		print_warning("Polygon- or height-layer is invalid!")
+		return
+
+	layer.geo_feature_layer = polygon_layer#.clone()
+	layer.render_info.ground_height_layer = height_layer.clone()
+	layer.render_info.height_attribute_name = $RightBox/HeightAttributeText.text
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func init_specific_layer_info(layer):
+	$RightBox/GroundHeightChooser.init_from_layer(
+		layer.render_info.ground_height_layer)
+	$RightBox/PolygonChooser.init_from_layer(
+		layer.geo_feature_layer)
