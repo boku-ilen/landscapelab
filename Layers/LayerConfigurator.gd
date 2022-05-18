ede597c7584febf8c0e88929861f26bb6fa924f2
Karl <karl.bittner@hexaquo.at>
2022-05-18 13:49:27 +0200

    Adapt GameSystem config to Joglland data

--- Layers/LayerConfigurator.gd
+++ Layers/LayerConfigurator.gd
@@ -37,37 +37,47 @@ func load_gpkg(geopackage_path: String):
 	
 	# FIXME: Game Engine Testing
 	var game_mode = GameMode.new()
-	var wka = game_mode.add_game_object_collection_for_feature_layer("WKA Alt", Layers.geo_layers["features"]["WKA_NeuWei_Bestand"])
 	
-	game_mode.game_object_collections["WKA Alt"].add_explicit_attribute_mapping("Rotor", "Rotordurch")
-	game_mode.game_object_collections["WKA Alt"].add_implicit_attribute_mapping("Hohe", Layers.geo_layers["rasters"]["dhm"])
+	var wka = game_mode.add_game_object_collection_for_feature_layer("WKA", Layers.geo_layers["features"]["windmills"])
+	game_mode.game_object_collections["WKA"].add_implicit_attribute_mapping("MW", Layers.geo_layers["rasters"]["windturbine_potentials"])
+	game_mode.game_object_collections["WKA"].icon_name = "windmill_icon"
 	
-	game_mode.game_object_collections["WKA Alt"].icon_name = "windmill_icon"
+	var wka_condition = GreaterThanRasterCreationCondition.new("WKA Potential Condition", Layers.geo_layers["rasters"]["windturbine_potentials"], 0.0)
+	wka.add_creation_condition(wka_condition)
 	
+	var pv_small = game_mode.add_game_object_collection_for_feature_layer("PV klein", Layers.geo_layers["features"]["pv_small"])
+	game_mode.game_object_collections["PV klein"].add_implicit_attribute_mapping("MW", Layers.geo_layers["rasters"]["pv_potentials"])
+	game_mode.game_object_collections["PV klein"].icon_name = "pv_icon"
+	
+	var pv_large = game_mode.add_game_object_collection_for_feature_layer("PV groß", Layers.geo_layers["features"]["pv_small"])
+	game_mode.game_object_collections["PV groß"].add_implicit_attribute_mapping("MW", Layers.geo_layers["rasters"]["pv_potentials"])
+	game_mode.game_object_collections["PV groß"].icon_name = "pv_icon"
+	
+	var pv_condition = GreaterThanRasterCreationCondition.new("PV Potential Condition", Layers.geo_layers["rasters"]["pv_potentials"], 0.0)
+	pv_small.add_creation_condition(pv_condition)
+	pv_large.add_creation_condition(pv_condition)
+
 	game_mode.set_extent(569000.0, 380000.0, 599000.0, 410000.0)
-	
+
 	var score = GameScore.new()
-	score.name = "Test Score"
-	score.add_contributor(game_mode.game_object_collections["WKA Alt"], "Rotor")
-	score.target = 10000.0
-	
+	score.name = "Strom PV"
+	score.add_contributor(game_mode.game_object_collections["PV groß"], "MW")
+	score.add_contributor(game_mode.game_object_collections["PV klein"], "MW")
+	score.target = 222667.0
+
 	game_mode.add_score(score)
-	
+
 	var score2 = GameScore.new()
-	score2.name = "Second Test Score"
-	score2.add_contributor(game_mode.game_object_collections["WKA Alt"], "Rotor")
-	score2.add_contributor(game_mode.game_object_collections["WKA Alt"], "Hohe")
-	score2.target = 20000.0
-	
+	score2.name = "Strom WKA"
+	score2.add_contributor(game_mode.game_object_collections["WKA"], "MW")
+	score2.target = 69726.0
+
 	game_mode.add_score(score2)
-	
-#	var condition = GreaterThanRasterCreationCondition.new("Test Condition", Layers.geo_layers["rasters"]["dhm"], 116.0)
-#	wka.add_creation_condition(condition)
-	
+
 	# Add player game object collection
 	var player_game_object_collection = PlayerGameObjectCollection.new("Players", get_parent().get_node("FirstPersonPC"))
 	game_mode.add_game_object_collection(player_game_object_collection)
-	
+
 	GameSystem.current_game_mode = game_mode
 
 
