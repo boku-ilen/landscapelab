90f2204d6a5484b18ef511aca4e2c659b3b8793c
Landscapelab <landscapelab@boku.ac.at>
2022-06-14 16:03:18 +0200

    Added Spinboxed to UI Datetime

--- UI/Datetime/Datetime.gd
+++ UI/Datetime/Datetime.gd
@@ -6,6 +6,20 @@ var time_manager: TimeManager
 
 func _ready():
 	$ConfirmButton.connect("pressed", self, "_on_confirm_pressed")
+	
+	$TimeSetting/TimeSlider.connect("value_changed", self, "_update_spinbox", 
+		[$TimeSetting/SpinBox])
+	$SeasonSetting/DaySlider.connect("value_changed", self, "_update_spinbox", 
+		[$SeasonSetting/SpinBox])
+	$YearSetting/YearSlider.connect("value_changed", self, "_update_spinbox", 
+		[$YearSetting/SpinBox])
+	
+	$TimeSetting/SpinBox.connect("value_changed", self, "_update_slider", 
+		[$TimeSetting/TimeSlider])
+	$SeasonSetting/SpinBox.connect("value_changed", self, "_update_slider", 
+		[$SeasonSetting/DaySlider])
+	$YearSetting/SpinBox.connect("value_changed", self, "_update_slider", 
+		[$YearSetting/YearSlider])
 
 
 func _on_confirm_pressed():
@@ -14,3 +28,11 @@ func _on_confirm_pressed():
 		$SeasonSetting/DaySlider.value,
 		$YearSetting/YearSlider.value
 	)
+
+
+func _update_spinbox(value, spinbox: SpinBox):
+	spinbox.value = value
+
+
+func _update_slider(value, slider: HSlider):
+	slider.value = value
