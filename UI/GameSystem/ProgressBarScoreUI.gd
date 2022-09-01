2a6b99a58b91f12902c491adb7ff58b1d2d17b7a
Landscapelab <landscapelab@boku.ac.at>
2022-09-01 16:27:36 +0200

    Arranges the progressbar vertically instead of horizontally
    Therefore, a change to a textureprogress was necessary (see https://github.com/godotengine/godot-proposals/issues/4514)

--- UI/GameSystem/ProgressBarScoreUI.gd
+++ UI/GameSystem/ProgressBarScoreUI.gd
@@ -1,4 +1,4 @@
-extends HBoxContainer
+extends BoxContainer
 
 
 var score: GameScore setget set_score
@@ -8,28 +8,32 @@ func set_score(new_score):
 	score = new_score
 	
 	$Name.text = score.name
-	$MaxValue.text = str(score.target)
-	$ProgressBar.min_value = 0.0
-	$ProgressBar.max_value = score.target
+	$VBoxContainer/MaxValue.text = str(score.target)
+	$CenterContainer/ProgressBar.min_value = 0.0
+	$CenterContainer/ProgressBar.max_value = score.target
 	
 	if score.contributors[0].color_code:
-		# Get current stylebox of the progress, duplicate so it only affects this node
-		# and only override the background color so it fits the rest of the theme
-		var new_stylebox = $ProgressBar.get_stylebox("fg").duplicate()
-		# For many themes, a texture could be used which does not have a color
-		# in no styleboxflat is used, create one on our own
-		if "bg_color" in new_stylebox:
-			new_stylebox.bg_color = score.contributors[0].color_code
-			$ProgressBar.add_stylebox_override("fg", new_stylebox)
-		else: 
-			new_stylebox = StyleBoxFlat.new()
-			new_stylebox.bg_color = score.contributors[0].color_code
-			$ProgressBar.add_stylebox_override("fg", new_stylebox)
+		if $CenterContainer/ProgressBar is ProgressBar:
+			# Get current stylebox of the progress, duplicate so it only affects this node
+			# and only override the background color so it fits the rest of the theme
+			var new_stylebox = $CenterContainer/ProgressBar.get_stylebox("fg").duplicate()
+			# For many themes, a texture could be used which does not have a color
+			# in no styleboxflat is used, create one on our own
+			if "bg_color" in new_stylebox:
+				new_stylebox.bg_color = score.contributors[0].color_code
+				$CenterContainer/ProgressBar.add_stylebox_override("fg", new_stylebox)
+			else: 
+				new_stylebox = StyleBoxFlat.new()
+				new_stylebox.bg_color = score.contributors[0].color_code
+				$CenterContainer/ProgressBar.add_stylebox_override("fg", new_stylebox)
+		elif $CenterContainer/ProgressBar is TextureProgress:
+			$CenterContainer/ProgressBar.tint_progress = score.contributors[0].color_code
+	
 	
 	_update_data(score.value)
 	score.connect("value_changed", self, "_update_data")
 
 
 func _update_data(new_value):
-	$CurrentValue.text = str(new_value)
-	$ProgressBar.value = new_value
+	$VBoxContainer/CurrentValue.text = str(new_value)
+	$CenterContainer/ProgressBar.value = new_value
