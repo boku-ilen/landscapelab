97f8f525b24c643a6d3aca5b9c6433b8c872b77e
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2022-09-05 17:43:30 +0200

    Updates physical appearance of icon-text score

--- UI/GameSystem/IconTextScoreUI.gd
+++ UI/GameSystem/IconTextScoreUI.gd
@@ -11,13 +11,13 @@ func set_score(new_score):
 	score = new_score
 	
 	$VBox/HBox/Name.text = score.name
-	$TextureDescriptor.texture = load(
+	$VBox/HBox2/TextureDescriptor.texture = load(
 		"res://Resources/Icons".plus_file(icon_folder).plus_file(score.icon_descriptor) + ".svg")
-	$TextureSubject.texture = load(
+	$VBox/HBox2/TextureSubject.texture = load(
 		"res://Resources/Icons".plus_file(icon_folder).plus_file(score.icon_subject) + ".svg")
 	_update_data(score.value)
 	score.connect("value_changed", self, "_update_data")
 
 
 func _update_data(value):
-	$VBox/Value.text = str(round(score.value))
+	$VBox/HBox2/Value.text = str(round(score.value))
