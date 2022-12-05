83ad4b2c2873d6bfacd4557c5619d94d6cbe6666
Mathias Baumgartinger <hiasbaum@gmail.com>
2022-12-05 19:37:20 +0100

    Adds GeoLayerUI directly to the viewport and introduces z-index representation by items in geo-layer-ui
    Similar to common UI-applications a ui-list will represent which layer is above and which is below

--- UI/Layers/GeoLayerViewport.gd
+++ UI/Layers/GeoLayerViewport.gd
@@ -1,5 +1,6 @@
 extends SubViewportContainer
 
+@export var geo_layer_ui: NodePath
 
 func _gui_input(event):
 	$SubViewport/Camera2D.input(event)
@@ -8,5 +9,5 @@ func _gui_input(event):
 func _ready():
 	$ZoomContainer/ZoomIn.pressed.connect($SubViewport/Camera2D.do_zoom.bind(1.1))
 	$ZoomContainer/ZoomOut.pressed.connect($SubViewport/Camera2D.do_zoom.bind(0.9))
-	
-	
+	get_node(geo_layer_ui).get_node("ItemList").z_index_changed.connect(
+		$SubViewport/GeoLayerRenderers.reclassify_z_indices)
