5cc0fb05597122b5a80d4881c36266896bab1661
DavidPgl <panagiotopulos.d@gmail.com>
2022-12-12 20:59:36 +0100

    Add nodes for debugging

--- Layers/Renderers/Path/PathRenderer.gd
+++ Layers/Renderers/Path/PathRenderer.gd
@@ -5,6 +5,7 @@ var path_layer: GeoFeatureLayer
 
 var chunks = []
 var chunk_size = 1000.0
+var step_size = chunk_size / (300.0 + 1.0)
 
 var center = [0,0]
 var radius: float = 500.0
@@ -15,21 +16,36 @@ var heightmaps: Dictionary = {}
 
 
 func load_roads() -> void:
-	_create_heightmap_dictionary()
+	print("LOADING ROADS STARTED")
+	for child in $"Debug".get_children():
+		child.free()
 	
-	var path_features = path_layer.get_features_near_position(float(center[0]), float(center[1]), radius, max_features)
+	_create_heightmap_dictionary()
+	var player_position = [int(center[0] + $"..".position_manager.center_node.position.x), int(center[1] - $"..".position_manager.center_node.position.z)]
+	print("player_position: %s | %s" %[player_position[0] - center[0], player_position[1] - center[1]])
+	print("Current Chunk: %s | %s" %[roundi((player_position[0] - center[0]) / 1000), roundi((player_position[1] - center[1]) / 1000)])
+	var path_features = path_layer.get_features_near_position(float(player_position[0]), float(player_position[1]), radius, max_features)
 	for path_feature in path_features:
 		var path = _create_path(path_feature)
+	
+	print("LOADING ROADS FINISHED")
 
 
 func _create_heightmap_dictionary() -> void:
 	for chunk in chunks:
-		var position_x = int(chunk.position.x / 1000)
-		var position_z = int(chunk.position.z / 1000)
+		var position_x = int(chunk.position.x / chunk_size)
+		var position_z = -int(chunk.position.z / chunk_size)
 		if heightmaps.has(position_x):
 			heightmaps[position_x][position_z] = chunk.current_heightmap.duplicate()
 		else:
 			heightmaps[position_x] = {position_z: chunk.current_heightmap.duplicate()}
+		
+		var label: Label3D = $Label3D.duplicate()
+		label.position.x = chunk.position.x
+		label.position.z = chunk.position.z
+		label.position.y = 200
+		label.text = "[%s, %s]" %[position_x, position_z]
+		$"Debug".add_child(label)
 
 
 func _create_path(path_feature) -> PathInstance:
@@ -38,30 +54,67 @@ func _create_path(path_feature) -> PathInstance:
 	var path_curve: Curve3D = path_feature.get_offset_curve3d(-center[0], 0, -center[1])
 	for index in range(path_curve.point_count):
 		var position = path_curve.get_point_position(index)
-		var height = _get_height(position)
-		var cube: MeshInstance3D = $"../MeshInstance3D".duplicate()
+		position = get_triangular_interpolation_point(position, step_size)
+		#var height = _get_height(position)
+		
+		# DEBUGGING
+		var space_state = get_parent().get_world_3d().direct_space_state
+		var ray: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(Vector3(position.x, 6000, position.z), Vector3(position.x, -1000, position.z))
+		var result = space_state.intersect_ray(ray)
+		if not result.is_empty():
+			var cube: MeshInstance3D = $"MeshInstance3D_test".duplicate()
+			cube.position = position
+			cube.position.y = result["position"].y
+			$"Debug".add_child(cube)
+		
+		
+		var cube: MeshInstance3D = $"MeshInstance3D".duplicate()
 		cube.position = position
-		cube.position.y = height
-		add_child(cube)
+		cube.position.y = position.y
+		$"Debug".add_child(cube)
 	
 	return path_instance
 
 
+# Returns the triangle surface point at the given point-position
+func get_triangular_interpolation_point(point: Vector3, step_size: float) -> Vector3:
+	var A = QuadUtil.get_lower_left_point(point, step_size)
+	var C = QuadUtil.get_upper_right_point(point, step_size)
+	var B
+	
+	# Check if point is in upper half of the triangle
+	if fposmod(point.x, step_size) < fposmod(point.z, step_size):
+		B = QuadUtil.get_lower_right_point(point, step_size)
+	else:
+		B = QuadUtil.get_upper_left_point(point, step_size)
+	
+	# Get barycentric weights
+	var weights = QuadUtil.triangular_interpolation(point, A, B, C)
+	# Calculate triangle surface point with weights
+	point.y = _get_height(A) * weights.x + _get_height(B) * weights.y + _get_height(C) * weights.z
+	return point
+
+
 func _get_height(position: Vector3) -> float:
 	# Get chunk
-	var chunk_x: int = position.x / 1000.0
-	var chunk_z: int = position.z / 1000.0
+	var chunk_x: int = int(position.x / chunk_size)
+	var chunk_z: int = int(position.z / chunk_size)
 	var heightmap: ImageTexture = heightmaps[chunk_x][chunk_z]
 	var image_size = heightmap.get_width()
 	
-	var image_x = int(((fposmod(position.x + chunk_size / 2.0, chunk_size)) / chunk_size) * image_size)
-	var image_y = int(((fposmod(position.z + chunk_size / 2.0, chunk_size)) / chunk_size) * image_size)
+	#var image_x = roundi(((fposmod(position.x + chunk_size / 2.0, chunk_size)) / chunk_size) * image_size)
+	#var image_y = roundi(((fposmod(position.z + chunk_size / 2.0, chunk_size)) / chunk_size) * image_size)
+	
+	var image_x = int((fposmod(position.x + chunk_size / 2.0, chunk_size) / chunk_size) * image_size)
+	var image_y = int((fposmod(-position.z + chunk_size / 2.0, chunk_size) / chunk_size) * image_size)
+	
 	var image_position: int = (image_y * image_size + image_x) * 4
 	# Read bytes from image
 	var value: float = read_from_texture(heightmap, image_position)
+	if value < 1.0:
+		print("No height decoded")
 	
 	return value
-
 
 
 static func read_from_texture(texture: ImageTexture, pos: int) -> float:
---
+++ Layers/Renderers/Path/QuadUtil.gd
@@ -0,0 +1,145 @@
+class_name QuadUtil
+
+
+# Returns the weights for a triangular interpolation using barycentric coordinates
+static func triangular_interpolation(P: Vector3, A: Vector3, B: Vector3, C: Vector3) -> Vector3:
+	var W1 = ((B.z - C.z) * (P.x - C.x) + (C.x - B.x) * (P.z - C.z)) / ((B.z - C.z) * (A.x - C.x) + (C.x - B.x) * (A.z - C.z))
+	var W2 = ((C.z - A.z) * (P.x - C.x) + (A.x - C.x) * (P.z - C.z)) / ((B.z - C.z) * (A.x - C.x) + (C.x - B.x) * (A.z - C.z))
+	var W3 = 1 - W1 - W2
+
+	return Vector3(W1, W2, W3)
+
+
+# Returns the lower left corner from the quad the point is in
+# The offset can be used to get corners from adjacent quads
+static func get_lower_left_point(point: Vector3, step_size: float, x_offset: int = 0, z_offset: int = 0) -> Vector3:
+	var x_grid = floor(point.x / step_size) + x_offset
+	var z_grid = floor(point.z / step_size) + z_offset
+	return Vector3(x_grid * step_size, point.y, z_grid * step_size)
+
+
+# Returns the upper left corner from the quad the point is in
+# The offset can be used to get corners from adjacent quads
+static func get_upper_left_point(point: Vector3, step_size: float, x_offset: int = 0, z_offset: int = 0) -> Vector3:
+	var x_grid = floor(point.x / step_size) + x_offset
+	var z_grid = floor(point.z / step_size) + z_offset
+	return Vector3(x_grid * step_size, point.y, (z_grid + 1) * step_size)
+
+
+# Returns the lower right corner from the quad the point is in
+# The offset can be used to get corners from adjacent quads
+static func get_lower_right_point(point: Vector3, step_size: float, x_offset: int = 0, z_offset: int = 0) -> Vector3:
+	var x_grid = floor(point.x / step_size) + x_offset
+	var z_grid = floor(point.z / step_size) + z_offset
+	return Vector3((x_grid + 1) * step_size, point.y, z_grid * step_size)
+
+
+# Returns the upper right corner from the quad the point is in
+# The offset can be used to get corners from adjacent quads
+static func get_upper_right_point(point: Vector3, step_size: float, x_offset: int = 0, z_offset: int = 0) -> Vector3:
+	var x_grid = floor(point.x / step_size) + x_offset
+	var z_grid = floor(point.z / step_size) + z_offset
+	return Vector3((x_grid + 1) * step_size, point.y, (z_grid + 1) * step_size)
+
+
+# Gets the intersection point with the quad diagonal (bottom-left to top-right)
+static func get_diagonal_intersection(from: Vector3, to: Vector3, step_size: float):
+	var direction: Vector3 = to - from
+	
+	# Non-Parallel to quad diagonal
+	if direction.z != 0 && direction.x / direction.z == -1:
+		return null
+	
+	var x_grid = floor(from.x / step_size)
+	var z_grid = floor(from.z / step_size)
+	
+	var on_x_grid_and_decending: bool = (from.x / step_size) == x_grid && direction.x < 0
+	var on_z_grid_and_decending: bool = (from.z / step_size) == z_grid && direction.z < 0
+	
+	# If point is exactly on grid and direction is negative, move points 'back'
+	if on_x_grid_and_decending:
+		x_grid -= 1
+	
+	if on_z_grid_and_decending:
+		z_grid -= 1
+	
+	
+	var A = Vector3((x_grid + 1) * step_size, 0, z_grid * step_size)
+	var C = Vector3(x_grid * step_size, 0, (z_grid + 1) * step_size)
+	
+	# Calculate intersection values
+	var den: float = (from.x - to.x) * (C.z - A.z) - (from.z - to.z) * (C.x - A.x)
+	
+	if den == 0:
+		return null
+	
+	var t: float = ((from.x - C.x) * (C.z - A.z) - (from.z - C.z) * (C.x - A.x)) / den
+	var u: float = -(((from.x - to.x) * (from.z - C.z) - (from.z - to.z) * (from.x - C.x)) / den)
+	
+	# Check if the intersection is with the line
+	if t >= 0.0 && t <= 1.0 && u >= 0.0 && u <= 1.0:
+		return Vector3(from.x + t * (to.x - from.x), 0, from.z + t * (to.z - from.z))
+	
+	return null
+
+
+# Gets the intersection with the grid in x direction (parallel to z-axis)
+static func get_horizontal_intersection(from: Vector3, to: Vector3, step_size: float):
+	var direction = to - from
+	
+	var offset = _get_offset(from.x, to.x, step_size)
+	if offset == null:
+		return null
+	
+	var z = direction.z / direction.x * offset
+	return Vector3(from.x + offset, 0, from.z + z)
+
+
+# Gets the intersection with the grid in z direction (parallel to x-axis)
+static func get_vertical_intersection(from: Vector3, to: Vector3, step_size: float):
+	var direction = to - from
+	
+	var offset = _get_offset(from.z, to.z, step_size)
+	if offset == null:
+		return null
+	
+	var x = direction.x / direction.z * offset
+	return Vector3(from.x + x, 0, from.z + offset)
+
+
+# Calculates the remaining distance to the next grid with the given step_size
+static func _get_offset(from: float, to: float, step_size: float):
+	var direction = to - from
+	
+	if direction == 0:
+		return null
+	
+	var grid_index = floor(from / step_size)
+	var intersections: int = abs(floor(to / step_size) - grid_index)
+	
+	# From point being exactly on the grid and a negative direction, causing additinal intersection
+	if (from / step_size) == grid_index && direction < 0:
+		intersections -= 1
+	
+	# To point being exactly on the grid, causing additional intersection
+	if (to / step_size) == floor((to / step_size)):
+		intersections -= 1
+	
+	if intersections >= 1:
+		# Calculate sampling grid offset
+		var offset: float = 0.0
+		
+		# If the direction is positive, go to the next grid on the right
+		if direction > 0:
+			offset = (step_size - fposmod(from, step_size))
+		# If the direction is negative, go to the next grid on the left
+		else:
+			offset = (fposmod(from, step_size)) * -1
+		
+		# If the offset is zero, go to next grid index
+		if offset == 0.0:
+			offset += step_size * sign(direction)
+		
+		return offset
+	
+	return null
