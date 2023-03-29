f84a2a414dc4899ae2fcbd2f24be76288fcc3232
LandscapeLab Office <landscapelab@boku.ac.at>
2023-03-29 16:17:49 +0200

    Make Dolly the center node when activated
    TODO: Reset the center again once the window is closed

--- UI/Imaging/ImagingButton.gd
+++ UI/Imaging/ImagingButton.gd
@@ -1,8 +1,14 @@
 extends AutoIconButton
 
 @export var action_handler_3d_path: NodePath
+@export var position_manager_path: NodePath
 
 
 func _ready():
+	pressed.connect(_begin_dolly)
+
+
+func _begin_dolly():
+	$Window.popup()
 	$Window.action_handlers = [get_node(action_handler_3d_path)]
-	toggled.connect($Window.set_visible)
+	get_node(position_manager_path).center_node = $Window/Margin/VBox/SubViewportContainer/SubViewport/DollyCamera
