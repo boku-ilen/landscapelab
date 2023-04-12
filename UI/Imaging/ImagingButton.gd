589d1c888820daa7cf99fe001c2d125c872aa4a7
LandscapeLab Office <landscapelab@boku.ac.at>
2023-04-12 16:18:39 +0200

    Cleanup Dolly influence after closing window

--- UI/Imaging/ImagingButton.gd
+++ UI/Imaging/ImagingButton.gd
@@ -3,12 +3,26 @@ extends AutoIconButton
 @export var action_handler_3d_path: NodePath
 @export var position_manager_path: NodePath
 
+var previous_center_node
+
 
 func _ready():
+	$Window.action_handlers = [get_node(action_handler_3d_path)]
+	
 	pressed.connect(_begin_dolly)
+	$Window.close_requested.connect(_cleanup_dolly)
 
 
 func _begin_dolly():
 	$Window.popup()
-	$Window.action_handlers = [get_node(action_handler_3d_path)]
+	
+	# Swap center node
+	previous_center_node = get_node(position_manager_path).center_node
 	get_node(position_manager_path).center_node = $Window/Margin/VBox/SubViewportContainer/SubViewport/DollyCamera
+
+
+func _cleanup_dolly():
+	# Reset center node
+	get_node(position_manager_path).center_node = previous_center_node
+	
+	$Window.hide()
