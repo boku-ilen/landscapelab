52198b57a675dab6022179ccb8c8e8b7c2209311
Mathias Baumgartinger <hiasbaum@gmail.com>
2023-04-18 22:31:11 +0200

    Adds possibility to change Vegetation.plant_extent during runtime

--- UI/MenuBar/MenuBar.gd
+++ UI/MenuBar/MenuBar.gd
@@ -4,7 +4,7 @@ extends HBoxContainer
 enum ProjectOptions {
 	OPEN_CFG,
 	SAVE_CFG,
-	SET_VEGETATION_PATHS
+	VEGETATION_DETAILS,
 }
 
 
@@ -19,10 +19,11 @@ func _ready():
 	project_options.add_item("Save a .ll config...", ProjectOptions.SAVE_CFG)
 	project_options.set_item_metadata(ProjectOptions.SAVE_CFG, 
 		$ProjectButton/SaveCfg.popup_centered)
-	project_options.add_item("Set vegetation paths...", ProjectOptions.SET_VEGETATION_PATHS)
-	project_options.set_item_metadata(ProjectOptions.SET_VEGETATION_PATHS,
-		$ProjectButton/SetVegetationPaths.popup_centered)
+	project_options.add_item("Set vegetation paths...", ProjectOptions.VEGETATION_DETAILS)
+	project_options.set_item_metadata(ProjectOptions.VEGETATION_DETAILS,
+		$ProjectButton/VegetationDetails.popup_centered)
 	
+	# Conenct selecting an ll file with opening and applying the config
 	$ProjectButton/OpenCfg.file_selected.connect(func(path):
 		var ll_file_access = LLFileAccess.open(path)
 		if ll_file_access == null:
@@ -31,23 +32,29 @@ func _ready():
 			
 		ll_file_access.apply(Vegetation, Layers, Scenarios)
 	)
+	
+	# Connect selecting a file location for ll with serialization of current state
 	$ProjectButton/SaveCfg.file_selected.connect(func(path):
 		var ll_file_access = LLFileAccess.open(path)
 		ll_file_access.save()
 	)
-	$ProjectButton/SetVegetationPaths.confirmed.connect(_on_set_vegetation_paths)
-	$ProjectButton/SetVegetationPaths/Grid/Densities.file_selected.connect(
-		_try_set_all_paths
-	)
-	$ProjectButton/SetVegetationPaths/Grid/Plants.file_selected.connect(
-		_try_set_all_paths
-	)
-	$ProjectButton/SetVegetationPaths/Grid/Groups.file_selected.connect(
-		_try_set_all_paths
-	)
-	$ProjectButton/SetVegetationPaths/Grid/Textures.file_selected.connect(
-		_try_set_all_paths
-	)
+	
+	# Setup vegetation logic
+	$ProjectButton/VegetationDetails.confirmed.connect(_on_set_vegetation_paths)
+	# If one path is selected try selecting all from the same dir (else this is a lot of work)
+	$ProjectButton/VegetationDetails/VBox/Paths/Densities.file_selected.connect(
+		_try_set_all_paths)
+	$ProjectButton/VegetationDetails/VBox/Paths/Plants.file_selected.connect(
+		_try_set_all_paths)
+	$ProjectButton/VegetationDetails/VBox/Paths/Groups.file_selected.connect(
+		_try_set_all_paths)
+	$ProjectButton/VegetationDetails/VBox/Paths/Textures.file_selected.connect(
+		_try_set_all_paths)
+	# Set initial plant extent
+	$ProjectButton/VegetationDetails/VBox/Config/SpinBox.value = Vegetation.plant_extent_factor
+	# Changing plant extent should update the actual extent
+	$ProjectButton/VegetationDetails/VBox/Config/SpinBox.value_changed.connect(
+		func(val): Vegetation.plant_extent_factor = val)
 	
 	# Connect item pressed with callback
 	project_options.index_pressed.connect(
@@ -56,10 +63,10 @@ func _ready():
 
 func _on_set_vegetation_paths():
 	Vegetation.load_data_from_csv(
-		$ProjectButton/SetVegetationPaths/Grid/Plants/FileName.text,
-		$ProjectButton/SetVegetationPaths/Grid/Groups/FileName.text,
-		$ProjectButton/SetVegetationPaths/Grid/Densities/FileName.text,
-		$ProjectButton/SetVegetationPaths/Grid/Textures/FileName.text
+		$ProjectButton/VegetationDetails/VBox/Paths/Plants/FileName.text,
+		$ProjectButton/VegetationDetails/VBox/Paths/Groups/FileName.text,
+		$ProjectButton/VegetationDetails/VBox/Paths/Densities/FileName.text,
+		$ProjectButton/VegetationDetails/VBox/Paths/Textures/FileName.text
 	)
 
 
