5f0b834f48563f44dda66987943875a64ed98808
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2023-09-04 18:55:30 +0200

    feature: create a reusable resource for wall-types which has textures for basement, ground, middle and top and allows randomized color adjustments
    Using this resource modular buildings from different textures can be combined
    
    Refs: #327

--- Buildings/Components/Walls/PlainWallResource.gd
+++ Buildings/Components/Walls/PlainWallResource.gd
@@ -2,14 +2,12 @@ extends Resource
 class_name PlainWallResource
 
 
-
-
 @export var basement_texture: WallTextureBundle
 @export var ground_texture: WallTextureBundle
 @export var middle_texture: WallTextureBundle
 @export var top_texture: WallTextureBundle
 
-@export var colors: Array[Color]
+@export var random_colors: Array[Color]
 @export_flags("basement", "ground", "middle", "top") var apply_colors
 # Wether to draw the vertices in clock- or counterclock-wise fashion
 @export_flags("basement", "ground", "middle", "top") var wind_counterclockwise
