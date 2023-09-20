5570865cae96f86a9ae951ad82d71f8618722efe
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2023-09-20 13:29:29 +0200

    perf: use multimesh for pole/pillar generation
    For osm tag building:roof

--- Buildings/Components/Walls/PoledWall.gd
+++ Buildings/Components/Walls/PoledWall.gd
@@ -3,43 +3,54 @@ extends Node3D
 
 # Needs to be set prior to build
 var ground_height_at_center
-var floors: int
-var height := 0.
+var height
+var floors: int :
+	set(new_floors):
+		floors = new_floors
+		height = floor_height * new_floors
 var floor_height = 2.5
 
 
 func build(footprint: Array):
-	var mm: MultiMesh = $MultiMeshInstance3D.multimesh
-	#mm.set_instance_count(footprint.size() * floors)
+	var mm: MultiMesh = MultiMesh.new()
+	mm.transform_format = MultiMesh.TRANSFORM_3D
+	mm.set_instance_count(footprint.size() * floors)
 	
-	var count = 0
+	# Remap 2d vertices in 3d space
 	footprint = footprint.map(func(vert): 
 		return Vector3(vert.x, 0, vert.y))
 	
+	# Instead of placing multiple "floors" of poles make one larger pole in case
+	# of multiple floors
 	var box_mesh := BoxMesh.new()
-	box_mesh.size = Vector3(0.1, floor_height, 0.1)
-	var footprint_size = footprint.size()
+	box_mesh.size = Vector3(0.1, floors * floor_height, 0.1)
+	$MultiMeshInstance3D.material_override.uv1_scale = Vector3(0.1, floors * floor_height, 0.1)
+	mm.mesh = box_mesh
 	
-	for floor_num in range(floors):
-		for footprint_num in footprint.size():
-			var vec3: Vector3 = footprint[footprint_num]
-			var vec3_before = footprint[(footprint_num - 1) % footprint.size()]
-			var vec3_after = footprint[(footprint_num + 1) % footprint.size()]
-			
-			var shift_dir = vec3.direction_to(vec3_before) + vec3.direction_to(vec3_after)
-			
-			vec3.y = floor_height * floor_num + floor_height / 2
-			var mesh_inst := MeshInstance3D.new()
-			mesh_inst.mesh = box_mesh
-			
-			var t := Transform3D(Basis.IDENTITY, vec3)
-			t.basis.z = vec3.direction_to(vec3_after).normalized()
-			t.basis.y = Vector3.UP
-			t.basis.x = t.basis.y.cross(t.basis.z)
-			
-			mesh_inst.transform = t.orthonormalized()
-			
-			add_child(mesh_inst)
-			#mm.set_instance_transform(count, Transform3D(Basis.IDENTITY, vec3))
-			count += 1
-		height += floor_height
+	var count = 0
+	for footprint_num in footprint.size():
+		var vertex: Vector3 = footprint[footprint_num]
+		var vertex_after = footprint[(footprint_num + 1) % footprint.size()]
+		
+		# Box meshes are centered so we need to add half of the meshes size
+		# + small offset to prevent gaps from orthonormalizing
+		vertex.y = box_mesh.size.y / 2 + 0.02
+		
+		# Make the poles face the appropriate direction
+		var t := Transform3D(Basis.IDENTITY, Vector3.ZERO)
+		t.basis.z = vertex.direction_to(vertex_after).normalized()
+		t.basis.y = Vector3.UP
+		t.basis.x = t.basis.y.cross(t.basis.z)
+		
+		# The polygon is winded clockwise so always going in negative direction towards next vertex
+		# and then right will place it to a arguably correct place
+		vertex -= t.basis.x * box_mesh.size.x / 2
+		vertex += t.basis.z * box_mesh.size.z / 2
+		t.origin = vertex
+		
+		# Floating point inaccuracies might mess up the matrix so its sheared => orthonormalize
+		mm.set_instance_transform(count, t.orthonormalized())
+		count += 1
+	
+	$MultiMeshInstance3D.multimesh = mm
+
