a203dac09f844c2bdaafcf3fc79a8352c8463d5f
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2023-10-17 17:01:32 +0200

    fix: set thread safety checks for geolayerrenderers
    Also minor cleanups
    
    Refs: https://github.com/godotengine/godot/pull/78000

--- Layers/Renderers/GeoLayer/GeoLayerRenderers.gd
+++ Layers/Renderers/GeoLayer/GeoLayerRenderers.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @export var camera_path: NodePath
-@onready var loading_thread = Thread.new()
+@export var load_data_threaded := true
+
+var loading_thread := Thread.new() 
 var camera: Camera2D
 var raster_renderer = preload("res://Layers/Renderers/GeoLayer/GeoRasterLayerRenderer.tscn")
 var feature_renderer = preload("res://Layers/Renderers/GeoLayer/GeoFeatureLayerRenderer.tscn")
@@ -10,7 +12,6 @@ signal loading_started
 
 var renderers_finished := 0
 var renderers_count := 0
-var load_data_threaded := true
 
 # Center in geocordinates
 var center := Vector2.ZERO
@@ -79,6 +80,7 @@ func apply_offset(new_offset, new_viewport_size, new_zoom):
 
 
 func update_renderers(new_center, new_offset, new_viewport_size, new_zoom):
+	Thread.set_thread_safety_checks_enabled(false)
 	# The maximum radius is at the corners => get the diagonale divided by 2s
 	var radius = sqrt(
 		pow(new_viewport_size.x / new_zoom.x, 2) + pow(new_viewport_size.y / new_zoom.y, 2)) / 2
