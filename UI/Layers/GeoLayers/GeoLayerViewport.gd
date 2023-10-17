bf3e2201764620ae7663734236fe7c3de443c767
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2023-10-17 17:03:23 +0200

    refactor: clean up and expose script instead of built-in script in geolayerviewport

--- UI/Layers/GeoLayers/GeoLayerViewport.gd
+++ UI/Layers/GeoLayers/GeoLayerViewport.gd
@@ -2,6 +2,7 @@ extends SubViewportContainer
 
 @export var geo_layer_ui: NodePath
 
+
 func _gui_input(event):
 	$SubViewport/Camera2D.input(event)
 
@@ -11,3 +12,6 @@ func _ready():
 	$ZoomContainer/ZoomOut.pressed.connect($SubViewport/Camera2D.do_zoom.bind(0.9))
 	get_node(geo_layer_ui).get_node("ItemList").z_index_changed.connect(
 		$SubViewport/GeoLayerRenderers.reclassify_z_indices)
+	get_node(geo_layer_ui).get_node("ItemList").geolayer_visibility_changed.connect(
+		$SubViewport/GeoLayerRenderers.set_layer_visibility
+	)
