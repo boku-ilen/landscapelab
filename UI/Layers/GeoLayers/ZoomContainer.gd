eed15bbed67486ffc2633dec7dc3642f783923c2
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2023-10-19 11:34:15 +0200

    feat: make zoomcontainer a reusable scene

---
+++ UI/Layers/GeoLayers/ZoomContainer.gd
@@ -0,0 +1,8 @@
+extends HBoxContainer
+
+
+@export var camera_2d: Camera2D :
+	set(new_camera_2d):
+		camera_2d = new_camera_2d
+		get_node("ZoomIn").pressed.connect(camera_2d.do_zoom.bind(1.1))
+		get_node("ZoomOut").pressed.connect(camera_2d.do_zoom.bind(0.9))
