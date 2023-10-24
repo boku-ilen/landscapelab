912fe27eea78a9b919d081adea024bbe375b0672
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2023-10-24 15:24:29 +0200

    refactor/fix: inital loading of overviemap works generically, commented code

--- UI/LabTable/ControlContainer.gd
+++ UI/LabTable/ControlContainer.gd
@@ -31,20 +31,30 @@ func _ready():
 func _init_overview_map():
 	var geo_raster_renderer = $VBox/SubViewportContainer/SubViewport/OverviewRenderer
 	var geo_layer_cam = $VBox/SubViewportContainer/SubViewport/Camera2D
-	
+
+	# Duplicate resource otherwise it will be shares properties with
+	# other rasterlayers and will get influenced by its behaviour
 	geo_raster_renderer.get_node("TexturePlane").mesh = geo_raster_renderer.get_node("TexturePlane").mesh.duplicate()
-	geo_raster_renderer.geo_raster_layer = Layers.get_geo_layer_by_name(map_layer_name)
-	var center = Vector2(
-		geo_raster_renderer.geo_raster_layer.get_center().x, 
-		geo_raster_renderer.geo_raster_layer.get_center().z)
-	var extent = geo_raster_renderer.geo_raster_layer.get_extent()
-	extent = extent.position - extent.end
-	var zoom = abs(geo_raster_renderer.viewport_size / extent)
-	var zoom_factor = max(zoom.x, zoom.y)
-	geo_raster_renderer.center = center
-	geo_layer_cam.zoom = Vector2(zoom_factor, zoom_factor)
-	geo_raster_renderer.zoom = Vector2(zoom_factor, zoom_factor)
-	geo_raster_renderer.viewport_size = $VBox/SubViewportContainer/SubViewport.size
 	
+	# Manually set the renderers raster layer
+	var map_layer = Layers.get_geo_layer_by_name(map_layer_name)
+	geo_raster_renderer.geo_raster_layer = map_layer
+	
+	# Obtain metadata for correctly loading the full extent of the layer
+	var center = Vector2(map_layer.get_center().x, map_layer.get_center().z)
+	var extent = map_layer.get_extent().size
+	var zoom = Vector2(geo_layer_cam.get_viewport().size) / abs(extent)
+	
+	# Minimum of zoom vector -> the smaller the zoom the more will be rendered
+	var zoom_factor = min(zoom.x, zoom.y)
+	zoom = Vector2(zoom_factor, zoom_factor)
+	
+	# Set metadata
+	geo_layer_cam.zoom = zoom
+	geo_raster_renderer.center = center
+	geo_raster_renderer.zoom = zoom
+	geo_raster_renderer.viewport_size = geo_layer_cam.get_viewport().size
+	
+	# Manually load
 	geo_raster_renderer.load_new_data()
 	geo_raster_renderer.apply_new_data()
