boku-ilen/landscapelab

commit 04c8872ba774eb5b08004d35af7fac0105ec3cba

fix: edge cases in road rendering
Fixes road rendering in Pantelleria (streets not visible due to low y coordinate and a rare crash)

Layers/Renderers/Path/RoadNetworkRenderer.gd
@@ -110,17 +110,7 @@ func _create_roads(road_features) -> void:
 		# Get road data
 		var road_width = float(road_feature.get_attribute("width"))
 		
-		# FIXME: Could be done in a more general way
-		# We check whether this feature contains rails, because rails are
-		#  rendered in 3D -> we need heights
-		if not render_3d and road_feature.get_attribute("lane_uses").contains("5,"):
-			var point_count = road_curve.get_point_count()
-			for index in range(point_count):
-				var point = road_curve.get_point_position(index)
-				point.y = get_basic_height(point)
-				road_curve.set_point_position(index, point)
-		
-		if render_3d:
+		if render_3d or road_feature.get_attribute("lane_uses").contains("5,"):
 			#############################
 			# SET INITIAL POINT HEIGHTS #
 			#############################

Layers/Renderers/Path/Roads/Intersections/IntersectionInstance.gd
@@ -62,12 +62,11 @@ func update_intersection() -> void:
 		point_b += Vector2(direction_b.y, -direction_b.x) * (road_b.width / 2.0)
 		
 		var angle = counter_clockwise_angle(direction_a, direction_b)
-		var intersection_point: Vector2
+		var intersection_point = Geometry2D.line_intersects_line(point_a, direction_a, point_b, direction_b)
+		
 		# Angle greater than 180° results in the corner being one of the roads shifted points
-		if angle >= ALMOST_STRAIGHT_ANGLE:
+		if not intersection_point:
 			intersection_point = point_a if road_a.width > road_b.width else point_b
-		else:
-			intersection_point = Geometry2D.line_intersects_line(point_a, direction_a, point_b, direction_b)
 		
 		vertices.push_back(intersection_point)
 	

Layers/Renderers/Path/Roads/RoadLanes/RoadLaneCar.tscn
@@ -9,5 +9,4 @@ transform = Transform3D(1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 5, 0)
 script = ExtResource("1_rwxlc")
 
 [node name="RoadLanePolygon" parent="." index="0"]
-layers = 32768
 material = ExtResource("2_afqel")

commit d7b8b8d2391ed43f3d7a22651bc86712f2639959

fix: regression in 04c8872

Layers/Renderers/Path/RoadNetworkRenderer.gd
@@ -110,7 +110,17 @@ func _create_roads(road_features) -> void:
 		# Get road data
 		var road_width = float(road_feature.get_attribute("width"))
 		
-		if render_3d or road_feature.get_attribute("lane_uses").contains("5,"):
+		# FIXME: Could be done in a more general way
+		# We check whether this feature contains rails, because rails are
+		#  rendered in 3D -> we need heights
+		if not render_3d and road_feature.get_attribute("lane_uses").contains("5,"):
+			var point_count = road_curve.get_point_count()
+			for index in range(point_count):
+				var point = road_curve.get_point_position(index)
+				point.y = max(0.0, get_basic_height(point))
+				road_curve.set_point_position(index, point)
+		
+		if render_3d:
 			#############################
 			# SET INITIAL POINT HEIGHTS #
 			#############################