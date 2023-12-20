71f367476fe6949431bf8f6fc98aa7244c38d554
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2023-12-20 14:56:31 +0100

    refactor: menu buttons now inherit from a utility script
    This eases the creation of menu/submenus/items via utility classes (i.e. menu and menuitem)
    
    Also cleanup and reduce code/node-trees

---
+++ UI/MenuBar/Util.gd
@@ -0,0 +1,80 @@
+extends MenuButtonExtended
+
+
+@export var action_handler_3d_path: Node
+@export var position_manager: PositionManager
+@export var player_node: Node3D
+const TABLESCENE = preload("res://UI/LabTable/LabTable.tscn")
+const DOLLYSCENE = preload("res://UI/DollyCamera/DollyWindow.tscn")
+
+
+var open_dolly_item = MenuItem.new(
+	"Open...", _begin_dolly)
+var open_table_item = MenuItem.new(
+	"Open...", _open_labtable)
+var table_fullscreen_item = MenuItem.new(
+	"Set Fullscreen", _set_labtable_fullscreen, true, false)
+
+var imaging_menu = Menu.new(
+	false, "ImagingMenu", [open_dolly_item])
+var labtable_menu = Menu.new(
+	false, "LabTableMenu", [open_table_item, table_fullscreen_item])
+var util_menu = Menu.new(
+	true, "UtilMenu", [imaging_menu, labtable_menu])
+
+var dolly_window: Window
+var previous_center_node: Node3D
+var labtable_window: Window
+
+
+func _ready():
+	super._ready()
+	dolly_window = DOLLYSCENE.instantiate()
+	dolly_window.visible = false
+	
+	dolly_window.action_handlers = [action_handler_3d_path]
+	dolly_window.close_requested.connect(_cleanup_dolly)
+
+
+func _open_labtable():
+	if labtable_window == null:
+		labtable_window = TABLESCENE.instantiate()
+	if not dolly_window.is_inside_tree():
+		get_tree().get_root().add_child(labtable_window)
+	
+	labtable_menu.popup.set_item_disabled(
+		labtable_menu.menu_items.find(open_table_item), true)
+	labtable_menu.popup.set_item_disabled(
+		labtable_menu.menu_items.find(table_fullscreen_item), false)
+
+
+func _set_labtable_fullscreen():
+	if labtable_window.mode == Window.MODE_FULLSCREEN:
+		labtable_window.mode = Window.MODE_WINDOWED
+		labtable_menu.popup.set_item_checked(
+			labtable_menu.menu_items.find(table_fullscreen_item), false)
+	else:
+		labtable_window.mode = Window.MODE_FULLSCREEN
+		labtable_menu.popup.set_item_checked(
+			labtable_menu.menu_items.find(table_fullscreen_item), true)
+
+
+func _begin_dolly():
+	if not dolly_window.is_inside_tree():
+		get_tree().get_root().add_child(dolly_window)
+	
+	dolly_window.popup()
+	dolly_window.position_manager = position_manager
+	
+	imaging_menu.popup.set_item_disabled(imaging_menu.menu_items.find(open_dolly_item), true)
+	
+	# Swap center node
+	previous_center_node = position_manager.center_node
+	position_manager.center_node = dolly_window.dolly_scene.dolly_cam
+
+
+func _cleanup_dolly():
+	# Reset center node
+	position_manager.center_node = previous_center_node
+	dolly_window.hide()
+	imaging_menu.popup.set_item_disabled(imaging_menu.menu_items.find(open_dolly_item), false)
--- UI/MenuBar/View.gd
+++ UI/MenuBar/View.gd
@@ -1,4 +1,4 @@
-extends MenuButton
+extends MenuButtonExtended
 
 
 @export var world_ui: Control
@@ -8,51 +8,33 @@ extends MenuButton
 @export var docks: Array[Control]
 @export var dock_names: Array[String]
 
-@onready var table_options = get_popup()
 
-enum TableOptions {
-	SET_FULLSCREEN,
-	RENDER_MAIN,
-	SHOW_DOCKS
-}
+@onready var fullscreen_item = MenuItem.new(
+	"Fullscreen", world_ui.on_fullscreen)
+@onready var render_main_item = MenuItem.new(
+	"Render Main Viewport", toggle_viewport_rendering, true, true)
+@onready var docks_menu = Menu.new(
+	false, "Show docks", [])
+@onready var view_menu = Menu.new(
+	true, "ViewMenu", [fullscreen_item, render_main_item, docks_menu])
 
 
 func _ready():
-	# Add options and store callback functions in metadata to call when 
-	# the option is pressed
-	table_options.add_item("Set Fullscreen", TableOptions.SET_FULLSCREEN)
-	table_options.set_item_as_checkable(TableOptions.SET_FULLSCREEN, true)
-	table_options.set_item_checked(TableOptions.SET_FULLSCREEN, false)
-	table_options.set_item_metadata(TableOptions.SET_FULLSCREEN, world_ui.on_fullscreen)
-	
-	table_options.add_check_item("Render main Viewport", TableOptions.RENDER_MAIN)
-	table_options.set_item_checked(TableOptions.RENDER_MAIN, true)
-	table_options.set_item_metadata(TableOptions.RENDER_MAIN, toggle_viewport_rendering)
-	
-	var visible_options = PopupMenu.new()
-	visible_options.name = "VisibilityPopup"
-	table_options.add_child(visible_options)
-	table_options.add_submenu_item("Show...", "VisibilityPopup", TableOptions.SHOW_DOCKS)
-	var submenu_name = table_options.get_item_submenu(0)
-	
+	super._ready()
 	var i = 0
+	var test = docks_menu.popup
 	for dock in docks:
-		visible_options.add_check_item(dock_names[i], i)
-		visible_options.set_item_checked(i, false)
-		visible_options.set_item_metadata(i, func(): 
+		docks_menu.popup.add_check_item(dock_names[i], i)
+		docks_menu.popup.set_item_checked(i, true)
+		docks_menu.popup.set_item_metadata(i, func(): 
 			docks[i].set_visible(not docks[i].visible)
-			visible_options.set_item_checked(i, docks[i].visible))
+			docks_menu.popup.set_item_checked(i, docks[i].visible))
 		i += 1
-	visible_options.index_pressed.connect(
-		func(idx): visible_options.get_item_metadata(idx).call())
-	
-	
-	# Connect item pressed with callback
-	table_options.index_pressed.connect(
-		func(idx): table_options.get_item_metadata(idx).call())
 
 
 func toggle_viewport_rendering():
-	var active = table_options.is_item_checked(TableOptions.RENDER_MAIN)
+	var idx = view_menu.menu_items.find(render_main_item)
+	var active = not view_menu.popup.is_item_checked(idx)
+	view_menu.popup.set_item_checked(idx, active)
 	var update_mode = SubViewport.UPDATE_DISABLED if not active else SubViewport.UPDATE_ALWAYS
 	viewport.render_target_update_mode = update_mode
