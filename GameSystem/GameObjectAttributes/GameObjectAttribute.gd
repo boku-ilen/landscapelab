789574c05e5d688b2bfc34bee7c894021b214ab9
LandscapeLab Office <landscapelab@boku.ac.at>
2024-01-02 10:58:28 +0100

    feat: add set_value to ExplicitGameObjectAttribute
    the abstract GameObjectAttribute also got set_value and allow_change, this way it should be easy to handle generically.

--- GameSystem/GameObjectAttributes/GameObjectAttribute.gd
+++ GameSystem/GameObjectAttributes/GameObjectAttribute.gd
@@ -3,8 +3,14 @@ class_name GameObjectAttribute
 
 
 var name := ""
+var allow_change = false
 
 
 # To be implemented
 func get_value(_game_object):
+	pass
+
+
+# To be implemented
+func set_value(_game_object, _new_value):
 	pass
