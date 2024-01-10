ceec2106ded180f2e33b26861e37ca6a63f9ff14
Mathias Baumgartinger~ <hiasbaum@gmail.com>
2024-01-10 14:15:16 +0100

    feat: add possibility to include alpha channel to WallTextureBundle
    An alpha channel is necessary for overlay blending of e.g. Windows into the Wall Texture

--- Buildings/Components/Walls/WallTextureBundle.gd
+++ Buildings/Components/Walls/WallTextureBundle.gd
@@ -10,23 +10,24 @@ class_name WallTextureBundle
 @export var roughness_texture: Texture : 
 	set(texture):
 		roughness_texture = texture
-		_on_bundled_texture_changed(roughness_texture, metallic_texture, emission_texture)
+		_on_bundled_texture_changed(roughness_texture, metallic_texture, emission_texture, albedo_texture)
 @export var metallic_texture: Texture: 
 	set(texture):
 		metallic_texture = texture
-		_on_bundled_texture_changed(roughness_texture, metallic_texture, emission_texture)
+		_on_bundled_texture_changed(roughness_texture, metallic_texture, emission_texture, albedo_texture)
 @export var emission_texture: Texture: 
 	set(texture):
 		emission_texture = texture
 		window_shading = true
-		_on_bundled_texture_changed(roughness_texture, metallic_texture, emission_texture)
+		_on_bundled_texture_changed(roughness_texture, metallic_texture, emission_texture, albedo_texture)
 
 var window_shading := false
 
 var bundled_texture
 
 
-func _on_bundled_texture_changed(roughness: Texture, metallic: Texture, emission: Texture):
+func _on_bundled_texture_changed(roughness: Texture, metallic: Texture, 
+								emission: Texture, albedo: Texture2D = null):
 	# If no textures are set there is no sense in bundling
 	if roughness == null and metallic == null and emission == null: 
 		bundled_texture = null
@@ -48,9 +49,21 @@ func _on_bundled_texture_changed(roughness: Texture, metallic: Texture, emission
 	# The obtained data is an empty array in case of null-texture; fill with 0s
 	_fill_empty(roughness_data, metallic_data, emission_data, height * width)
 	
+	# Try to obtain the alpha channel from the albedo
+	var alpha_data = []
+	if albedo != null:
+		var albedo_data = albedo.get_image().get_data()
+		var count = 0
+		for num in albedo_data:
+			if count % 4 == 3:
+				alpha_data.append(num)
+			count += 1
+	
 	# Check for same size
 	var OK = roughness_data.size() == metallic_data.size() \
 			and roughness_data.size() == emission_data.size()
+	# Alpha has to be either unset or same size
+	OK = OK and (alpha_data.is_empty() or alpha_data.size() == roughness_data.size())
 	if not OK:
 		push_error("Textures are not the same size!")
 		return
@@ -59,7 +72,8 @@ func _on_bundled_texture_changed(roughness: Texture, metallic: Texture, emission
 	var bundled = _bundle( 
 		roughness_data, 
 		metallic_data, 
-		emission_data)
+		emission_data,
+		alpha_data)
 	
 	# Store at other textures place
 	var path = roughness.resource_path if roughness != null \
@@ -68,8 +82,14 @@ func _on_bundled_texture_changed(roughness: Texture, metallic: Texture, emission
 	path = path.substr(0, path.rfind("/"))
 	
 	# Save image to disk and store the resulting texture
-	var bundled_image = Image.create_from_data(width, height, false, Image.FORMAT_RGB8, bundled)
+	var bundled_image
+	if not alpha_data.is_empty():
+		bundled_image = Image.create_from_data(width, height, false, Image.FORMAT_RGBA8, bundled)
+	else:
+		bundled_image = Image.create_from_data(width, height, false, Image.FORMAT_RGB8, bundled)
+	
 	bundled_image.generate_mipmaps()
+	bundled_image.save_png(path + "/roughness_metallic_emission_bundled.png")
 	bundled_image.save_jpg(path + "/roughness_metallic_emission_bundled.jpg", 1.0)
 	
 	bundled_texture = ImageTexture.create_from_image(bundled_image)
@@ -101,19 +121,25 @@ func _fill_empty(roughness_data, metallic_data, emission_data, size):
 		emission_data.fill(0)
 
 
-func _bundle(roughness_data, metallic_data, emission_data):
-	var bundle = func(arr1: Array, arr2: Array, arr3: Array):
+func _bundle(roughness_data, metallic_data, emission_data, alpha_data: Array = []):
+	var bundle = func(arr1: Array, arr2: Array, arr3: Array, arr4: Array):
 		var new_arr = []
-		new_arr.resize(arr1.size() + arr2.size() + arr3.size())
+		var array_size = arr1.size() + arr2.size() + arr3.size()
+		# Include alpha channel
+		array_size += arr4.size() if not arr4.is_empty() else 0
+		new_arr.resize(array_size)
 		new_arr.fill(0)
 		
+		var step = 3 + (1 if not arr4.is_empty() else 0)
 		for i in range(arr1.size()):
-			new_arr[i * 3] = arr1[i]
-			new_arr[i * 3 + 1] = arr2[i]
-			new_arr[i * 3 + 2] = arr3[i]
+			new_arr[i * step] = arr1[i]
+			new_arr[i * step + 1] = arr2[i]
+			new_arr[i * step + 2] = arr3[i]
+			if not arr4.is_empty():
+				new_arr[i * step + 3] = arr4[i]
 			
 		return new_arr
 	
-	var bundled = bundle.call(roughness_data, metallic_data, emission_data)
+	var bundled = bundle.call(roughness_data, metallic_data, emission_data, alpha_data)
 	
 	return bundled
