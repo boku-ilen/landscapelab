dac3f134aa4a745b26d4c4e01b0bc95c72a4cbd8
LandscapeLab Office <landscapelab@boku.ac.at>
2024-08-29 11:33:02 +0200

    fix: old path in LabTable script

--- UI/LabTable/LabTable.gd
+++ UI/LabTable/LabTable.gd
@@ -10,18 +10,18 @@ extends Control
 		if geo_layers:
 			geo_layers.player_node = new_player
 			
-		get_parent().get_node("AtmosphereMenu/AtmosphereConfiguration/LiveWeatherService").player = new_player
+		get_parent().get_node("MarginContainer/AtmosphereMenu/AtmosphereConfiguration/LiveWeatherService").player = new_player
 
 @export var time_manager: TimeManager:
 	set(new_time_manager):
 		time_manager = new_time_manager
-		get_parent().get_node("AtmosphereMenu").time_manager = new_time_manager
+		get_parent().get_node("MarginContainer/AtmosphereMenu").time_manager = new_time_manager
 
 @export var weather_manager: WeatherManager:
 	set(new_weather_manager):
 		weather_manager = new_weather_manager
-		get_parent().get_node("AtmosphereMenu").weather_manager = new_weather_manager
-		get_parent().get_node("AtmosphereMenu/AtmosphereConfiguration/LiveWeatherService").weather_manager = new_weather_manager
+		get_parent().get_node("MarginContainer/AtmosphereMenu").weather_manager = new_weather_manager
+		get_parent().get_node("MarginContainer/AtmosphereMenu/AtmosphereConfiguration/LiveWeatherService").weather_manager = new_weather_manager
 
 # To debug it as standalone (without running the rest of the landscapelab
 # it is necessary to load the configuration
