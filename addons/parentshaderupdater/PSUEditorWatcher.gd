6d9b7554a72f139a7a3ca8069d6163cda798a86b
Georg Sochurek <joatschi@gmx.at>
2024-12-31 16:46:56 +0100

    fix: Small tweaks to ParentShaderUpdater

--- addons/parentshaderupdater/PSUEditorWatcher.gd
+++ addons/parentshaderupdater/PSUEditorWatcher.gd
@@ -1,8 +1,10 @@
-# EditorPlugin that detects when any resource is saved (EditorWatcher), sends message via EditorDebuggerPlugin "session" (Communicator), which is received by Manager.
-# Manager then triggers Material Gathering on the Gatherers (which need to be added as Children of Nodes in the scene tree).
-# Used to update Materials at runtime when a Shader file is updated.
+# Used to automatically update Materials at runtime when a Shader file is saved.
+# EditorPlugin (PSUEditorWatcher) detects when any resource is saved, if it's a Shader, sends message via subclass (PSUEditorCommunicator)'s EditorDebuggerPlugin "session", which is received by (PSUManager).
+# PSUManager then triggers Material Gathering on the Gatherers (which need to be added as Children of Nodes in the scene tree), then updates Materials using the saved Shader file.
+# PSUManager is added as auto-load singleton from here.
 #
 # To implement in any listener, use the following - but careful, seems only one Node can register_message_capture!
+# Currently implemented in PSUManager.
 #
 #	func _ready():
 #		EngineDebugger.register_message_capture("MessageStringFromSender", FunctionToCall)
@@ -57,20 +59,20 @@ func _exit_tree() -> void:
 	remove_debugger_plugin(communicator)
 
 
-# Generic func call on any saved resource, differentiates based on resource class
+# Generic func call on any saved resource, differentiates based on resource class (for future exendability).
 func _on_resource_saved(res) -> void:
 	var resource_path: String = res.resource_path
 	var resource_class: String = res.get_class()
 		
 	if resource_class == "Shader":
-		_on_shader_saved(resource_path, resource_class) # Call func to trigger Shader specific messaging
+		_on_shader_saved(resource_path, resource_class) # Call func to trigger Shader specific messaging.
 		return
 	else:
 		if debug_print: print("PSUEditorWatcher: Saved '", resource_class, "' at '", resource_path, "' -> not a Shader. Doing nothing...")
 		return
 
 
-# Specific func call on Shader type resources, send message via Communicator that can then be captured by PSUManager to trigger shader update.
+# Specific func call on Shader type resources, send message via PSUEditorCommunicator subclass that can then be captured by PSUManager to trigger shader update.
 func _on_shader_saved(res_path, res_class):
 	communicator.message("res_shader_saved:done", [res_path, res_class])	# Colon and any string after it are required, so listener detects message correctly.
 	if debug_print: print("PSUEditorWatcher: Saved '", res_class, "' at '", res_path, "' - messaging PSUManager.")
