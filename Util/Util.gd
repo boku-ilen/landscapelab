201dd6735e7711999c39f29f42b419e3953d4335
Mathias Baumgartinger <hiasbaum@gmail.com>
2025-01-03 16:31:52 +0100

    fix: add previously unpushed util functionality

--- Util/Util.gd
+++ Util/Util.gd
@@ -4,3 +4,8 @@ class_name util
 
 static func str_to_var_or_default(value: String, default):
 	return str_to_var(value) if str_to_var(value) != null else default
+
+
+static func rangef(start: float, stop: float, step: float):
+	var range = range(start * 10000, stop * 10000, step * 10000)
+	return range.map(func(i): return i / 10000)
