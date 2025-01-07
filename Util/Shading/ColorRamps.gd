80977d04fd61a3f5af68d7292a07002b11f56e21
mathiasBaumgartinger <mathias.baumgartinger@proton.me>
2025-01-07 13:47:55 +0100

    feat: create symbology (legend) from color ramp also add new color ramp for wind

--- Util/Shading/ColorRamps.gd
+++ Util/Shading/ColorRamps.gd
@@ -1,6 +1,26 @@
 extends Resource
 class_name Colors
 
+
+static func create_legend(
+		color_ramp_id: String, 
+		vertical:=true, 
+		min_size:=Vector2(50,200), 
+		min_size_per_bucket:=Vector2(1,1)) -> BoxContainer:
+	var box: BoxContainer = VBoxContainer.new() if vertical else HBoxContainer.new() as BoxContainer
+	box.custom_minimum_size = min_size
+	box.add_theme_constant_override("separation", 0)
+	for color in color_ramps[color_ramp_id]:
+		var color_bucket = ColorRect.new()
+		box.add_child(color_bucket)
+		color_bucket.size_flags_vertical = Control.SIZE_EXPAND_FILL
+		color_bucket.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		color_bucket.custom_minimum_size = min_size_per_bucket
+		color_bucket.color = color
+	
+	return box
+
+
 const color_ramps = {
 	"Viridis": [
 		Color(0.2667, 0.0039, 0.3294),
@@ -19,5 +39,14 @@ const color_ramps = {
 		Color(0.6471, 0.8588, 0.2118),
 		Color(0.8235, 0.8863, 0.1059),
 		Color(0.9922, 0.9059, 0.1451),
+	],
+	"WindSpeed": [
+		Color.WHITE_SMOKE,
+		Color(0.792, 0.8, 0.851), 
+		Color(0.659, 0.694, 0.89), 
+		Color(0.522, 0.584, 0.925), 
+		Color(0.376, 0.463, 0.929), 
+		Color(0.251, 0.373, 1),
+		Color.NAVY_BLUE
 	]
 }
