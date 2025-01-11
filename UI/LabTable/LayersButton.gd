27ccee4d9a5155fd54a2c26e02f5db9153e84d24
Mathias Baumgartinger <hiasbaum@gmail.com>
2025-01-11 18:27:35 +0100

    feat: show a color-ramp symbology for wind layer

--- UI/LabTable/LayersButton.gd
+++ UI/LabTable/LayersButton.gd
@@ -7,9 +7,16 @@ extends Button
 # FIXME: make this more responsive to the actual layers instead of hardcoding
 func _ready() -> void:
 	pressed.connect(func(): $LayersPanel.visible = !$LayersPanel.visible)
+	
 	$LayersPanel/Content/MapButton.pressed.connect(func(): 
 			Layers.layer_definitions["Wind"].z_index = -1
+			$ColorRampSymbology.visible = false
 			rendering_subviewport.set_mouse_filter(MouseFilter.MOUSE_FILTER_PASS))
+			
 	$LayersPanel/Content/WindButton.pressed.connect(func():
-			Layers.layer_definitions["Wind"].z_index = 1
+			var wind_layer = Layers.layer_definitions["Wind"]
+			wind_layer.z_index = 1
+			$ColorRampSymbology.gradient = wind_layer.render_info.gradient
+			$ColorRampSymbology.ticks_from_relative_values([0, 0.25, 0.5, 0.75, 1.], wind_layer.render_info.min_val, wind_layer.render_info.max_val)
+			$ColorRampSymbology.visible = true
 			rendering_subviewport.set_mouse_filter(MouseFilter.MOUSE_FILTER_IGNORE))
