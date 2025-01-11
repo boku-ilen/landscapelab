9bbb77a9793e0f89b195edd3a2271f51c320e039
Mathias Baumgartinger <hiasbaum@gmail.com>
2025-01-11 18:20:35 +0100

    feat: ignore input when showing wind map

--- UI/LabTable/LayersButton.gd
+++ UI/LabTable/LayersButton.gd
@@ -1,8 +1,15 @@
 extends Button
 
 
+@export var rendering_subviewport: SubViewportContainer
+
+
 # FIXME: make this more responsive to the actual layers instead of hardcoding
 func _ready() -> void:
 	pressed.connect(func(): $LayersPanel.visible = !$LayersPanel.visible)
-	$LayersPanel/Content/MapButton.pressed.connect(func(): Layers.layer_definitions["Wind"].z_index = -1)
-	$LayersPanel/Content/WindButton.pressed.connect(func(): Layers.layer_definitions["Wind"].z_index = 1)
+	$LayersPanel/Content/MapButton.pressed.connect(func(): 
+			Layers.layer_definitions["Wind"].z_index = -1
+			rendering_subviewport.set_mouse_filter(MouseFilter.MOUSE_FILTER_PASS))
+	$LayersPanel/Content/WindButton.pressed.connect(func():
+			Layers.layer_definitions["Wind"].z_index = 1
+			rendering_subviewport.set_mouse_filter(MouseFilter.MOUSE_FILTER_IGNORE))
