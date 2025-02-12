b56e0d87f4501cba2f58a0c2d78e2d6769c04eda
mathiasBaumgartinger <mathias.baumgartinger@proton.me>
2025-02-12 09:46:45 +0100

    feat: update/fix PolygonObjectRenderer
    Instead of filling all polygons of the underlying dataset (using geodot features), now points activate polygons (by intersection) and then get filled using only internal godot math

--- Layers/Renderers/PolygonObject/PolygonObjectRenderer.gd
+++ Layers/Renderers/PolygonObject/PolygonObjectRenderer.gd
@@ -1,67 +1,87 @@
 extends LayerCompositionRenderer
 
 
+@export var create_objects_as_geodata: bool = false
 var radius = 10000.0
 var max_features = 2000
 var distance_between_objects = 10
 # Stores if the object-layer has been processed previously
 var processed = false
-var very_large_number = 999999.0
 
 var object_instances = []
 
 
 func full_load():
+	# Polygons (e.g. fields)
 	var polygon_layer: GeoFeatureLayer = layer_composition.render_info.polygon_layer
-	var object_layer: GeoFeatureLayer = layer_composition.render_info.object_layer
-	
-	# Extract features
-	var features = polygon_layer.get_features_near_position(float(center[0]), float(center[1]), radius, max_features)
+	# Points which activate (i.e. fill a polygon) by looking for intersection
+	var activation_layer: GeoFeatureLayer = layer_composition.render_info.activation_layer
 	
 	# Create the objects inside each individual polygon
-	for poly_feature in features:
-		var polygon = poly_feature.get_outer_vertices()
+	for activation_point in activation_layer.get_features_near_position(
+		float(center[0]) + position_manager.center_node.position.x, 
+		float(center[1]) - position_manager.center_node.position.z, 
+		radius, 
+		max_features):
 		
-		# Find left-most and bottom-most and right-most, top-most point in polygon
-		var min_pos = Vector3(very_large_number, 0, very_large_number)
-		var max_pos = Vector3(-very_large_number, 0, -very_large_number)
-		for vertex in polygon: 
-			min_pos.x = vertex.x if vertex.x < min_pos.x else min_pos.x
-			min_pos.z = vertex.y if vertex.y < min_pos.z else min_pos.z
+		# Extract polygons
+		var pos = activation_point.get_vector3()#get_offset_vector3(-center[0], 0, -center[1])
+		var poly_features = polygon_layer.get_features_near_position(pos.x, -pos.z, 0.2, 1)
+		
+		for poly_feature in poly_features:
+			var polygon = poly_feature.get_float_offset_outer_vertices(-center[0], -center[1])
 			
-			max_pos.x = vertex.x if vertex.x > max_pos.x else max_pos.x
-			max_pos.z = vertex.y if vertex.y > max_pos.z else max_pos.z
-		
-		var object: Node3D = layer_composition.render_info.object.instantiate()
-		
-		var current_pos = min_pos
-		while current_pos.x <= max_pos.x:
-			current_pos.z = min_pos.z
-			while current_pos.z <= max_pos.z:
-				# Predefined points in the object that have to be inside the polygon
-				# (i.e. in most cases some form of foothold)
-				var fully_inside = true
-				for foothold in object.get_node("Footholds").get_children():
-					# Add relative foothold position to absolute object position
-					var foothold_pos = (current_pos + foothold.position)
-					var point_feature = object_layer.create_feature()
-					#point_feature.set_offset_vector3(Vector3(foothold_pos.x, 0, foothold_pos.z), -center[0], 0, -center[1])
-					point_feature.set_vector3(Vector3(foothold_pos.x, 0, foothold_pos.z))
+			# Find left-most and bottom-most and right-most, top-most point in polygon
+			var min_pos = Vector3(INF, 0, INF)
+			var max_pos = Vector3(-INF, 0, -INF)
+			for vertex in polygon: 
+				min_pos.x = vertex.x if vertex.x < min_pos.x else min_pos.x
+				min_pos.z = vertex.y if vertex.y < min_pos.z else min_pos.z
+				
+				max_pos.x = vertex.x if vertex.x > max_pos.x else max_pos.x
+				max_pos.z = vertex.y if vertex.y > max_pos.z else max_pos.z
+			
+			var object: Node3D = load(layer_composition.render_info.object).instantiate()
+			
+			var aabb = util.get_summed_aabb(object)
+			
+			var current_pos = min_pos
+			while current_pos.x <= max_pos.x:
+				current_pos.z = min_pos.z
+				while current_pos.z <= max_pos.z:
+					# Predefined points in the object that have to be inside the polygon
+					# (i.e. in most cases some form of foothold)
+					var fully_inside = true
+					var bounds = [
+						Vector2(aabb.position.x, aabb.position.z), 
+						Vector2(aabb.position.x, aabb.end.z),
+						Vector2(aabb.end.x, aabb.end.z),
+						Vector2(aabb.end.x, aabb.position.z)
+					]
 					
-					if not poly_feature.intersects_with(point_feature):
-						fully_inside = false
-				
-				if fully_inside:
-					var new_object = layer_composition.render_info.object.instantiate()
-					new_object.rotation.y = deg_to_rad(layer_composition.render_info.individual_rotation)
-					new_object.position = current_pos
-					new_object.position.y = layer_composition.render_info.ground_height_layer.get_value_at_position(
-						center[0] + current_pos.x, center[1] - current_pos.z)
-					object_instances.append(new_object)
-				
-				# Go up and right
-				current_pos += Vector3.BACK * distance_between_objects
-			current_pos += Vector3.RIGHT * distance_between_objects
+					# Add relative foothold position to absolute object position
+					var current_pos_2d = Vector2(current_pos.x, current_pos.z)
+					fully_inside = bounds.reduce(func(still_inside, b): 
+						return Geometry2D.is_point_in_polygon(b + current_pos_2d, polygon) and still_inside, true)
+					var engine_pos = current_pos
+					engine_pos.z = -engine_pos.z
+					#engine_pos.x -= position_manager.center_node.position.x
+					#engine_pos.z -= position_manager.center_node.position.z
+					
+					engine_pos.y = layer_composition.render_info.ground_height_layer.get_value_at_position(
+							center[0] + engine_pos.x, (center[1] - engine_pos.z))
+					print(engine_pos)
+					print(center[0] + engine_pos.x, " - ", center[1] - engine_pos.z)
+					
+					if fully_inside:
+						var new_object = object.duplicate()
+						new_object.rotation.y = deg_to_rad(layer_composition.render_info.individual_rotation)
+						new_object.position = engine_pos
+						object_instances.append(new_object)
+					
+					# Go up and right
+					current_pos += Vector3.BACK * distance_between_objects
+				current_pos += Vector3.RIGHT * distance_between_objects
 
 
 func apply_new_data():
