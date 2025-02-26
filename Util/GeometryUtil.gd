769ae488afe8e78808230d2fa6b72dcff0c67cf8
mathiasBaumgartinger <mathias.baumgartinger@proton.me>
2025-02-26 14:04:35 +0100

    feat: add a geometry util script
    These are static functions to help work with geometry. Specifically two functions for "inseting" a polygon have been designed. Insetting we define shrinking a polygon by not scaling, but by going inside at each vertex for a certain distance.

---
+++ Util/GeometryUtil.gd
@@ -0,0 +1,40 @@
+extends Node
+class_name GeometryUtil
+
+
+
+static func get_polygon_vertex_directions(polygon: Array[Vector2]) -> Array[Vector2]:
+	var directions: Array[Vector2] = []
+	directions.resize(polygon.size())
+	
+	for idx in polygon.size():
+		var current_vert: Vector2 = polygon[idx]
+		var prev_vert: Vector2 = polygon[(idx - 1) % polygon.size()]
+		var next_vert: Vector2 = polygon[(idx + 1) % polygon.size()]
+		
+		# To connect a polygon, starting and finishing vertex sometimes are duplicates
+		# if so, skip them
+		if   current_vert == next_vert: next_vert = polygon[(idx + 2) % polygon.size()]
+		elif current_vert == prev_vert: prev_vert = polygon[(idx - 2) % polygon.size()]
+		
+		var current_to_prev: Vector2 = current_vert.direction_to(prev_vert)
+		var current_to_next: Vector2 = current_vert.direction_to(next_vert)
+		
+		# Find the vertex direction given the two previous vertices 
+		var angle: float = current_to_prev.angle_to(current_to_next)
+		var direction: Vector2 = (-current_to_prev - current_to_next).normalized()
+		direction *= -sign(angle)
+		
+		directions[idx] = direction
+	
+	return directions
+
+
+static func offset_polygon_vertices(
+	polygon: Array[Vector2], directions: Array[Vector2], offset: float):
+	assert(polygon.size() == directions.size())
+	
+	for idx in polygon.size():
+		polygon[idx] += directions[idx] * offset
+	
+	return polygon
