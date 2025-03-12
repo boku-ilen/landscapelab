e7b372a91f1596accabaf8824a94bd2386aa6df7
Karl Office <karl.bittner@boku.ac.at>
2025-03-12 17:08:13 +0100

    feat: add ProgressFromCenter score option for GameSystem
    this progress bar does not count up from 0 to a target, but centers at 0, displaying a green bar if the value is above and a red bar if the value is below 0.

--- UI/GameSystem/ProgressBarScoreUI.gd
+++ UI/GameSystem/ProgressBarScoreUI.gd
@@ -1,6 +1,8 @@
 extends "res://UI/GameSystem/AbstractScoreUI.gd"
 
 
+var current_progress_bar
+
 var score: GameScore :
 	get:
 		return score 
@@ -8,28 +10,38 @@ var score: GameScore :
 		super.set_score(new_score)
 		score = new_score
 		
-		$VBoxContainer/Name.text = score.name
-		$VBoxContainer/MaxValue.text = str(score.target)
-		$ProgressBar.min_value = 0.0
-		$ProgressBar.max_value = score.target
-		
-		if score.contributors[0].color_code:
-			# TODO: Godot 4.0 implements a vertical ProgressBar
-			if $ProgressBar is ProgressBar:
-				# Get current stylebox of the progress, duplicate so it only affects this node
-				# and only override the background color so it fits the rest of the theme
-				var new_stylebox = $ProgressBar.get_stylebox("fg").duplicate()
-				# For many themes, a texture could be used which does not have a color
-				# in no styleboxflat is used, create one checked our own
-				if "bg_color" in new_stylebox:
-					new_stylebox.bg_color = score.contributors[0].color_code
-					$ProgressBar.add_theme_stylebox_override("fg", new_stylebox)
-				else: 
-					new_stylebox = StyleBoxFlat.new()
-					new_stylebox.bg_color = score.contributors[0].color_code
-					$ProgressBar.add_theme_stylebox_override("fg", new_stylebox)
-			elif $ProgressBar is TextureProgressBar:
-				$ProgressBar.tint_progress = score.contributors[0].color_code
+		if score.target > 0:
+			current_progress_bar = $ProgressBar
+			$ProgressFromCenter.visible = false
+			
+			$VBoxContainer/Name.text = score.name
+			$VBoxContainer/MaxValue.text = str(score.target)
+			$ProgressBar.min_value = 0.0
+			$ProgressBar.max_value = score.target
+			
+			if score.contributors[0].color_code:
+				# TODO: Godot 4.0 implements a vertical ProgressBar
+				if $ProgressBar is ProgressBar:
+					# Get current stylebox of the progress, duplicate so it only affects this node
+					# and only override the background color so it fits the rest of the theme
+					var new_stylebox = $ProgressBar.get_stylebox("fg").duplicate()
+					# For many themes, a texture could be used which does not have a color
+					# in no styleboxflat is used, create one checked our own
+					if "bg_color" in new_stylebox:
+						new_stylebox.bg_color = score.contributors[0].color_code
+						$ProgressBar.add_theme_stylebox_override("fg", new_stylebox)
+					else: 
+						new_stylebox = StyleBoxFlat.new()
+						new_stylebox.bg_color = score.contributors[0].color_code
+						$ProgressBar.add_theme_stylebox_override("fg", new_stylebox)
+				elif $ProgressBar is TextureProgressBar:
+					$ProgressBar.tint_progress = score.contributors[0].color_code
+		else:
+			current_progress_bar = $ProgressFromCenter
+			$VBoxContainer/Name.text = score.name
+			$ProgressBar.visible = false
+			$VBoxContainer/ValueSeparator.visible = false
+			$VBoxContainer/MaxValue.visible = false
 	
 	
 		_update_data(score.value)
@@ -38,4 +50,4 @@ var score: GameScore :
 
 func _update_data(new_value):
 	$VBoxContainer/CurrentValue.text = str(round(new_value))
-	$ProgressBar.value = new_value
+	current_progress_bar.value = new_value
