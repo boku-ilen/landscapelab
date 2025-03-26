ba5827676e61469f6d27d3f8318bd87819c8d03c
Georg Sochurek <joatschi@gmx.at>
2025-03-26 11:44:27 +0100

    fix: bugs with buttons pressed by LabTable bricks

--- UI/LabTable/TableButton.gd
+++ UI/LabTable/TableButton.gd
@@ -5,7 +5,6 @@ func _input(event):
 	if not is_visible_in_tree(): return
 	
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-			if get_global_rect().has_point(event.position):
-				# On left click (new brick placed), close immediately
+		if get_global_rect().has_point(event.position):
+				pressed.emit()
 				get_viewport().set_input_as_handled()
-				GameSystem.activate_next_game_mode()
