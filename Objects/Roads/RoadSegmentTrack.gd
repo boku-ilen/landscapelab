677bd593cab0cea0677d8f3500ab80ebab676182
Karl Office <karl.bittner@boku.ac.at>
2025-08-28 10:54:39 +0200

    feat: Road improvements

--- Objects/Roads/RoadSegmentTrack.gd
+++ Objects/Roads/RoadSegmentTrack.gd
@@ -17,26 +17,41 @@ func setup(new_feature):
 	
 	var grade = feature.get_attribute("tracktype")
 	
+	var surface = feature.get_attribute("surface")
+	
 	var lid_center
 	var lid_edge
 	var lid_outer_edge
 	var lid_track
 	
-	if grade == "grade1":
-		lid_center = 2001
-		lid_edge = 2001
-		lid_track = 2001
-		lid_outer_edge = 7502
-	elif grade == "grade2":
-		lid_center = 10000
-		lid_edge = 10000
-		lid_track = 2001
-		lid_outer_edge = 7502
-	else:
-		lid_center = 7501
-		lid_edge = 7502
-		lid_track = 10000
-		lid_outer_edge = 7503
+	if surface and not surface.is_empty():
+		if surface == "grass":
+			lid_center = 7501
+			lid_edge = 0
+			lid_track = 7501
+			lid_outer_edge = 0
+		elif surface == "asphalt":
+			lid_center = 2001
+			lid_edge = 2001
+			lid_track = 2001
+			lid_outer_edge = 7502
+	
+	if not lid_center:
+		if grade == "grade1":
+			lid_center = 2001
+			lid_edge = 2001
+			lid_track = 2001
+			lid_outer_edge = 7502
+		elif grade == "grade2":
+			lid_center = 10000
+			lid_edge = 10000
+			lid_track = 2001
+			lid_outer_edge = 7502
+		else:
+			lid_center = 7501
+			lid_edge = 7502
+			lid_track = 10000
+			lid_outer_edge = 7503
 	
 	material_override.set_shader_parameter("lid_color_center", Color8(
 		lid_center % 255,
