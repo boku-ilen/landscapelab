d0b530ce6844d05b9ae59c1ccbf3866657d6bac1
Karl Office <karl.bittner@boku.ac.at>
2026-04-22 14:08:32 +0200

    perf: use viewport downscaling on main vp when vr is active

--- UI/VRPerformanceManager.gd
+++ UI/VRPerformanceManager.gd
@@ -2,6 +2,9 @@ extends Node
 
 @export var main_viewport: SubViewport
 
+@export var fps_when_vr_used := 5
+@export var viewport_downscale_when_vr_used := 0.5
+
 
 func _ready():
 	$FPSTimer.timeout.connect(set_new_frame)
@@ -16,11 +19,14 @@ func _on_openxr_visible_state():
 	# User took off VR headset -> maximize framerate
 	$FPSTimer.stop()
 	main_viewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+	main_viewport.scaling_3d_scale = 1.0
 
 
 func _on_openxr_focused_state():
 	# User put on VR headset -> limit main viewport framerate
+	$FPSTimer.timeout = 1.0 / float(fps_when_vr_used)
 	$FPSTimer.start()
+	main_viewport.scaling_3d_scale = viewport_downscale_when_vr_used
 
 
 func set_new_frame():
