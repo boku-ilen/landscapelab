96e00451e96a005d18ac2aa6db29f1ef150ca078
Karl Office <karl.bittner@boku.ac.at>
2026-05-13 15:45:02 +0200

    feat: move third (last) batch of assets

--- Vegetation/VegetationLoadingUtil.gd
+++ Vegetation/VegetationLoadingUtil.gd
@@ -9,6 +9,15 @@ static func _create_density_classes(densities_data: Array) -> Dictionary:
 	var density_classes = {}
 	
 	for line in densities_data:
+		# Backwards compatibility and parsing for mesh
+		var mesh
+		if line["Mesh"] == "res://Resources/Meshes/VegetationBillboard/1m_billboard_cluster.obj" or line["Mesh"] == "Cluster":
+			mesh = load("res://Assets/Natural/Plants/Foliage/Meshes/1m_billboard_cluster.obj")
+		elif line["Mesh"] == "res://Resources/Meshes/_NEW assets/shrub/small.obj" or line["Mesh"] == "Shrub":
+			mesh = load("res://Assets/Natural/Plants/Foliage/Meshes/Shrub/small.obj")
+		else:
+			mesh = load("res://Resources/Meshes/VegetationBillboard/1m_billboard.obj")
+		
 		var density_class = DensityClass.new(
 			str_to_var(line["ID"]),
 			line["Density Class"],
@@ -16,7 +25,7 @@ static func _create_density_classes(densities_data: Array) -> Dictionary:
 			line["Note"],
 			str_to_var(line["Godot Density per m"]),
 			str_to_var(line["Base Extent"]),
-			load(line["Mesh"]) if "Mesh" in line and not line["Mesh"].is_empty() else load("res://Resources/Meshes/VegetationBillboard/1m_billboard.obj"),
+			mesh,
 			str_to_var(line["Is Billboard"]) if "Is Billboard" in line and not line["Is Billboard"].is_empty() else true
 		)
 		density_classes[density_class.id] = density_class
